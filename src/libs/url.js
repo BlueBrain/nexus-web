796db20f6cbe38dd725bb3e5fc8407a409cb38ec
Pirman Kenneth William <kenneth.pirman@epfl.ch>
2018-05-02 17:43:17 +0200

    make empty search results, add login suggestion

--- src/libs/url.js
+++ src/libs/url.js
@@ -25,3 +25,14 @@ export function removeTokenFromUrl(location) {
   window.history.replaceState({}, document.title, appLocation);
   return access_token;
 }
+
+export function getParameterByName(url, name) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
