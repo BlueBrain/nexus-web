2d162768affb792ba1b09802cd70b56e8675c6d2
kenjinp <github@kenny.wtf>
2018-08-13 11:44:28 +0200

    add copy token button

--- src/components/CopyToClipboard.js
+++ src/components/CopyToClipboard.js
@@ -1,72 +1,75 @@
-import React from 'react';
-import styled from 'styled-jss';
-import PropTypes from 'prop-types';
-import { CopyToClipboard as Copy} from 'react-copy-to-clipboard';
-import FontAwesome from 'react-fontawesome';
-import ReactTooltip from 'react-tooltip'
+import React, { cloneElement } from "react";
+import styled from "styled-jss";
+import PropTypes from "prop-types";
+import { CopyToClipboard as Copy } from "react-copy-to-clipboard";
+import FontAwesome from "react-fontawesome";
+import ReactTooltip from "react-tooltip";
 
 const COPY_REVERT_DURATION = 1000;
-const COPY_TEXT = ['copied!', 'copy details'];
+const COPY_TEXT = ["copied!", "copy details"];
 
-const CopyBlock = styled('small')({
-  cursor: 'pointer',
-  fontSize: '1.2em',
-  '& span': {
-    transition: 'color 200ms'
+const CopyBlock = styled("small")({
+  cursor: "pointer",
+  fontSize: "1.2em",
+  "& span": {
+    transition: "color 200ms"
   },
-  '& .copied': {
-    color: 'rgba(185, 233, 212, 1)'
+  "& .copied": {
+    color: "rgba(185, 233, 212, 1)"
   }
 });
 
 class CopyToClipboard extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-          copied: false
-      };
-    }
-    onCopy () {
-      this.setState({copied: true})
-      setTimeout(() => {
-        this.setState({copied: false})
-      }, COPY_REVERT_DURATION)
-    }
-    render() {
-      const { value, text, icon } = this.props;
-      const id = 'copy' + value.toString().replace(' ', '-') + Math.random();
-      const { copied } = this.state
-      const [textCopied, textCopy] = COPY_TEXT
+  constructor(props) {
+    super(props);
+    this.state = {
+      copied: false
+    };
+  }
+  onCopy() {
+    this.setState({ copied: true });
+    setTimeout(() => {
+      this.setState({ copied: false });
+    }, COPY_REVERT_DURATION);
+  }
+  render() {
+    const { value, text, icon, children } = this.props;
+    const id = "copy" + value.toString().replace(" ", "-") + Math.random();
+    const { copied } = this.state;
+    const [textCopied, textCopy] = COPY_TEXT;
+    let content = children ? (
+      cloneElement(children, { copied })
+    ) : (
+      <FontAwesome
+        className={copied ? "copied" : ""}
+        name={icon || "paste"}
+        style={{ textShadow: "0 1px 0 rgba(0, 0, 0, 0.1)" }}
+      />
+    );
 
-      return (
-          <CopyBlock>
-              <ReactTooltip
-                  id={id}
-                  className='small-tooltip'
-                  effect='solid'
-                  getContent={() => copied ? textCopied : text || textCopy }
-              />
-              <Copy
-                  text={value}
-                  onCopy={this.onCopy.bind(this)}
-              >
-                  <FontAwesome
-                      data-for={id}
-                      data-tip
-                      className={ copied ? 'copied' : ''}
-                      name={icon || 'paste'}
-                      style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
-                  />
-              </Copy>
-          </CopyBlock>
-      );
-    }
+    return (
+      <CopyBlock>
+        <ReactTooltip
+          id={id}
+          className="small-tooltip"
+          effect="solid"
+          getContent={() => (copied ? textCopied : text || textCopy)}
+        />
+        <Copy text={value} onCopy={this.onCopy.bind(this)}>
+          <div data-for={id} data-tip>
+            {content}
+          </div>
+        </Copy>
+      </CopyBlock>
+    );
+  }
 }
 
 CopyToClipboard.propTypes = {
-    value: PropTypes.string,
-    text: PropTypes.string,
-    icon: PropTypes.string
+  value: PropTypes.string,
+  text: PropTypes.string,
+  icon: PropTypes.string,
+  children: PropTypes.element
 };
 
-export default CopyToClipboard
+export default CopyToClipboard;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,61 @@
-import React from "react";
+import React, { Fragment } from "react";
+import styled from "styled-jss";
 import { Auth, WithStore } from "@bbp/nexus-react";
 import { auth } from "../store/actions";
+import CopyToClipboard from "./CopyToClipboard";
+
+const CopyToken = styled("small")({
+  fontSize: "12px",
+  backgroundColor: "white",
+  padding: "1em",
+  borderRadius: "2px",
+  "& a": {
+    color: "#888",
+    transition: "color 200ms"
+  },
+  "& a.copied": {
+    color: "rgba(185, 233, 212, 1)"
+  }
+});
+
+
+const CopyTokenContainer = ({ copied }) => (
+  <CopyToken>
+    <a className={copied ? "copied" : ""} target="_blank" rel="noopener noreferrer">
+      {" "}
+      copy token{" "}
+    </a>
+  </CopyToken>
+);
 
 const Login = () => (
-  <div className="login-block">
-    <WithStore
-      mapStateToProps={({ auth, config }) => ({
-        name: auth.tokenOwner,
-        loginURI: config.loginURI
-      })}
-      mapDispatchToProps={{
-        authenticate: auth.authenticate,
-        logout: auth.logout
-      }}
-    >
-      {({ name, loginURI, authenticate, logout }) => (
-        <Auth.Container
-          name={name}
-          authenticate={() => authenticate(window.location.href)}
-          logout={logout}
-          loginURI={loginURI}
-        />
-      )}
-    </WithStore>
-  </div>
+  <WithStore
+    mapStateToProps={({ auth, config }) => ({
+      name: auth.tokenOwner,
+      token: auth.token,
+      loginURI: config.loginURI
+    })}
+    mapDispatchToProps={{
+      authenticate: auth.authenticate,
+      logout: auth.logout
+    }}
+  >
+    {({ name, loginURI, authenticate, logout, token }) => (
+      <Fragment>
+          <CopyToClipboard text={"copy token to clipboard"} value={token}>
+            <CopyTokenContainer/>
+          </CopyToClipboard>
+        <div className="login-block">
+          <Auth.Container
+            name={name}
+            authenticate={() => authenticate(window.location.href)}
+            logout={logout}
+            loginURI={loginURI}
+          />
+        </div>
+      </Fragment>
+    )}
+  </WithStore>
 );
 
 export default Login;
