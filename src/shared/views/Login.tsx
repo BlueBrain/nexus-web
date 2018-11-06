9df474fdcd5921a00678216b64879ed61c9800f2
Julien Machon <julienmachon@gmail.com>
2018-11-06 17:11:39 +0100

    add fake auth link

--- src/shared/views/Login.tsx
+++ src/shared/views/Login.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 const Login: React.StatelessComponent = () => (
-  <p>LOGIN</p>
+  <a href="authSuccess?access_token=aksmdkadadasdad">LOGIN</a>
 );
 
 export default Login;
