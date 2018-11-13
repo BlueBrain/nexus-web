7a0c195f57d0725aba965eff81cba3ecfc032b3a
Julien Machon <julienmachon@gmail.com>
2018-11-13 10:13:46 +0100

    initial Login component

---
+++ src/shared/components/Login/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import './Login.less';
+
+export interface LoginProps {}
+
+const Login: React.SFC<LoginProps> = () => (
+  <div className="Login">
+    <a>Login</a>
+  </div>
+);
+
+export default Login;
