27e09234956ca96d036dac1d19db0f83429d2b85
Julien Machon <julienmachon@gmail.com>
2018-11-13 10:11:25 +0100

    refactor React import

--- src/shared/components/Header/Header.stories.tsx
+++ src/shared/components/Header/Header.stories.tsx
@@ -1,11 +1,12 @@
-import React = require('react');
+import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 
 import Header from './index';
 
-storiesOf('Components/Header', module)
-  .add('Header', withInfo(`
+storiesOf('Components/Header', module).add(
+  'Header',
+  withInfo(`
     The header component is used throughout the application.
 
     It is used to display the user name and navigation links
@@ -16,10 +17,12 @@ storiesOf('Components/Header', module)
       <a href="#">Settings</a>,
     ]} />
     ~~~
-  `)(() => <div style={{ margin: '50px 40px 0px' }}><Header
-    name="Mark Hamil"
-    links={[
-      <a href="#">Home</a>,
-      <a href="#">Settings</a>,
-    ]} /></div>,
-));
+  `)(() => (
+    <div style={{ margin: '50px 40px 0px' }}>
+      <Header
+        name="Mark Hamil"
+        links={[<a href="#">Home</a>, <a href="#">Settings</a>]}
+      />
+    </div>
+  ))
+);
