7f50d47d139f05b95e4fef5b8e46b20bc81ccd0d
Dupont Michaël <michael.dupont@epfl.ch>
2018-12-07 17:02:34 +0100

    Remove v0 version, now lives in nexus-explorer branch

--- src/store/modules/index.js
+++
@@ -1,8 +0,0 @@
-import authStoreModule from "./auth";
-import {
-  searchBar as searchBarModule,
-  searchResults as searchResultsModule
-} from "./search";
-export const auth = authStoreModule;
-export const searchBar = searchBarModule;
-export const searchResults = searchResultsModule;
