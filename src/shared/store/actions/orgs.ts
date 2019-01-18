10ffc1e7c7ab482948664f3c41803a8e3b26340c
Julien Machon <julienmachon@gmail.com>
2019-01-18 14:17:50 +0100

    create Org redux actions

---
+++ src/shared/store/actions/orgs.ts
@@ -0,0 +1,113 @@
+import { Action, ActionCreator, Dispatch } from 'redux';
+import { Organization } from '@bbp/nexus-sdk';
+import { ThunkAction } from '..';
+
+//
+// Action Types
+//
+interface CreateOrgAction extends Action {
+  type: '@@nexus/ORG_CREATING';
+}
+interface CreateOrgSuccessAction extends Action {
+  type: '@@nexus/ORG_CREATING_SUCCESS';
+  org: Organization;
+}
+interface CreateOrgFailureAction extends Action {
+  type: '@@nexus/ORG_CREATING_FAILURE';
+  error: Error;
+}
+
+interface ModifyOrgAction extends Action {
+  type: '@@nexus/ORG_MODIFYING';
+}
+interface ModifyOrgSuccessAction extends Action {
+  type: '@@nexus/ORG_MODIFYING_SUCCESS';
+  org: Organization;
+}
+interface ModifyOrgFailureAction extends Action {
+  type: '@@nexus/ORG_MODIFYING_FAILURE';
+  error: Error;
+}
+export type OrgActions =
+  | CreateOrgAction
+  | CreateOrgSuccessAction
+  | CreateOrgFailureAction
+  | ModifyOrgAction
+  | ModifyOrgSuccessAction
+  | ModifyOrgFailureAction;
+
+//
+// Action definitions
+//
+const createOrgAction: ActionCreator<CreateOrgAction> = () => ({
+  type: '@@nexus/ORG_CREATING',
+});
+const createOrgSuccessAction: ActionCreator<CreateOrgSuccessAction> = (
+  org: Organization
+) => ({
+  org,
+  type: '@@nexus/ORG_CREATING_SUCCESS',
+});
+const createOrgFailureAction: ActionCreator<CreateOrgFailureAction> = (
+  error: any
+) => ({
+  error,
+  type: '@@nexus/ORG_CREATING_FAILURE',
+});
+
+const modifyOrgAction: ActionCreator<ModifyOrgAction> = () => ({
+  type: '@@nexus/ORG_MODIFYING',
+});
+const modifyOrgSuccessAction: ActionCreator<ModifyOrgSuccessAction> = (
+  org: Organization
+) => ({
+  org,
+  type: '@@nexus/ORG_MODIFYING_SUCCESS',
+});
+const modifyOrgFailureAction: ActionCreator<ModifyOrgFailureAction> = (
+  error: any
+) => ({
+  error,
+  type: '@@nexus/ORG_MODIFYING_FAILURE',
+});
+
+//
+// Action implementations
+//
+export const createOrg: ActionCreator<ThunkAction> = (
+  orgLabel: string,
+  orgName: string
+) => {
+  return async (
+    dispatch: Dispatch<any>,
+    getState,
+    { nexus }
+  ): Promise<CreateOrgSuccessAction | CreateOrgFailureAction> => {
+    dispatch(createOrgAction());
+    try {
+      const org: Organization = await Organization.create(orgLabel, orgName);
+      return dispatch(createOrgSuccessAction(org));
+    } catch (e) {
+      return Promise.reject(dispatch(createOrgFailureAction(e)));
+    }
+  };
+};
+export const modifyOrg: ActionCreator<ThunkAction> = (
+  orgLabel: string,
+  rev: number,
+  orgName: string
+) => {
+  return async (
+    dispatch: Dispatch<any>,
+    getState,
+    { nexus }
+  ): Promise<ModifyOrgSuccessAction | ModifyOrgFailureAction> => {
+    dispatch(modifyOrgAction());
+    try {
+      const org: Organization = await Organization.update(orgLabel, rev, name);
+      return dispatch(modifyOrgSuccessAction(org));
+    } catch (e) {
+      return Promise.reject(dispatch(modifyOrgFailureAction(e)));
+    }
+  };
+};
