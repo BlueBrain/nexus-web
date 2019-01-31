852ca0ca5fc0c331240cf3e319c2789eaedc2741
kenjinp <github@kenny.wtf>
2019-01-31 11:31:45 +0100

    add a basic popover to view metadata details of resources

--- src/shared/components/Resources/ResourceItem.tsx
+++ src/shared/components/Resources/ResourceItem.tsx
@@ -1,34 +1,65 @@
 import * as React from 'react';
-import { List } from 'antd';
+import { Popover, Card } from 'antd';
 import TypesIcon from '../Types/TypesIcon';
 
 import './Resources.less';
+import { Meta } from 'antd/lib/list/Item';
+import moment = require('moment');
 
-const { Item } = List;
-
-export interface ResourceItemProps {
-  name?: string;
+export interface ResourceItemProps extends ResourceMetadataCardProps {
   id: string;
   type?: string[];
-  constrainedBy: string;
   onClick?(): void;
   onEdit?(): void;
 }
 
-const ResourceListItem: React.FunctionComponent<ResourceItemProps> = ({
-  constrainedBy,
-  type,
-  name,
-  onClick = () => {},
-}) => {
+export interface ResourceMetadataCardProps {
+  name?: string;
+  createdAt: string;
+  updatedAt: string;
+  constrainedBy: string;
+}
+
+const ResourceMetadataCard: React.FunctionComponent<
+  ResourceMetadataCardProps
+> = props => {
+  const { constrainedBy, name, createdAt, updatedAt } = props;
   return (
-    <div className="clickable-container resource-item" onClick={onClick}>
-      <div className="name">
-        <em>{name}</em>
+    <Card>
+      <Meta
+        title={
+          <div
+            className="name"
+            style={{ display: 'flex', justifyContent: 'space-between' }}
+          >
+            <em>{name}</em>
+            <span>{moment(createdAt).format('DD/MM/YYYY')}</span>
+          </div>
+        }
+        description={
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <div>last updated {moment(updatedAt).fromNow()}</div>
+            <div>
+              constrained by <b>{constrainedBy}</b>
+            </div>
+          </div>
+        }
+      />
+    </Card>
+  );
+};
+
+const ResourceListItem: React.FunctionComponent<ResourceItemProps> = props => {
+  const { type, name, onClick = () => {} } = props;
+  return (
+    <Popover content={<ResourceMetadataCard {...props} />}>
+      <div className="clickable-container resource-item" onClick={onClick}>
+        <div className="name">
+          <em>{name}</em>
+        </div>
+        {type && type.length && <TypesIcon type={type} />}
       </div>
-      {/* <div className="schema">{constrainedBy}</div> */}
-      {type && type.length && <TypesIcon type={type} />}
-    </div>
+    </Popover>
   );
 };
 
