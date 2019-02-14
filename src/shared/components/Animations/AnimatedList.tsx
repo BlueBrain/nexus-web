b5998aa118f20a9587d0f0edfd6e049f0b5775a8
kenjinp <github@kenny.wtf>
2019-02-14 17:46:31 +0100

    add side menu to workspace

--- src/shared/components/Animations/AnimatedList.tsx
+++ src/shared/components/Animations/AnimatedList.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
 import { Spin, Pagination, Empty, Divider } from 'antd';
 import './AnimatedList.less';
-import { Transition, config, animated } from 'react-spring';
+import { useTransition, animated } from 'react-spring';
 
-const DEFAULT_TRAIL_MS = 100;
+const DEFAULT_TRAIL_MS = 50;
 
 const DEFAULT_ANIMATIONS = {
   from: { transform: 'scale3d(0.8, 0.8, 0.8)', height: 0, opacity: 0 },
   leave: { transform: 'scale3d(0.8, 0.8, 0.8)', height: 0, opacity: 0 },
-  enter: { transform: 'scale3d(1, 1, 1)', height: 117, opacity: 1 },
+  enter: { transform: 'scale3d(1, 1, 1)', height: 'auto', opacity: 1 },
+  trail: DEFAULT_TRAIL_MS,
 };
 
 export interface AnimatedListProps<Item> {
   header?: React.ReactNode;
-  itemComponent: (item: Item, index: number, state: any) => React.ReactNode;
+  itemComponent: (item: Item, index: number) => React.ReactNode;
   itemName?: string;
+  itemClassName?: string;
   results: Item[];
   total: number;
   makeKey?: (item: Item) => string;
@@ -27,6 +29,7 @@ const AnimatedList: React.FunctionComponent<AnimatedListProps<any>> = props => {
   const {
     makeKey = (item: any) => item.id,
     itemComponent,
+    itemClassName,
     header = <div />,
     loading = false,
     itemName = 'Item',
@@ -50,7 +53,7 @@ const AnimatedList: React.FunctionComponent<AnimatedListProps<any>> = props => {
       />
     );
   }
-
+  const transitions = useTransition(results, makeKey, DEFAULT_ANIMATIONS);
   return (
     <div className="list-container">
       <div className="header">
@@ -64,21 +67,12 @@ const AnimatedList: React.FunctionComponent<AnimatedListProps<any>> = props => {
       </div>
       <div className="list">
         <Spin spinning={loading}>
-          {!!total && (
-            <Transition
-              trail={DEFAULT_TRAIL_MS}
-              items={results}
-              keys={makeKey}
-              {...DEFAULT_ANIMATIONS}
-              native={true}
-            >
-              {(item: any, state: any, index: number) => props => (
-                <animated.div style={props}>
-                  {itemComponent(item, index, state)}
-                </animated.div>
-              )}
-            </Transition>
-          )}
+          {!!total &&
+            transitions.map(({ item, key, props }, index: number) => (
+              <animated.div className={itemClassName} style={props} key={key}>
+                {itemComponent(item, index)}
+              </animated.div>
+            ))}
           {!total && <Empty />}
           {PaginationSection}
         </Spin>
---
+++ src/shared/components/Workspace/SideMenu.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { Icon, Card } from 'antd';
+import './SideMenu.less';
+import { useTransition, animated } from 'react-spring';
+
+const DEFAULT_ANIMATIONS = {
+  from: () => ({ right: -300, opacity: 0 }),
+  leave: () => ({ right: -300, opacity: 0 }),
+  enter: () => ({ right: 0, opacity: 1 }),
+};
+
+export interface SideMenuProps {
+  title?: string;
+  visible: boolean;
+  onClose: () => void;
+}
+
+const SideMenu: React.FunctionComponent<SideMenuProps> = props => {
+  const { title, children, visible, onClose } = props;
+  const transitions = useTransition(visible, null, DEFAULT_ANIMATIONS);
+  return (
+    <>
+      {transitions.map(
+        ({ item, key, props }) =>
+          item && (
+            <animated.div className="side-menu" style={props} key={key}>
+              <Card
+                title={
+                  <div className="header">
+                    {!!title && <div className="ant-drawer-title">{title}</div>}
+                    <button
+                      className="ant-drawer-close"
+                      aria-label="Close"
+                      onClick={onClose}
+                    >
+                      <Icon type="close" />
+                    </button>
+                  </div>
+                }
+              >
+                <div className="content">{children}</div>
+              </Card>
+            </animated.div>
+          )
+      )}
+    </>
+  );
+};
+
+export default SideMenu;
