33581c9115e0e9a0726a24604870cfbcfaec891f
kenjinp <github@kenny.wtf>
2019-02-18 17:17:31 +0100

    remove dead code

--- src/shared/components/Types/TypesIcon.tsx
+++ src/shared/components/Types/TypesIcon.tsx
@@ -4,16 +4,6 @@ import * as Identicon from 'identicon.js';
 import './Types.less';
 import * as md5 from 'md5';
 
-// https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
-function componentToHex(component: number) {
-  const hex = component.toString(16);
-  return hex.length === 1 ? `0${hex}` : hex;
-}
-
-function rgbToHex(r: number, g: number, b: number) {
-  return `#${componentToHex(r)}${componentToHex(g)}${componentToHex(b)}`;
-}
-
 export interface TypesIconProps {
   type: string;
 }
