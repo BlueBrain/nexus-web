2214617e9367d767671f6ec85b0f131f04ba7450
kenjinp <github@kenny.wtf>
2019-02-18 11:03:40 +0100

    simple json editor with validation

--- src/shared/components/Resources/ResourceEditor.tsx
+++ src/shared/components/Resources/ResourceEditor.tsx
@@ -1,63 +1,36 @@
 import * as React from 'react';
-import { Resource } from '@bbp/nexus-sdk';
-import { Spin, Button, notification, Icon } from 'antd';
+import { Button, Icon } from 'antd';
 import { UnControlled as CodeMirror } from 'react-codemirror2';
 import './ResourceEditor.less';
 
+// Codemirror will not load on the server, so we need to make sure
+// the language support code doesn't load either.
 if (typeof window !== 'undefined') {
   require('codemirror/mode/javascript/javascript');
 }
 
 export interface ResourceEditorProps {
-  resource: Resource | null;
-  error: Error | null;
-  isFetching: boolean | false;
-  onSuccess: VoidFunction;
+  rawData: { [key: string]: any }; // any object
+  onSubmit: (rawData: { [key: string]: any }) => void;
 }
 
 const ResourceEditor: React.FunctionComponent<ResourceEditorProps> = props => {
-  const { resource, error, isFetching, onSuccess } = props;
+  const { rawData, onSubmit } = props;
   const [editing, setEditing] = React.useState(false);
-  const [busy, setFormBusy] = React.useState(isFetching);
   const [valid, setValid] = React.useState(true);
   const [value, setValue] = React.useState(
-    resource && {
-      context: resource.context,
-      type: resource.type,
-      ...resource.data,
-    }
+    rawData
+    // resource && {
+    //   context: resource.context,
+    //   type: resource.type,
+    //   ...resource.data,
+    // }
   );
-
-  const handleSubmit = async () => {
-    if (resource) {
-      try {
-        setFormBusy(true);
-        await resource.update({
-          context: resource.context,
-          ...value,
-        });
-        notification.success({
-          message: 'Resource saved',
-          description: resource.name,
-          duration: 2,
-        });
-        onSuccess();
-        setFormBusy(false);
-        setEditing(false);
-      } catch (error) {
-        notification.error({
-          message: 'An unknown error occurred',
-          description: error.message,
-          duration: 0,
-        });
-        setFormBusy(false);
-      }
-    }
-  };
 
   const handleChange = (editor: any, data: any, value: any) => {
     try {
-      setValue(JSON.parse(value));
+      const parsedVal = JSON.parse(value);
+      setValue(parsedVal);
       setEditing(true);
       setValid(true);
     } catch (error) {
@@ -65,64 +38,65 @@ const ResourceEditor: React.FunctionComponent<ResourceEditorProps> = props => {
     }
   };
 
-  return (
-    <div className="resource" style={{ width: '100%' }}>
-      <Spin spinning={busy} style={{ width: '100%' }}>
-        {!!resource && (
-          <div
-            className={valid ? 'resource-editor' : 'resource-editor _invalid'}
-          >
-            <div className="control-panel">
-              <div>
-                {!editing && (
-                  <div className="feedback">Directly edit this resource</div>
-                )}
-                {editing && valid && (
-                  <div className="feedback _positive">
-                    <Icon type="check-circle" /> Valid
-                  </div>
-                )}
-                {editing && !valid && (
-                  <div className="feedback _negative">
-                    <Icon type="exclamation-circle" /> Invalid JSON-LD
-                  </div>
-                )}
-              </div>
-              <div className="controls">
-                {editing && valid && (
-                  <Button
-                    icon="save"
-                    type="primary"
-                    size="small"
-                    onClick={handleSubmit}
-                  >
-                    Save
-                  </Button>
-                )}
-              </div>
-            </div>
+  const handleSubmit = () => {
+    if (onSubmit) {
+      onSubmit(value);
+    }
+  };
 
-            <CodeMirror
-              value={JSON.stringify(
-                {
-                  '@context': resource.context,
-                  '@type': resource.type,
-                  ...resource.data,
-                },
-                null,
-                2
-              )}
-              options={{
-                mode: { name: 'javascript', json: true },
-                theme: 'base16-light',
-                lineNumbers: true,
-                viewportMargin: Infinity,
-              }}
-              onChange={handleChange}
-            />
-          </div>
-        )}
-      </Spin>
+  return (
+    <div className={valid ? 'resource-editor' : 'resource-editor _invalid'}>
+      <div className="control-panel">
+        <div>
+          {!editing && (
+            <div className="feedback">Directly edit this resource</div>
+          )}
+          {editing && valid && (
+            <div className="feedback _positive">
+              <Icon type="check-circle" /> Valid
+            </div>
+          )}
+          {editing && !valid && (
+            <div className="feedback _negative">
+              <Icon type="exclamation-circle" /> Invalid JSON-LD
+            </div>
+          )}
+        </div>
+        <div className="controls">
+          {editing && valid && (
+            <Button
+              icon="save"
+              type="primary"
+              size="small"
+              onClick={handleSubmit}
+            >
+              Save
+            </Button>
+          )}
+        </div>
+      </div>
+
+      <CodeMirror
+        value={
+          JSON.stringify(rawData, null, 2)
+          //   JSON.stringify(
+          //   {
+          //     '@context': resource.context,
+          //     '@type': resource.type,
+          //     ...resource.data,
+          //   },
+          //   null,
+          //   2
+          // )
+        }
+        options={{
+          mode: { name: 'javascript', json: true },
+          theme: 'base16-light',
+          lineNumbers: true,
+          viewportMargin: Infinity,
+        }}
+        onChange={handleChange}
+      />
     </div>
   );
 };
