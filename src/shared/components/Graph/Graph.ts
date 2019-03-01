cff47ee4782786ba638641984d01013335cc60b7
kenjinp <github@kenny.wtf>
2019-03-01 15:33:04 +0100

    remove console

--- src/shared/components/Graph/Graph.ts
+++ src/shared/components/Graph/Graph.ts
@@ -21,8 +21,6 @@ export class Graph {
     try {
       this.destroy = graph(this.dotGraph, this.element);
     } catch (error) {
-      // tslint:disable-next-line:no-console
-      console.error(error);
       this.renderError = error;
     }
   }
