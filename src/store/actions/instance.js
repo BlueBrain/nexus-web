5308e6c88c5a7c8c6373b1d4cb3d90c89e2d254e
Kenneth Pirman <github@kenny.wtf>
2019-03-15 14:53:12 +0100

    fix: [Nexus-Explorer] force login if viewing resource without access (#171)
    * fix: force login if viewing resoruce without access
    
    * remove unsused var

--- src/store/actions/instance.js
+++ src/store/actions/instance.js
@@ -1,80 +1,92 @@
-import { getInstance } from '@bbp/nexus-js-helpers';
-import * as types from './types';
+import { getInstance } from "@bbp/nexus-js-helpers";
+import * as types from "./types";
 
 export default {
   fetchInstance,
   fetchInstanceStarted,
   fetchInstanceFulfilled,
-  fetchInstanceFailed,
-}
+  fetchInstanceFailed
+};
 
-function fetchInstance () {
+function fetchInstance() {
   return (dispatch, getState) => {
-    let state = getState()
-    const { org, domain, schema, ver, instance } = state.pick
-    const { api } = state.config
-    const { token } = state.auth
-    dispatch(fetchInstanceStarted())
+    let state = getState();
+    const {
+      pick: { org, domain, schema, ver, instance },
+      config: { loginURI }
+    } = state;
+    const { api } = state.config;
+    const { token } = state.auth;
+    dispatch(fetchInstanceStarted());
     return getInstance([org, domain, schema, ver, instance], {}, api, token)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(`Encountered HTTP error ${response.status}. Instance info is not available.`);
-    })
-    .then(instance => {
-      if (instance === undefined) {
-        throw new Error('Instance is not defined');
-      }
-      dispatch(fetchInstanceFulfilled(instance))
-      return fetchIncomingOutgoing(instance.links, token);
-    })
-    .then(([incoming, outgoing]) => {
-      dispatch(instanceLinksAdded({ incoming: incoming.results, outgoing: outgoing.results }));
-    })
-    .catch(error => {
-      console.error(error);
-      dispatch(fetchInstanceFailed(error))
-    });
-  }
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error(
+          `Encountered HTTP error ${
+            response.status
+          }. Instance info is not available.`
+        );
+      })
+      .then(instance => {
+        if (instance === undefined) {
+          throw new Error("Instance is not defined");
+        }
+        dispatch(fetchInstanceFulfilled(instance));
+        return fetchIncomingOutgoing(instance.links, token);
+      })
+      .then(([incoming, outgoing]) => {
+        dispatch(
+          instanceLinksAdded({
+            incoming: incoming.results,
+            outgoing: outgoing.results
+          })
+        );
+      })
+      .catch(error => {
+        console.error(error);
+        if (error.message.includes("401")) {
+          window.location = loginURI;
+        }
+        return dispatch(fetchInstanceFailed(error));
+      });
+  };
 }
 
 function fetchIncomingOutgoing(links, access_token) {
-  const requestOptions = access_token? { headers: { "Authorization": "Bearer "+ access_token } }: {};
+  const requestOptions = access_token
+    ? { headers: { Authorization: "Bearer " + access_token } }
+    : {};
   return Promise.all([
-    fetch(links['incoming'] + '?fields=all', requestOptions),
-    fetch(links['outgoing'] + '?fields=all', requestOptions),
-  ])
-  .then(responses => Promise.all([
-      responses[0].json(),
-      responses[1].json()
-    ])
-  )
+    fetch(links["incoming"] + "?fields=all", requestOptions),
+    fetch(links["outgoing"] + "?fields=all", requestOptions)
+  ]).then(responses => Promise.all([responses[0].json(), responses[1].json()]));
 }
 
-function fetchInstanceStarted () {
+function fetchInstanceStarted() {
   return {
-    type: types.FETCH_INSTANCE_STARTED,
-  }
+    type: types.FETCH_INSTANCE_STARTED
+  };
 }
 
 function fetchInstanceFulfilled(data) {
   return {
     type: types.FETCH_INSTANCE_FULFILLED,
     payload: data
-  }
+  };
 }
 
-function fetchInstanceFailed (error) {
+function fetchInstanceFailed(error) {
   return {
     type: types.FETCH_INSTANCE_FAILED,
     error: error
-  }
+  };
 }
 
 function instanceLinksAdded(data) {
   return {
     type: types.INSTANCE_LINKS_ADDED,
     payload: data
-  }
+  };
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,32 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import { routerReducer, routerMiddleware } from 'react-router-redux';
-import persistState from 'redux-localstorage';
-import thunk from 'redux-thunk'
-import history from '../libs/history';
-import * as customReducers from './reducers';
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { routerReducer, routerMiddleware } from "react-router-redux";
+import persistState from "redux-localstorage";
+import thunk from "redux-thunk";
+import history from "../libs/history";
+import * as customReducers from "./reducers";
+
+let composeEnhancers = compose;
+try {
+  // DEVELOPMENT ONLY
+  // if the Redux Devtools browser extension is present,
+  // enable its debugging capabilities.
+  if (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) {
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+  }
+} catch (e) {
+  composeEnhancers = compose;
+}
 
 const reducers = combineReducers({
   ...customReducers,
   routing: routerReducer
-})
+});
 
-const middleware = [ thunk, routerMiddleware(history) ]
+const middleware = [thunk, routerMiddleware(history)];
 
 const reduxStore = createStore(
   reducers,
-  compose(
-    persistState('auth'),
-    applyMiddleware(
-        ...middleware
-    )
-  )
-)
+  composeEnhancers(persistState("auth"), applyMiddleware(...middleware))
+);
 
 export default reduxStore;
