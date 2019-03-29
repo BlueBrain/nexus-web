bd506b4ae36feb7b7cdbe3598662887c64d94552
Kenneth Pirman <github@kenny.wtf>
2019-03-29 14:56:19 +0100

    feat: Infinite scroll (#189)
    * initial commit
    
    * basic setup
    
    * testing hooks with storybook
    
    * beautiful simulated list
    
    * smooter feeling load more button
    
    * nice loading transition
    
    * scroll bar styling
    
    * make sure event handlers are updated
    
    * remove commented styles
    
    * load at percent revealed && end of list message
    
    * rename next to LoadNextPage for clarity

---
+++ src/shared/components/Animations/InfiniteScroll.tsx
@@ -0,0 +1,138 @@
+import * as React from 'react';
+import { Empty } from 'antd';
+import './infinite-scroll.less';
+import './list-item.less';
+import { useTransition, animated, useSpring, config } from 'react-spring';
+import { PaginatedList } from '@bbp/nexus-sdk';
+import { FetchableState } from '../../store/reducers/utils';
+import useInfiniteScroll from '../hooks/useInfiniteScroll';
+
+const DEFAULT_TRAIL_MS = 50;
+
+const DEFAULT_ANIMATIONS = {
+  from: { transform: 'scale3d(0.8, 0.8, 0.8)', height: 0, opacity: 0 },
+  leave: { transform: 'scale3d(0.8, 0.8, 0.8)', height: 0, opacity: 0 },
+  enter: { transform: 'scale3d(1, 1, 1)', height: 'auto', opacity: 1 },
+  trail: DEFAULT_TRAIL_MS,
+};
+
+export const InfiniteScrollLoadMoreButton: React.FunctionComponent<{
+  hasMore: boolean;
+  totalItemsListLength: number;
+  onClick: VoidFunction;
+  isFetching: boolean;
+}> = ({ hasMore, totalItemsListLength, onClick, isFetching }) => {
+  const animatedOpacity = useSpring({
+    opacity: hasMore ? 1 : 0,
+    // TODO why: This doesn't work as described https://www.react-spring.io/docs/hooks/api
+    config: config.molasses,
+    delay: DEFAULT_TRAIL_MS * totalItemsListLength,
+  });
+  const animatedLoadTransition = useTransition(isFetching, null, {
+    from: { position: 'absolute', opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+  return (
+    <animated.div style={animatedOpacity}>
+      <a onClick={onClick}>
+        <li className="list-item -action -load">
+          <div className="loading">
+            {animatedLoadTransition.map(({ item, props }) =>
+              item ? (
+                <animated.div style={props} className="center-helper">
+                  <div className="center">Loading</div>
+                </animated.div>
+              ) : (
+                <animated.div style={props} className="center-helper">
+                  <div className="center">Load More</div>
+                </animated.div>
+              )
+            )}
+          </div>
+        </li>
+      </a>
+    </animated.div>
+  );
+};
+
+export interface InfiniteScrollProps {
+  itemComponent: (item: any, index: number) => React.ReactElement;
+  itemClassName?: string;
+  makeKey: (item: any, index: number) => string;
+  loadNextPage: VoidFunction;
+  style?: React.CSSProperties;
+  fetchablePaginatedList: FetchableState<PaginatedList<any>>;
+  loadAtPercentRevealed?: number;
+}
+
+const LOAD_AT_PERCENTAGE_REVEALED = 0.8;
+
+const InfiniteScroll: React.FunctionComponent<InfiniteScrollProps> = props => {
+  const {
+    makeKey,
+    itemClassName,
+    itemComponent,
+    fetchablePaginatedList,
+    loadNextPage,
+    style,
+    loadAtPercentRevealed = LOAD_AT_PERCENTAGE_REVEALED,
+  } = props;
+  const { isFetching, data, error } = fetchablePaginatedList;
+  const [totalItems, setTotalItems] = React.useState<number>(0);
+  const [totalItemsList, setTotalItemsList] = React.useState<any[]>([]);
+  const [bind] = useInfiniteScroll(
+    loadNextPage,
+    isFetching,
+    loadAtPercentRevealed
+  );
+  React.useEffect(() => {
+    if (data) {
+      setTotalItemsList([...totalItemsList, ...data.results]);
+      setTotalItems(data.total);
+    }
+  }, [data && data.index]);
+  const hasMore = totalItemsList.length < totalItems;
+  const keys = totalItemsList.map(makeKey);
+  const transitions = useTransition(totalItemsList, keys, {
+    ...DEFAULT_ANIMATIONS,
+    unique: true,
+  });
+  return (
+    <div {...bind} className="infinite-scroll" style={style}>
+      {!!error && <Empty description={error.message} />}
+      {!error && (
+        <ul className="list">
+          {transitions.map(({ item, props }, index: number) => {
+            return (
+              <animated.div
+                className={itemClassName}
+                style={props}
+                key={keys[index]}
+              >
+                {itemComponent(item, index)}
+              </animated.div>
+            );
+          })}
+          {!data || (!data.total && <Empty />)}
+          {hasMore && (
+            <InfiniteScrollLoadMoreButton
+              key="loading-action"
+              isFetching={isFetching}
+              hasMore={hasMore}
+              totalItemsListLength={totalItemsList.length}
+              onClick={loadNextPage}
+            />
+          )}
+          {!!totalItems && !isFetching && !hasMore && (
+            <li className="list-item -action -end-of-list">
+              You've reached the end of this list
+            </li>
+          )}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default InfiniteScroll;
---
+++ src/shared/components/hooks/useInfiniteScroll.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { isBrowser } from '../../utils';
+
+// Watch an when a scrolling element reaches the bottom, then callback!
+const useInfiniteScroll = (
+  callback: VoidFunction,
+  isFetching: boolean,
+  loadAtPercentRevealed: number
+) => {
+  const ref = React.useRef<HTMLDivElement>(null);
+  if (!isBrowser) {
+    return [{ ref }];
+  }
+
+  React.useEffect(() => {
+    if (ref && ref.current) {
+      ref.current.addEventListener('scroll', handleScroll);
+    }
+    return () => {
+      if (ref && ref.current) {
+        ref.current.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, [ref, isFetching]);
+
+  const handleScroll = () => {
+    if (!ref || !ref.current) {
+      return;
+    }
+    if (
+      !isFetching &&
+      ref.current.offsetHeight + ref.current.scrollTop >=
+        ref.current.scrollHeight * loadAtPercentRevealed
+    ) {
+      callback();
+    }
+  };
+
+  return [{ ref }];
+};
+
+export default useInfiniteScroll;
