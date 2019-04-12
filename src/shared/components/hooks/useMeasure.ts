6b0fea613bc2fae9e9d3a07c05c2b71245dc6d0a
Kenneth Pirman <github@kenny.wtf>
2019-04-12 11:12:55 +0200

    feat: Routed menus (#200)
    * feat: route menu component
    
    * fix: ts checking and console

---
+++ src/shared/components/hooks/useMeasure.ts
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import ResizeObserver from 'resize-observer-polyfill';
+
+interface Bounds {
+  left: number;
+  top: number;
+  width: number;
+  height: number;
+}
+
+export default function useMeasure() {
+  const ref = React.useRef<HTMLElement>();
+  const [bounds, set] = React.useState<Bounds>({
+    left: 0,
+    top: 0,
+    width: 0,
+    height: 0,
+  });
+  const [ro] = React.useState(
+    () => new ResizeObserver(([entry]) => set(entry.contentRect))
+  );
+  React.useEffect(() => {
+    if (ref && ref.current) {
+      ro.observe(ref.current);
+      const { height, width, top, left } = ref.current.getBoundingClientRect();
+      set({ height, width, top, left });
+    }
+    return ro.disconnect;
+  }, [ref]);
+  return [{ ref }, bounds] as [
+    { ref: React.MutableRefObject<HTMLElement> },
+    Bounds
+  ];
+}
