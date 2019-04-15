9a58f9704ee085c86b377ce58c1daa99ba9e8ccf
kenjinp <github@kenny.wtf>
2019-04-15 09:33:36 +0200

    first pass workspace menu

---
+++ src/shared/components/Header/WorkspaceSelectorMenu/WorkspaceSelectorMenuButton.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Button } from 'antd';
+import './Header.less';
+
+export interface WorkspaceSelectorMenuButtonProps {
+}
+
+const WorkspaceSelectorMenuButton: React.FunctionComponent<WorkspaceSelectorMenuButtonProps> = ({
+}) => {
+
+  return (
+  );
+};
+
+export default WorkspaceSelectorMenuButton;
