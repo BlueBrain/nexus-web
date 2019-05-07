ca756a909d02375f84e17f90099d1c34cfc4b2d1
Kenneth Pirman <github@kenny.wtf>
2019-05-07 09:15:56 +0200

    👷‍♀️FIX: Resource/ File links (#216)
    * use same QueryListItem and fix undefined function in links
    
    * remove unused code
    
    * no more animated lists on links page
    
    * bump sdk version
    
    * allow copying inside uneditable fields

--- src/shared/components/Resources/Links.tsx
+++ src/shared/components/Resources/Links.tsx
@@ -1,15 +1,22 @@
 import * as React from 'react';
-import { Resource, PaginationSettings, NexusFile } from '@bbp/nexus-sdk';
+import {
+  Resource,
+  PaginationSettings,
+  NexusFile,
+  PaginatedList,
+} from '@bbp/nexus-sdk';
 import { Spin, Card, Icon } from 'antd';
 
 import AnimatedList from '../Animations/AnimatedList';
 import { ResourceLink } from '@bbp/nexus-sdk/lib/Resource/types';
-import ResourceListItem from './ResourceItem';
 import { labelOf } from '../../utils';
-import { LinksState } from '../../store/reducers/links';
 
 import './links-container.less';
 import { LinkDirection } from '../../store/actions/nexus/links';
+import QueryListItem from '../Workspace/Queries/Query/QueryItem';
+import { LinksState } from '../../store/reducers/links';
+import InfiniteScroll from '../Animations/InfiniteScroll';
+import { FetchableState } from '../../store/reducers/utils';
 
 export interface LinksListProps extends LinksContainerProps {
   linkDirection: LinkDirection;
@@ -29,71 +36,83 @@ const LinksList: React.FunctionComponent<LinksListProps> = props => {
   const from = (linkState && linkState.data && linkState.data.index) || 0;
   const total = (linkState && linkState.data && linkState.data.total) || 0;
   const paginationSettings = { from, total, size: linksListPageSize };
+  const fetchablePaginatedList: FetchableState<PaginatedList<ResourceLink>> = {
+    error: (linkState && linkState.error) || null,
+    isFetching: (linkState && linkState.isFetching) || false,
+    data: (linkState && linkState.data) || {
+      index: 0,
+      total: 0,
+      results: [],
+    },
+  };
 
   React.useEffect(() => {
     fetchLinks(resource, linkDirection, paginationSettings);
   }, [resource]);
 
+  const next = () => {
+    fetchLinks(resource, linkDirection, {
+      ...paginationSettings,
+      from:
+        ((fetchablePaginatedList.data && fetchablePaginatedList.data.index) ||
+          0) +
+        1 * paginationSettings.size,
+    });
+  };
+
   return (
-    <AnimatedList
-      header={
-        <h3 className="title">
-          {linkDirection === LinkDirection.INCOMING ? (
-            <Icon type="download" />
-          ) : (
-            <Icon type="upload" />
-          )}{' '}
-          {linkDirection} Links
-        </h3>
-      }
-      loading={linkState ? linkState.isFetching : true}
-      makeKey={(item: ResourceLink, index: number) =>
-        `${resource.id}-${
-          item.isExternal ? (item.link as string) : (item.link as Resource).id
-        }-${index}-${linkDirection}`
-      }
-      results={(linkState && linkState.data && linkState.data.results) || []}
-      total={(linkState && linkState.data && linkState.data.total) || 0}
-      paginationSettings={{ from, total, pageSize: linksListPageSize }}
-      onPaginationChange={(page: number, pageSize?: number) => {
-        // NOTE: page begins from 1, not 0.
-        // from is the total number of resources beggining from 0, not the page number!
-        const size = pageSize || linksListPageSize;
-        fetchLinks(resource, linkDirection, {
-          size,
-          from: page * size - size,
-        });
-      }}
-      itemComponent={(resourceLink: ResourceLink, index: number) => {
-        const predicate = labelOf(resourceLink.predicate);
-        return (
-          <>
-            {resourceLink.isExternal ? (
-              <a
-                className="clickable-container resource-item"
-                href={resourceLink.link as string}
-                target="_blank"
-              >
-                <div className="predicate">{predicate}</div>
-                <div className="label">
-                  <div className="name">
-                    <Icon type="export" /> {resourceLink.link as string}
+    <div className="links">
+      <h3 className="title">
+        {linkDirection === LinkDirection.INCOMING ? (
+          <Icon type="download" />
+        ) : (
+          <Icon type="upload" />
+        )}{' '}
+        {linkDirection} Links
+      </h3>
+      <InfiniteScroll
+        fetchablePaginatedList={fetchablePaginatedList}
+        loadNextPage={next}
+        itemComponent={(resourceLink: ResourceLink, index: number) => {
+          const predicate = labelOf(resourceLink.predicate);
+          return (
+            <>
+              {resourceLink.isExternal ? (
+                <a
+                  className="clickable-container resource-item"
+                  href={resourceLink.link as string}
+                  target="_blank"
+                >
+                  <div className="predicate">{predicate}</div>
+                  <div className="label">
+                    <div className="name">
+                      <Icon type="export" /> {resourceLink.link as string}
+                    </div>
                   </div>
-                </div>
-              </a>
-            ) : (
-              <ResourceListItem
-                getFilePreview={getFilePreview}
-                predicate={predicate}
-                index={index}
-                resource={resourceLink.link as Resource}
-                onClick={() => goToResource(resourceLink.link as Resource)}
-              />
-            )}
-          </>
-        );
-      }}
-    />
+                </a>
+              ) : (
+                <QueryListItem
+                  predicate={
+                    linkDirection === LinkDirection.INCOMING ? (
+                      <>
+                        <Icon type="arrow-right" /> {predicate}
+                      </>
+                    ) : (
+                      <>
+                        {predicate} <Icon type="arrow-right" />
+                      </>
+                    )
+                  }
+                  resource={resourceLink.link as Resource}
+                  getFilePreview={getFilePreview}
+                  onClick={() => goToResource(resourceLink.link as Resource)}
+                />
+              )}
+            </>
+          );
+        }}
+      />
+    </div>
   );
 };
 
