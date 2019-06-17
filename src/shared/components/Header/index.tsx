8414721426468605e13045161ff1e9d19ead1651
Julien Machon <julienmachon@gmail.com>
2019-06-17 16:59:29 +0200

    Footer bbp and epfl logo (#255)
    * remove footer and add nice little popover available from header
    
    * copyright at the end
    
    * update snapshot
    
    * update header tests

--- src/shared/components/Header/index.tsx
+++ src/shared/components/Header/index.tsx
@@ -1,11 +1,46 @@
 import * as React from 'react';
-import { Menu, Dropdown, Icon, Button } from 'antd';
+import { Menu, Dropdown, Icon, Button, Popover } from 'antd';
 import './Header.less';
 import Copy from '../Copy';
 
 const logo = require('../../logo.svg');
+const epflLogo = require('../../EPFL-logo.svg');
 
+interface InformationContentProps {
+  version: string;
+  githubIssueURL: string;
+}
+
+const InformationContent = (props: InformationContentProps) => {
+  return (
+    <>
+      <p>Nexus is Open Source and available under the Apache 2 License. </p>
+      <p>
+        v{props.version}{' '}
+        <a href={props.githubIssueURL} target="_blank">
+          <Icon type="github" /> post an issue on github
+        </a>
+      </p>
+      <p>
+        © 2017-2019
+        <a href="https://www.epfl.ch/" target="_blank">
+          <img
+            style={{ width: '3em', marginBottom: 3 }}
+            src={epflLogo}
+            alt="EPFL"
+          />
+        </a>
+        {'| '}
+        <a href="https://bluebrain.epfl.ch/" target="_blank">
+          <span className="bbp-logo">Blue Brain Project</span>
+        </a>
+      </p>
+    </>
+  );
+};
 export interface HeaderProps {
+  version: string;
+  githubIssueURL: string;
   name?: string;
   token?: string;
   links?: React.ReactNode[];
@@ -21,6 +56,8 @@ const Header: React.FunctionComponent<HeaderProps> = ({
   links = [],
   children,
   onLoginClick,
+  version,
+  githubIssueURL,
 }) => {
   const menu = (
     <Menu>
@@ -54,6 +91,19 @@ const Header: React.FunctionComponent<HeaderProps> = ({
             )}
           />
         )}
+        <Popover
+          content={
+            <InformationContent
+              version={version}
+              githubIssueURL={githubIssueURL}
+            />
+          }
+          trigger="click"
+          title="Information"
+          placement="bottomRight"
+        >
+          <Button size="small" icon="info-circle" />
+        </Popover>
         {name ? (
           <Dropdown overlay={menu}>
             <a className="menu-dropdown ant-dropdown-link">
