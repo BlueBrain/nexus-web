5f99389df28e87b4bebe780a7abbe10504664fee
Kenneth Pirman <github@kenny.wtf>
2019-06-17 16:57:49 +0200

    fix-image-detection (#254)

--- src/shared/components/hooks/useNexusImage.ts
+++ src/shared/components/hooks/useNexusImage.ts
@@ -5,13 +5,32 @@ import useNexusFile from './useNexusFile';
 // Don't download preview if filesize is > than 1MB
 const DEFAULT_DISPLAY_SIZE = 1e6;
 
+function includesAny(s: string, listOfStrings: string[]) {
+  return listOfStrings.reduce(
+    (memo, compareString) => memo || s.includes(compareString),
+    false
+  );
+}
+
 export function hasDisplayableImage(resource: Resource): boolean {
+  // Is a File
+  // has mediaType Image
+  // OR
+  // Includes any of these image names in the filename
   return (
-    resource.type &&
-    resource.type.includes('File') &&
-    (resource.data as any)['_mediaType'] &&
-    (resource.data as any)['_mediaType'].includes('image') &&
-    (resource.data as any)['_bytes'] <= DEFAULT_DISPLAY_SIZE
+    (resource.type &&
+      resource.type.includes('File') &&
+      ((resource.data as any)['_mediaType'] &&
+        (resource.data as any)['_mediaType'].includes('image'))) ||
+    (includesAny((resource.data as any)['_filename'], [
+      'tiff',
+      'tif',
+      'jpeg',
+      'jpg',
+      'png',
+      'svg',
+    ]) &&
+      (resource.data as any)['_bytes'] <= DEFAULT_DISPLAY_SIZE)
   );
 }
 
