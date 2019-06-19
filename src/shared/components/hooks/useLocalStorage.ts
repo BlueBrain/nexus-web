825019a1ff508fb423003bef7a4ea0e547ade00c
kenjinp <github@kenny.wtf>
2019-06-19 14:13:49 +0200

    remove logs

--- src/shared/components/hooks/useLocalStorage.ts
+++ src/shared/components/hooks/useLocalStorage.ts
@@ -7,13 +7,11 @@ const useLocalStorage = (key: string) => {
       setValue(JSON.parse(localStorage.getItem(key) || '{}'));
       return;
     }
-    console.warn('no localStorage found');
   }, []);
   const setLocalStorageValue = (value: any) => {
     if (localStorage) {
       return localStorage.setItem(key, JSON.stringify(value));
     }
-    console.warn('no localStorage found');
   };
   return [value, setLocalStorageValue];
 };
