3c9b0164030ae52ca39ecb852d342efd089ba7e0
Kenneth Pirman <github@kenny.wtf>
2019-06-27 14:26:21 +0200

    fix: Org Item improvements (#279)
    * remove dropdown
    
    * update org card to use list item, rename org card to org item
    
    * update stories and orgsList
    
    * removed unecessary styling
    
    * rename numProjects to numberOfProjects

---
+++ src/shared/components/Orgs/OrgItem.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import { Button } from 'antd';
+import { AccessControl } from '@bbp/react-nexus';
+import ListItem from '../Animations/ListItem';
+
+export interface OrgItemProps {
+  label: string;
+  description?: string;
+  numberOfProjects?: number;
+  onClick?(): void;
+  onEdit?(): void;
+}
+
+const OrgItem: React.FunctionComponent<OrgItemProps> = ({
+  label,
+  description,
+  numberOfProjects,
+  onClick = () => {},
+  onEdit = () => {},
+}) => {
+  return (
+    <ListItem
+      onClick={onClick}
+      key={label}
+      id={label}
+      label={label}
+      description={description}
+      details={
+        !!numberOfProjects ? (
+          <p className="project-number">
+            <span className="number">{numberOfProjects}</span> project
+            {numberOfProjects > 1 && 's'}
+          </p>
+        ) : null
+      }
+      action={
+        onEdit ? (
+          <AccessControl
+            path={`/${label}`}
+            permissions={['organizations/write']}
+          >
+            <Button
+              className="edit-button"
+              type="primary"
+              tabIndex={1}
+              onClick={(e: React.SyntheticEvent) => {
+                e.stopPropagation();
+                onEdit();
+              }}
+            >
+              Edit
+            </Button>
+          </AccessControl>
+        ) : null
+      }
+    />
+  );
+};
+
+export default OrgItem;
--- src/shared/components/Orgs/Orgs.stories.tsx
+++ src/shared/components/Orgs/Orgs.stories.tsx
@@ -2,42 +2,31 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { action } from '@storybook/addon-actions';
-import {
-  withKnobs,
-  text,
-  number,
-  knob,
-  boolean,
-  select,
-} from '@storybook/addon-knobs';
+import { withKnobs, text, number } from '@storybook/addon-knobs';
 
 import OrgList from './OrgList';
-import OrgCard, { OrgCardProps } from './OrgCard';
-import OrgDropdown from './OrgDropdown';
+import OrgItem, { OrgItemProps } from './OrgItem';
 import OrgForm from './OrgForm';
-import Header from '../Header';
-import { type } from 'os';
 
-const logo = require('../../logo.svg');
-const orgs: OrgCardProps[] = [
-  { label: 'nexus', projectNumber: 1200 },
-  { label: 'bbp', projectNumber: 300 },
-  { label: 'hbp', projectNumber: 1 },
-  { label: 'nasa', projectNumber: 912839 },
-  { label: 'tesla', projectNumber: 3 },
-  { label: 'rolex', projectNumber: 3424 },
+const orgs: OrgItemProps[] = [
+  { label: 'nexus', numberOfProjects: 1200 },
+  { label: 'bbp', numberOfProjects: 300 },
+  { label: 'hbp', numberOfProjects: 1 },
+  { label: 'nasa', numberOfProjects: 912839 },
+  { label: 'tesla' },
+  { label: 'rolex', numberOfProjects: 3424 },
 ];
 
 storiesOf('Components/Orgs', module)
   .addDecorator(withKnobs)
   .add(
-    'OrgCard',
+    'OrgItem',
     withInfo(`
     The Organization Card component is used across multiple components.
     It displays the the basic Organization informations.
 
     ~~~js
-      <OrgCard />
+      <OrgItem />
     ~~~
   `)(() => {
       const label = text('Label', 'nexus');
@@ -46,14 +35,13 @@ storiesOf('Components/Orgs', module)
         <React.Fragment>
           <div style={{ margin: '50px 40px 0px' }}>
             <h2>No logo</h2>
-            <OrgCard projectNumber={123} label="bbp" />
+            <OrgItem numberOfProjects={123} label="bbp" />
           </div>
           <div style={{ margin: '50px 40px 0px' }}>
             <h2>Logo and edit button</h2>
-            <OrgCard
+            <OrgItem
               label={label}
-              projectNumber={projects}
-              logo={logo}
+              numberOfProjects={projects}
               onClick={action('org-click')}
               onEdit={action('edit-button-click')}
             />
@@ -70,7 +58,7 @@ storiesOf('Components/Orgs', module).add(
     It allows to filter Organizations by name.
 
     ~~~js
-      <OrgCard />
+      <OrgItem />
     ~~~
   `)(() => {
     return (
@@ -87,50 +75,6 @@ storiesOf('Components/Orgs', module).add(
     );
   })
 );
-
-storiesOf('Components/Orgs', module)
-  .addDecorator(withKnobs)
-  .add(
-    'OrgDropdown',
-    withInfo(`
-    The Organization Dropdown is used mainly in the header component.
-    It allows to display the selected organization as well as
-    navigating and filtering all available organizations.
-
-    ~~~js
-      <OrgDropdown />
-    ~~~
-  `)(() => {
-      const busy = knob('busy', { value: false, type: 'boolean' });
-      const error = knob('error', { value: false, type: 'boolean' });
-      return (
-        <React.Fragment>
-          <div style={{ margin: '50px 40px 0px' }}>
-            <h2>Organization Dopdown in header</h2>
-            <Header
-              name="Mark Hamill"
-              links={[]}
-              githubIssueURL="some.url"
-              version="v1.1.0"
-            >
-              <OrgDropdown
-                busy={busy}
-                orgs={orgs}
-                error={error ? { message: '', name: '' } : undefined}
-                onCreateNewClick={action('onCreateNewClick')}
-                onExploreAllClick={action('onExploreAllClick')}
-                onRefreshClick={action('onRefreshClick')}
-              />
-            </Header>
-          </div>
-          <div style={{ margin: '50px 40px 0px' }}>
-            <h2>Organization Dropdown</h2>
-            <OrgDropdown orgs={orgs} />
-          </div>
-        </React.Fragment>
-      );
-    })
-  );
 
 storiesOf('Components/Orgs', module)
   .addDecorator(withKnobs)
