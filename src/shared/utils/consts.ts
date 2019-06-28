59032543c0e7420c090242f9b4bc87d61777d8d6
kenjinp <github@kenny.wtf>
2019-06-28 14:37:43 +0200

    fix orgListPageSize

--- src/shared/utils/consts.ts
+++ src/shared/utils/consts.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_UI_SETTINGS = {
   pageSizes: {
-    orgsListPageSize: 2,
+    orgsListPageSize: 20,
     projectsListPageSize: 20,
     resourcesListPageSize: 20,
     linksListPageSize: 10,
