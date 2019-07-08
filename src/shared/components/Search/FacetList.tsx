fe9e2e1858cd30884322f0c81aa7de8c2273c6b7
kenjinp <github@kenny.wtf>
2019-07-08 17:02:38 +0200

    add FacetList

---
+++ src/shared/components/Search/FacetList.tsx
@@ -0,0 +1,94 @@
+import * as React from 'react';
+import { Checkbox, Collapse, Badge } from 'antd';
+import CollapsePanel from 'antd/lib/collapse/CollapsePanel';
+import './FacetList.less';
+
+export interface FilterUpdatePayload {
+  filterName: string;
+  values: string[];
+}
+
+export interface Filter {
+  name: string;
+  values: {
+    id: string;
+    label: string;
+  }[];
+}
+
+interface FilterProps {
+  appliedFilters: { [filterName: string]: string[] };
+  filters?: Filter[];
+  updateFilters: (filterUpdate: FilterUpdatePayload) => void;
+}
+
+const FacetList: React.FunctionComponent<FilterProps> = props => {
+  const { appliedFilters, filters, updateFilters } = props;
+
+  const handleChange = (filterName: string, values: any[]) => {
+    updateFilters({
+      filterName,
+      values,
+    });
+  };
+
+  return (
+    <div className="facet-list">
+      <Collapse bordered={false}>
+        {filters &&
+          !!filters.length &&
+          filters.map((filter, index) => {
+            const groupAppliedFilters = appliedFilters[filter.name];
+            const groupAppliedFiltersCount = groupAppliedFilters
+              ? groupAppliedFilters.length
+              : 0;
+            const header = (
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <div style={{ width: '60%' }}>{filter.name}</div>
+                <div style={{ width: '20%' }}>
+                  <Badge
+                    count={groupAppliedFiltersCount}
+                    style={{ backgroundColor: '#44c7f4' }}
+                  />
+                </div>
+                <div>{filter.values.length}</div>
+              </div>
+            );
+            return (
+              <CollapsePanel header={header} key={`$${index}`}>
+                <Checkbox.Group
+                  style={{ width: '100%' }}
+                  onChange={values => handleChange(filter.name, values)}
+                >
+                  <ul className="checkbox-list">
+                    {filters.map(value => {
+                      return (
+                        <li key={value.id}>
+                          <Checkbox
+                            value={value.id}
+                            checked={
+                              groupAppliedFilters &&
+                              groupAppliedFilters.indexOf(value.id) >= 0
+                            }
+                          >
+                            {value.label}
+                          </Checkbox>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </Checkbox.Group>
+              </CollapsePanel>
+            );
+          })}
+      </Collapse>
+    </div>
+  );
+};
+
+export default FacetList;
--- src/shared/views/Search.tsx
+++ src/shared/views/Search.tsx
@@ -1,12 +1,62 @@
 import * as React from 'react';
+import { match } from 'react-router';
+import { useNexusContext } from '@bbp/react-nexus';
 
-interface SearchViewProps {}
+interface SearchViewProps {
+  match: match<{ org: string; project: string; searchViewId: string }>;
+}
 
 const SearchView: React.FunctionComponent<SearchViewProps> = props => {
-  console.log({ props });
+  const { match } = props;
+
+  const nexus = useNexusContext();
+  const [searchView, setSearchView] = React.useState<any>(null);
+  const [aggView, setAggView] = React.useState<any>(null);
+  const [facets, setFacets] = React.useState<any>(null);
+
+  console.log({ searchView, aggView, facets });
+
+  React.useEffect(() => {
+    if (aggView) {
+      nexus
+        .httpPost({
+          body: searchView && searchView.facets,
+          path: `${aggView['_self']}/sparql`,
+          headers: {
+            'Content-Type': 'text/plain',
+          },
+        })
+        .then(res => {
+          setFacets(res);
+        });
+    }
+  }, [aggView && aggView['@id']]);
+
+  React.useEffect(() => {
+    if (searchView) {
+      nexus
+        .httpGet({
+          path: searchView.view,
+        })
+        .then(res => {
+          setAggView(res);
+        });
+    }
+  }, [searchView && searchView['@id']]);
+
+  React.useEffect(() => {
+    nexus.Resource.get(
+      match.params.org,
+      match.params.project,
+      match.params.searchViewId
+    ).then(res => {
+      setSearchView(res);
+    });
+  }, [match.url]);
+
   return (
     <div className="search-view view-container">
-      <h1>This is a SearchView</h1>
+      <h1 className="name">Search</h1>
     </div>
   );
 };
