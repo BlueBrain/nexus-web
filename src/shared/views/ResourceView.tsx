38ec56142e3f72dd67ded3abd15abff5ba4c230b
Dhanesh Neela Mana <dhanesh.n.m19@gmail.com>
2019-11-01 12:06:56 +0100

    Remove old SDK from ResourceView
    - The network call to pull an image file for preview is moved to
    ResourceView
    - ImagePreviewContainer is no longer used in ResourceView

--- src/shared/views/ResourceView.tsx
+++ src/shared/views/ResourceView.tsx
@@ -7,7 +7,7 @@ import { Spin, Card, Empty, Tabs, notification, Alert } from 'antd';
 import * as queryString from 'query-string';
 import { useAsyncEffect } from 'use-async-effect';
 import { useNexusContext } from '@bbp/react-nexus';
-import { Resource, ResourceLink } from '@bbp/nexus-sdk';
+import { Resource, ResourceLink, NexusFile } from '@bbp/nexus-sdk';
 
 import { getResourceLabel, getResourceLabelsAndIdsFromSelf } from '../utils';
 import ResourceCardComponent from '../components/ResourceCard';
@@ -16,6 +16,7 @@ import ResourceLinksContainer from '../containers/ResourceLinks';
 import ResourceActionsContainer from '../containers/ResourceActions';
 import { isDeprecated } from '../utils/nexusMaybe';
 import ResourceEditorContainer from '../containers/ResourceEditor';
+import { ImagePreviewComponent } from '../components/Images/Preview';
 
 const TabPane = Tabs.TabPane;
 const DEFAULT_ACTIVE_TAB_KEY = '#JSON';
@@ -58,6 +59,9 @@ const ResourceView: React.FunctionComponent<ResourceViewProps> = props => {
     error: null,
   });
 
+  const [previewImageSrc, setPreviewImageSrc] = React.useState<
+    string | undefined
+  >(undefined);
   const [latestResource, setLatestResource] = React.useState<
     Resource & { [key: string]: any } | null
   >(null);
@@ -78,6 +82,14 @@ const ResourceView: React.FunctionComponent<ResourceViewProps> = props => {
       revision: resource ? resource._rev : undefined,
       tab: activeTabKey,
     });
+  };
+
+  const makePreviewImage = (previewImageSrc: string) => {
+    const img = new Image();
+    img.src = previewImageSrc;
+    return (
+      <ImagePreviewComponent loading={false} hasImage={true} image={img} />
+    );
   };
 
   const handleEditFormSubmit = async (value: any) => {
@@ -142,6 +154,22 @@ const ResourceView: React.FunctionComponent<ResourceViewProps> = props => {
             rev: Number(rev),
           })) as Resource)
         : latestResource;
+      if (newResource['@type'] === 'File') {
+        const file = (await nexus.File.get(orgLabel, projectLabel, resourceId, {
+          as: 'json',
+        })) as NexusFile;
+        if (file._mediaType.includes('image')) {
+          const rawData = (await nexus.File.get(
+            orgLabel,
+            projectLabel,
+            resourceId,
+            { as: 'blob' }
+          )) as Blob;
+          const blob = new Blob([rawData], { type: file._mediaType });
+          const src = URL.createObjectURL(blob);
+          setPreviewImageSrc(src);
+        }
+      }
       setResource({
         resource: newResource,
         error: null,
@@ -209,7 +237,14 @@ const ResourceView: React.FunctionComponent<ResourceViewProps> = props => {
                   closable
                 />
               )}
-              <ResourceCardComponent resource={resource} />
+              <ResourceCardComponent
+                resource={resource}
+                preview={
+                  previewImageSrc
+                    ? makePreviewImage(previewImageSrc)
+                    : undefined
+                }
+              />
               <ResourceActionsContainer resource={resource} />
               <Tabs activeKey={activeTabKey} onChange={handleTabChange}>
                 <TabPane tab="JSON" key="#JSON">
