e541f8c7f1e3432d43f70e6c24cb43a31216149c
Julien Machon <julienmachon@gmail.com>
2019-11-05 11:04:31 +0100

    😎 final cleanup 🎆 (#335)
    * remove rawQuery redux stuff
    
    * use Identity type from sdk
    
    * remove toolbar component. It is unused
    
    * AND THE LEGACY SDK IS GONE
    
    * removed unsed components and functions
    
    * remove imports and test of deleted files

--- src/shared/store/actions/utils/__tests__/errors.spec.ts
+++ src/shared/store/actions/utils/__tests__/errors.spec.ts
@@ -1,4 +1,4 @@
-import { RequestError, formatError } from '../errors';
+import { RequestError } from '../errors';
 import { HTTP_STATUSES, HTTP_STATUS_TYPE_KEYS } from '../statusCodes';
 
 describe('RequestError', () => {
@@ -27,34 +27,6 @@ describe('RequestError', () => {
     expect(forbiddenError).toHaveProperty(
       'type',
       HTTP_STATUSES[HTTP_STATUS_TYPE_KEYS.FORBIDDEN].type
-    );
-  });
-});
-
-describe('formatError', () => {
-  it('should take a common error response and format it into the correct RequestError', () => {
-    const sdkError = new Error('Not Found');
-    expect(formatError(sdkError)).toBeInstanceOf(RequestError);
-    expect(formatError(sdkError)).toHaveProperty(
-      'code',
-      HTTP_STATUSES[HTTP_STATUS_TYPE_KEYS.NOT_FOUND].code
-    );
-    expect(formatError(sdkError)).toHaveProperty(
-      'type',
-      HTTP_STATUSES[HTTP_STATUS_TYPE_KEYS.NOT_FOUND].type
-    );
-  });
-
-  it('should default to BAD_REQUEST error type if the error messages doesnt match anything', () => {
-    const sdkError = new Error('I Am A Teapot');
-    expect(formatError(sdkError)).toBeInstanceOf(RequestError);
-    expect(formatError(sdkError)).toHaveProperty(
-      'code',
-      HTTP_STATUSES[HTTP_STATUS_TYPE_KEYS.BAD_REQUEST].code
-    );
-    expect(formatError(sdkError)).toHaveProperty(
-      'type',
-      HTTP_STATUSES[HTTP_STATUS_TYPE_KEYS.BAD_REQUEST].type
     );
   });
 });
--- src/shared/store/reducers/index.ts
+++ src/shared/store/reducers/index.ts
@@ -2,19 +2,12 @@ import { StaticRouterProps } from 'react-router';
 import { UserState } from 'redux-oidc';
 import auth, { AuthState } from './auth';
 import config, { ConfigState } from './config';
-import rawQuery, {
-  RawQueryState,
-  rawElasticSearchQueryReducer,
-  RawElasticSearchQueryState,
-} from './rawQuery';
 import uiSettingsReducer, { UISettingsState } from './ui-settings';
 
 export interface RootState {
   auth: AuthState;
   config: ConfigState;
   router?: StaticRouterProps;
-  rawQuery?: RawQueryState;
-  rawElasticSearchQuery?: RawElasticSearchQueryState;
   uiSettings: UISettingsState;
   oidc: UserState;
 }
@@ -22,7 +15,5 @@ export interface RootState {
 export default {
   auth,
   config,
-  rawQuery,
-  rawElasticSearchQuery: rawElasticSearchQueryReducer,
   uiSettings: uiSettingsReducer,
 };
