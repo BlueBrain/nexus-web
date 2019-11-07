c428efb990c445addcdd5563187c45544401a7de
kenjinp <github@kenny.wtf>
2019-11-07 12:17:54 +0100

    remove names from routes

--- src/shared/routes.ts
+++ src/shared/routes.ts
@@ -10,56 +10,43 @@ import SparqlQueryView from './views/SparqlQueryView';
 import ACLsView from './views/ACLsView';
 import UserView from './views/UserView';
 
-type RoutePropsWithName = RouteProps & {
-  name: string;
-};
-
-const routes: RoutePropsWithName[] = [
+const routes: RouteProps[] = [
   {
     path: '/',
     exact: true,
-    name: 'OrgsView',
     component: OrgsView,
   },
   {
-    name: 'LoginView',
     path: '/login',
     component: Login,
   },
   {
-    name: 'UserView',
     path: '/user',
     component: UserView,
   },
   {
-    name: 'ProjectsView',
     path: '/:orgLabel',
     exact: true,
     component: ProjectsView,
   },
   {
-    name: 'ProjectView',
     path: '/:orgLabel/:projectLabel',
     exact: true,
     component: ProjectView,
   },
   {
-    name: 'ResourceView',
     path: '/:orgLabel/:projectLabel/resources/:resourceId',
     component: ResourceView,
   },
   {
-    name: 'ElasticSearchQueryView',
     path: '/:orgLabel/:projectLabel/:viewId/_search',
     component: ElasticSearchQueryView,
   },
   {
-    name: 'SparqlQueryView',
     path: '/:orgLabel/:projectLabel/:viewId/sparql',
     component: SparqlQueryView,
   },
   {
-    name: 'ACLsView',
     path: '/:orgLabel/:projectLabel/_settings/acls',
     component: ACLsView,
   },
