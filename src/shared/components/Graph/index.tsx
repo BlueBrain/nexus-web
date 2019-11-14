223e3aa678106d346cddce74af29ee2427c5452b
Kenneth Pirman <github@kenny.wtf>
2019-11-14 11:06:56 +0100

    871 show blank nodes (#371)
    * blank nodes
    
    * attempt different dagre algorithms
    
    * remove un-used layout type and style the labels to make them legible
    
    * add blank node to legend
    
    * clean up by puting the node and edge creation in function

--- src/shared/components/Graph/index.tsx
+++ src/shared/components/Graph/index.tsx
@@ -1,31 +1,28 @@
 import * as React from 'react';
 import * as cytoscape from 'cytoscape';
-import { Alert, Switch, Button, Tooltip } from 'antd';
+import { Alert, Button } from 'antd';
 import * as cola from 'cytoscape-cola';
 
 import './GraphComponent.less';
 
-const DEFAULT_LAYOUT = {
-  name: 'cola',
-  idealEdgeLength: 100,
-  nodeOverlap: 20,
-  refresh: 20,
-  fit: true,
-  padding: 100,
-  randomize: false,
-  componentSpacing: 100,
-  nodeRepulsion: 400000,
-  edgeElasticity: 100,
-  nestingFactor: 5,
-  gravity: 10,
-  numIter: 1000,
-  initialTemp: 200,
-  coolingFactor: 0.95,
-  minTemp: 1.0,
-  nodeSpacing() {
-    return 40;
+const LAYOUTS: {
+  [layoutName: string]: {
+    name: string;
+    [optionKey: string]: any;
+  };
+} = {
+  COLA: {
+    name: 'cola',
+    label: 'Forced Graph',
+    maxSimulationTime: 1000,
+  },
+  BREADTH_FIRST: {
+    name: 'breadthfirst',
+    label: 'Tree',
   },
 };
+
+const DEFAULT_LAYOUT = 'BREADTH_FIRST';
 
 const Graph: React.FunctionComponent<{
   elements: cytoscape.ElementDefinition[];
@@ -33,25 +30,23 @@ const Graph: React.FunctionComponent<{
   onNodeExpand?(id: string, isExternal: boolean): void;
 }> = ({ elements, onNodeClick, onNodeExpand }) => {
   const container = React.useRef<HTMLDivElement>(null);
-  const [showLabels, setShowLabels] = React.useState(false);
   const [showAlert, setShowAlert] = React.useState(true);
-  const [layoutType, setLayoutType] = React.useState(DEFAULT_LAYOUT.name);
+  const [layoutBusy, setLayoutBusy] = React.useState(false);
+  const [layoutType, setLayoutType] = React.useState(DEFAULT_LAYOUT);
   const graph = React.useRef<cytoscape.Core>();
 
   const forceLayout = () => {
     if (graph.current) {
-      if (layoutType === DEFAULT_LAYOUT.name) {
-        graph.current && graph.current.layout(DEFAULT_LAYOUT).run();
-        return;
-      }
-      if (layoutType === 'lines') {
-        graph.current && graph.current.layout({ name: 'breadthfirst' }).run();
-        return;
-      }
-      if (layoutType === 'grid') {
-        graph.current && graph.current.layout({ name: 'grid' }).run();
-        return;
-      }
+      setLayoutBusy(true);
+      graph.current &&
+        graph.current
+          .layout({
+            ...LAYOUTS[layoutType],
+            stop() {
+              setLayoutBusy(false);
+            },
+          })
+          .run();
     }
   };
 
@@ -62,8 +57,8 @@ const Graph: React.FunctionComponent<{
   React.useEffect(() => {
     if (graph.current) {
       graph.current.on('tap', 'node', (e: cytoscape.EventObject) => {
-        const { visited } = e.target.data();
-        if (visited) {
+        const { visited, isBlankNode } = e.target.data();
+        if (visited || isBlankNode) {
           return;
         }
         onNodeExpand &&
@@ -87,21 +82,45 @@ const Graph: React.FunctionComponent<{
 
   const style = [
     {
+      selector: 'node',
+      style: {
+        height: 16,
+        width: 16,
+      },
+    },
+    {
+      selector: 'edge',
+      style: {
+        width: 2,
+        'line-color': '#8a8b8b',
+      },
+    },
+    {
       selector: 'node[label]',
       style: {
+        'text-outline-color': 'white',
+        'text-outline-width': 2,
         label: 'data(label)',
       },
     },
     {
       selector: 'edge[label]',
-      style: showLabels
-        ? {
-            label: 'data(label)',
-            // this style is not included in the types
-            // @ts-ignore
-            'edge-text-rotation': 'autorotate',
-          }
-        : {},
+      style: {
+        label: 'data(label)',
+        'text-outline-color': 'white',
+        'text-outline-width': 2,
+        'edge-text-rotation': 'autorotate',
+      },
+    },
+    {
+      selector: '.blank-node',
+      style: {
+        width: 12,
+        height: 12,
+        'background-color': 'white',
+        'border-color': '#00adee',
+        'border-width': 2,
+      },
     },
     {
       selector: '.external',
@@ -132,10 +151,6 @@ const Graph: React.FunctionComponent<{
   };
 
   React.useEffect(() => {
-    graph.current && graph.current.style(style);
-  }, [showLabels]);
-
-  React.useEffect(() => {
     replaceElements(elements);
   }, [JSON.stringify(elements)]);
 
@@ -164,41 +179,24 @@ const Graph: React.FunctionComponent<{
         <div>
           <span className="node -internal" /> Internal Link
         </div>
+        <div>
+          <span className="node -blank-node" /> Blank Node
+        </div>
       </div>
       <div className="top">
         <div className="controls">
-          <Switch
-            checkedChildren={'hide labels'}
-            unCheckedChildren={'show labels'}
-            checked={showLabels}
-            onChange={() => {
-              setShowLabels(!showLabels);
-            }}
-          />
           <div>
-            <Tooltip title="Recenter">
-              <Button
-                type={
-                  layoutType === DEFAULT_LAYOUT.name ? 'primary' : 'default'
-                }
-                icon="border-inner"
-                onClick={handleLayoutClick(DEFAULT_LAYOUT.name)}
-              />
-            </Tooltip>
-            <Tooltip title="Array nodes in lines">
-              <Button
-                type={layoutType === 'lines' ? 'primary' : 'default'}
-                icon="small-dash"
-                onClick={handleLayoutClick('lines')}
-              />
-            </Tooltip>
-            <Tooltip title="Array nodes as grid">
-              <Button
-                type={layoutType === 'grid' ? 'primary' : 'default'}
-                icon="table"
-                onClick={handleLayoutClick('grid')}
-              />
-            </Tooltip>
+            {Object.keys(LAYOUTS).map(layoutKey => {
+              return (
+                <Button
+                  disabled={layoutBusy}
+                  type={layoutKey === layoutType ? 'primary' : 'default'}
+                  onClick={handleLayoutClick(layoutKey)}
+                >
+                  {LAYOUTS[layoutKey].label}
+                </Button>
+              );
+            })}
           </div>
         </div>
         <div className="alert">
