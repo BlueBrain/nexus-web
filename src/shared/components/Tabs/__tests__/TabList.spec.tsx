ed113e51b9898606754d226c5f582f5f8060ec5a
Dhanesh Neela Mana <dhanesh.n.m19@gmail.com>
2019-11-14 13:13:51 +0100

    Refactor test

--- src/shared/components/Tabs/__tests__/TabList.spec.tsx
+++ src/shared/components/Tabs/__tests__/TabList.spec.tsx
@@ -1,15 +1,10 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
 
-// Need to mock Date.now() because it is used down the line
-// to generate a per-element class, which breaks snapshot testing.
-// Must be done before importing the React component.
-jest.spyOn(Date, 'now').mockImplementation(() => 1234567890123);
-
 import TabList from '../TabList';
 
-const tabListCompenent = <TabList items={[]} onSelected={jest.fn}/>;
-const wrapper = shallow(tabListCompenent);
+const tabListComponent = <TabList items={[]} onSelected={jest.fn}/>;
+const wrapper = shallow(tabListComponent);
 
 describe('TabList component', () => {
   it('should render correctly', () => {
@@ -17,5 +12,5 @@ describe('TabList component', () => {
   });
 });
 
-// Restoring Date.now() to the real thing, so as not to break other tests.
+
 jest.restoreAllMocks();
