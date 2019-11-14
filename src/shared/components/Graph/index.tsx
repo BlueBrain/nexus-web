00342d5f1e5f6512291145f6911c5732548d86ef
Natalia Stafeeva <n.stafeeva@gmail.com>
2019-11-14 14:33:01 +0100

    FIX: disable popup for blanc nodes (#374)
    * Disable popup for blanc nodes and external links
    
    * Update cursor for nodes
    
    * Show grab cursor for blanc nodes
    
    * Add mousedown and mouseup events

--- src/shared/components/Graph/index.tsx
+++ src/shared/components/Graph/index.tsx
@@ -34,6 +34,7 @@ const Graph: React.FunctionComponent<{
   const [showAlert, setShowAlert] = React.useState(true);
   const [layoutBusy, setLayoutBusy] = React.useState(false);
   const [layoutType, setLayoutType] = React.useState(DEFAULT_LAYOUT);
+  const [cursorPointer, setCursorPointer] = React.useState<string | null>(null);
   const graph = React.useRef<cytoscape.Core>();
 
   const forceLayout = () => {
@@ -58,8 +59,8 @@ const Graph: React.FunctionComponent<{
   React.useEffect(() => {
     if (graph.current) {
       graph.current.on('tap', 'node', (e: cytoscape.EventObject) => {
-        const { visited, isBlankNode, isExpandable } = e.target.data();
-        if (visited || isBlankNode || !isExpandable) {
+        const { isBlankNode, isExpandable } = e.target.data();
+        if (isBlankNode || !isExpandable) {
           return;
         }
         onNodeExpand &&
@@ -69,14 +70,37 @@ const Graph: React.FunctionComponent<{
         onNodeClick && onNodeClick(e.target.id(), e.target.data('isExternal'));
       });
       graph.current.on('mouseover', 'node', (e: cytoscape.EventObject) => {
+        const { isBlankNode, isExpandable } = e.target.data();
+
+        if (isExpandable) {
+          setCursorPointer('pointer');
+        } else {
+          setCursorPointer('grab');
+        }
+
+        if (isBlankNode) return;
+
         onNodeHoverOver && onNodeHoverOver(e.target.id(), e.target.data('isExternal'));
       });
+      graph.current.on('mouseout', 'node', (e: cytoscape.EventObject) => {
+        setCursorPointer(null);
+      });
+      graph.current.on('mousedown', 'node', (e: cytoscape.EventObject) => {
+        setCursorPointer('grabbing');
+      });
+      graph.current.on('mouseup', 'node', (e: cytoscape.EventObject) => {
+        setCursorPointer('grab');
+      });
     }
+
     return () => {
       if (graph.current) {
         graph.current.removeListener('tap');
         graph.current.removeListener('taphold');
         graph.current.removeListener('mouseover');
+        graph.current.removeListener('mouseout');
+        graph.current.removeListener('mousedown');
+        graph.current.removeListener('mouseup');
       }
     };
   });
@@ -188,7 +212,9 @@ const Graph: React.FunctionComponent<{
 
   return (
     <div className="graph-component">
-      <div className="graph" ref={container}></div>
+      <div className="graph" ref={container} style={cursorPointer ? {
+        cursor: cursorPointer,
+      } : {}}></div>
       <div className="legend">
         <div>
           <span className="node -external" /> External Link
