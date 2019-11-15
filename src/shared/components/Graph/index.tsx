5b790223282f564dc5b4e007f70027fb9e3a7090
Natalia Stafeeva <n.stafeeva@gmail.com>
2019-11-15 14:59:06 +0100

    882 Add arrows (#381)
    * Add arrows
    
    * Move stlyes to a file
    
    * Change file extension

--- src/shared/components/Graph/index.tsx
+++ src/shared/components/Graph/index.tsx
@@ -3,6 +3,8 @@ import * as cytoscape from 'cytoscape';
 import { Alert, Button } from 'antd';
 import * as cola from 'cytoscape-cola';
 
+import style from './style';
+
 import './GraphComponent.less';
 
 const LAYOUTS: {
@@ -110,80 +112,6 @@ const Graph: React.FunctionComponent<{
   const handleLayoutClick = (type: string) => () => {
     setLayoutType(type);
   };
-
-  const style = [
-    {
-      selector: 'node',
-      style: {
-        height: 16,
-        width: 16,
-      },
-    },
-    {
-      selector: '.-expandable',
-      style: {
-        height: 24,
-        width: 24,
-      },
-    },
-    {
-      selector: 'edge',
-      style: {
-        width: 2,
-        'target-arrow-shape': 'triangle',
-        'line-color': '#8a8b8b',
-      },
-    },
-    {
-      selector: 'node[label]',
-      style: {
-        'text-outline-color': 'white',
-        'text-outline-width': 2,
-        label: 'data(label)',
-      },
-    },
-    {
-      selector: 'edge[label]',
-      style: {
-        label: 'data(label)',
-        'text-outline-color': 'white',
-        'text-outline-width': 2,
-        'edge-text-rotation': 'autorotate',
-      },
-    },
-    {
-      selector: '.blank-node',
-      style: {
-        width: 12,
-        height: 12,
-        'background-color': 'white',
-        'border-color': '#faad14',
-        'border-width': 2,
-      },
-    },
-    {
-      selector: '.-external',
-      style: {
-        'background-color': 'white',
-        'border-color': '#00adee',
-        'border-width': 2,
-      },
-    },
-    {
-      selector: '.-internal',
-      style: {
-        'background-color': '#ff6666',
-      },
-    },
-    {
-      selector: '.-internal.-expanded',
-      style: {
-        'background-color': 'white',
-        'border-color': '#ff6666',
-        'border-width': 2,
-      },
-    },
-  ];
 
   const replaceElements = (elements: cytoscape.ElementDefinition[]) => {
     if (graph.current) {
---
+++ src/shared/components/Graph/style.ts
@@ -0,0 +1,77 @@
+const style = [
+    {
+      selector: 'node',
+      style: {
+        height: 16,
+        width: 16,
+      },
+    },
+    {
+      selector: '.-expandable',
+      style: {
+        height: 24,
+        width: 24,
+      },
+    },
+    {
+      selector: 'edge',
+      style: {
+        width: 2,
+        'curve-style': 'bezier',
+        'target-arrow-shape': 'triangle',
+        'line-color': '#8a8b8b',
+      },
+    },
+    {
+      selector: 'node[label]',
+      style: {
+        'text-margin-y': -10,
+        'text-outline-color': 'white',
+        'text-outline-width': 2,
+        label: 'data(label)',
+      },
+    },
+    {
+      selector: 'edge[label]',
+      style: {
+        label: 'data(label)',
+        'text-outline-color': 'white',
+        'text-outline-width': 2,
+        'edge-text-rotation': 'autorotate',
+      },
+    },
+    {
+      selector: '.blank-node',
+      style: {
+        width: 12,
+        height: 12,
+        'background-color': 'white',
+        'border-color': '#faad14',
+        'border-width': 2,
+      },
+    },
+    {
+      selector: '.-external',
+      style: {
+        'background-color': 'white',
+        'border-color': '#00adee',
+        'border-width': 2,
+      },
+    },
+    {
+      selector: '.-internal',
+      style: {
+        'background-color': '#ff6666',
+      },
+    },
+    {
+      selector: '.-internal.-expanded',
+      style: {
+        'background-color': 'white',
+        'border-color': '#ff6666',
+        'border-width': 2,
+      },
+    },
+  ];
+  
+  export default style;
