fa29479930403d911699d55d12192e0e85b7ca57
Natalia Stafeeva <n.stafeeva@gmail.com>
2019-11-15 15:22:31 +0100

    884 Display mini resource card for external resources (#380)
    * Display mini resource card for external resources
    
    * Refactor styles
    
    * Fix indentation

--- src/shared/containers/ResourcePreviewCardContainer.tsx
+++ src/shared/containers/ResourcePreviewCardContainer.tsx
@@ -2,16 +2,17 @@ import * as React from 'react';
 import { useAsyncEffect } from 'use-async-effect';
 import { useNexusContext } from '@bbp/react-nexus';
 import { Resource } from '@bbp/nexus-sdk';
-import { Card } from 'antd';
 
 import ResourceCard from '../components/ResourceCard';
 import ResourceCardCollapsed from '../components/ResourceCard/ResourceCardCollapsed';
+import ResourcePreviewCard from '../components/ResourceCard/ResourcePreviewCard';
 
 const ResourcePreviewCardContainer: React.FunctionComponent<{
     orgLabel: string,
     projectLabel: string,
     resourceId: string,
-}> = ({ orgLabel, projectLabel, resourceId }) => {
+    isExternal: boolean | null,
+}> = ({ orgLabel, projectLabel, resourceId, isExternal }) => {
     const nexus = useNexusContext();
     const [showFullCard, setShowFullCard] = React.useState(false);
     const [{ busy, resource, error }, setResource] = React.useState<{
@@ -26,6 +27,15 @@ const ResourcePreviewCardContainer: React.FunctionComponent<{
 
     useAsyncEffect(async isMounted => {
       if (!isMounted()) {
+        return;
+      }
+
+      if (isExternal) {
+        setResource({
+          resource: null,
+          error: null,
+          busy: false,
+        });
         return;
       }
 
@@ -53,22 +63,29 @@ const ResourcePreviewCardContainer: React.FunctionComponent<{
         });
       }
     }, [orgLabel, projectLabel, resourceId]);
-  
-  if (resource) {		
+
+  if (isExternal) {
     return (
-      <div style={{
-        margin: '20px',
-        position: 'absolute',
-        bottom: 0,
-        right: 0,
-        maxWidth: '600px',
-      }}>
+      <ResourcePreviewCard>
+        <ResourceCardCollapsed resourceId={resourceId} busy={busy} isExternal />
+      </ResourcePreviewCard>
+    )
+  }
+  
+  if (resource) {
+    const {
+      '@type': type,
+    } = resource;
+    const types: string[] = Array.isArray(type) ? type : [type || ''];
+
+    return (
+      <ResourcePreviewCard>
         {showFullCard ? (
           <ResourceCard resource={resource} onClickCollapse={() => setShowFullCard(false)} />
         ) : (
-          <ResourceCardCollapsed resource={resource} onClickExpand={() => setShowFullCard(true)} busy={busy} />
+          <ResourceCardCollapsed resourceId={resourceId} onClickExpand={() => setShowFullCard(true)} busy={busy} types={types} />
         )}
-      </div>
+      </ResourcePreviewCard>
     );
   }
 
