8261373ed37e4bf02184e658630ca5830c6d0a0e
Dhanesh Neela Mana <dhanesh.n.m19@gmail.com>
2019-11-18 09:59:32 +0100

    List Dashboard Resources as a Table
    Adds container/component to list resources in a Dashboard.

---
+++ src/shared/components/ResultsTable/ResultsTable.tsx
@@ -0,0 +1,113 @@
+import * as React from 'react';
+import { Table } from 'antd';
+import './ResultTable.less';
+import moment from 'moment';
+import Search from 'antd/lib/input/Search';
+
+const PAGE_SIZE = 10;
+
+type ResultTableProps = {
+  headerProperties?: {
+    title: string;
+    dataIndex: string;
+  }[];
+  items: {
+    id: string;
+    [dataIndex: string]: any;
+  }[];
+  pageSize?: number;
+  handleClick: (self: string) => void;
+};
+
+const ResultsTable: React.FunctionComponent<ResultTableProps> = ({
+  headerProperties,
+  items,
+  pageSize = PAGE_SIZE,
+  handleClick,
+}) => {
+  const [searchValue, setSearchValue] = React.useState();
+
+  const columnList = [
+    ...(headerProperties
+      ? headerProperties.map(({ title, dataIndex }) => {
+          // We can create special renderers for the cells here
+          let render;
+          switch (title) {
+            case 'Created At':
+              render = (date: string) => <span>{moment(date).fromNow()}</span>;
+              break;
+            case 'Project':
+              render = (projectURI: string) => {
+                const [org, project] = ['1', '2'];
+                return (
+                  <span>
+                    <b>{org}</b> / {project}
+                  </span>
+                );
+              };
+              break;
+            default:
+              render = (value: string) => <span>{value}</span>;
+              break;
+          }
+
+          return {
+            title,
+            dataIndex,
+            render,
+            className: `result-column ${dataIndex}`,
+          };
+        })
+      : []),
+  ];
+
+  const filteredItems = items.filter(item => {
+    return (
+      Object.values(item)
+        .join(' ')
+        .toLowerCase()
+        .search((searchValue || '').toLowerCase()) >= 0
+    );
+  });
+
+  const tableItems = searchValue ? filteredItems : items;
+  const total = tableItems.length;
+  const showPagination = total > pageSize;
+
+  return (
+    <div className="result-table">
+      <Table
+        onRow={data => ({
+          onClick: event => handleClick(data.self.value),
+        })}
+        columns={columnList}
+        dataSource={tableItems}
+        bordered
+        pagination={
+          showPagination && {
+            total,
+            pageSize,
+          }
+        }
+        title={() => (
+          <div className="header">
+            {(showPagination || !!searchValue) && (
+              <Search
+                className="search"
+                value={searchValue}
+                onChange={(e: React.FormEvent<HTMLInputElement>) => {
+                  setSearchValue(e.currentTarget.value);
+                }}
+              />
+            )}
+            <div className="total">
+              {total} {`Result${total > 1 ? 's' : ''}`}
+            </div>
+          </div>
+        )}
+      />
+    </div>
+  );
+};
+
+export default ResultsTable;
