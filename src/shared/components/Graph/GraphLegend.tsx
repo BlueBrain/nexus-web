7ebd3163c10578f0b4c80c3bd11693eb357dba96
Stafeeva <n.stafeeva@gmail.com>
2019-11-18 14:55:53 +0100

    Fix indentation

--- src/shared/components/Graph/GraphLegend.tsx
+++ src/shared/components/Graph/GraphLegend.tsx
@@ -3,24 +3,24 @@ import * as React from 'react';
 import './GraphLegend.less';
 
 const GraphLegend: React.FunctionComponent = () => (
-	<div className="legend">
-		<div>
-			<span className="node -main" />
-			Origin
-		</div>
-		<div>
-			<span className="node -external" />
-			External Link
-		</div>
-		<div>
-			<span className="node -internal" />
-			Internal Link
-		</div>
-		<div>
-			<span className="node -blank-node" />
-			Blank Node
-		</div>
-	</div>
+  <div className="legend">
+    <div>
+      <span className="node -main" />
+      Origin
+    </div>
+    <div>
+      <span className="node -external" />
+      External Link
+    </div>
+    <div>
+      <span className="node -internal" />
+      Internal Link
+    </div>
+    <div>
+      <span className="node -blank-node" />
+      Blank Node
+    </div>
+  </div>
 );
 
 export default GraphLegend;
