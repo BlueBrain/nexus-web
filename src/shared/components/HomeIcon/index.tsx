c97d3b0819dc4350db8903cb07f36e6cd453d76d
Stafeeva <n.stafeeva@gmail.com>
2019-12-03 10:59:25 +0100

    Fix indentation

--- src/shared/components/HomeIcon/index.tsx
+++ src/shared/components/HomeIcon/index.tsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 import { Tooltip, Icon } from 'antd';
 
 const HomeIcon: React.FunctionComponent<{}> = () => (
-	<Link to="/">
-		<Tooltip title="Back to all organizations" placement="right">
-			<Icon type="home" />
-		</Tooltip>
-	</Link>
+  <Link to="/">
+    <Tooltip title="Back to all organizations" placement="right">
+      <Icon type="home" />
+    </Tooltip>
+  </Link>
 );
 
 export default HomeIcon;
