aa48d84639ab56d8389889981e5fac5dc895f9cb
Dhanesh Neela Mana <dhanesh.n.m19@gmail.com>
2019-12-05 14:40:08 +0100

    Update derived vlaues on change of rawData

--- src/shared/components/ResourceEditor/index.tsx
+++ src/shared/components/ResourceEditor/index.tsx
@@ -60,6 +60,8 @@ const ResourceEditor: React.FunctionComponent<ResourceEditorProps> = props => {
 
   React.useEffect(() => {
     setEditing(false);
+    setStringValue(JSON.stringify(rawData, null, 2));
+    setParsedValue(rawData);
   }, [rawData]); // only runs when Editor receives new resource to edit
 
   const handleChange = (editor: any, data: any, value: any) => {
