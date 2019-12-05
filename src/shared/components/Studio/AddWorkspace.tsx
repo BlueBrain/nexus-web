6de40914ac55c512a1fae8cfc204d557b1dd245d
Stafeeva <n.stafeeva@gmail.com>
2019-12-05 17:16:07 +0100

    Fix indentation

--- src/shared/components/Studio/AddWorkspace.tsx
+++ src/shared/components/Studio/AddWorkspace.tsx
@@ -2,21 +2,21 @@ import * as React from 'react';
 import { Button, Modal } from 'antd';
 
 const AddWorkspace: React.FC = () => {
-	const [showModal, setShowModal] = React.useState(false);
+  const [showModal, setShowModal] = React.useState(false);
 
-	return (
-		<>
-			<Button onClick={() => setShowModal(true)} icon="plus" />
-			<Modal
-				title="Create a new Workspace"
-				visible={showModal}
-				footer={null}
-				onCancel={() => setShowModal(false)}
-			>
-				<p>Hello</p>
-			</Modal>
-		</>
-	);
+  return (
+    <>
+      <Button onClick={() => setShowModal(true)} icon="plus" />
+      <Modal
+        title="Create a new Workspace"
+        visible={showModal}
+        footer={null}
+        onCancel={() => setShowModal(false)}
+      >
+        <p>Hello</p>
+      </Modal>
+    </>
+  );
 }
 
 export default AddWorkspace;
