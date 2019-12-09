ae017c274176d6bd953666eb8065707fe6c3f298
Kenneth Pirman <github@kenny.wtf>
2019-12-09 16:27:42 +0100

    create studio context (#454)

--- src/shared/containers/CreateStudioContainer.tsx
+++ src/shared/containers/CreateStudioContainer.tsx
@@ -3,8 +3,10 @@ import { Button, Modal, notification } from 'antd';
 import { useNexusContext } from '@bbp/react-nexus';
 
 import StudioEditorForm from '../components/Studio/StudioEditorForm';
+import STUDIO_CONTEXT from '../components/Studio/StudioContext';
 
-const DEFAULT_STUDIO_TYPE = 'https://bluebrainnexus.io/studio/vocabulary/Studio';
+export const DEFAULT_STUDIO_TYPE =
+  'https://bluebrainnexus.io/studio/vocabulary/Studio';
 
 const CreateStudioContainer: React.FC<{
   orgLabel: string;
@@ -15,42 +17,67 @@ const CreateStudioContainer: React.FC<{
   const [showModal, setShowModal] = React.useState(false);
 
   const generateStudioResource = (label: string, description?: string) => ({
-    // TODO: decide if a context is mandatory for studio creation
     label,
     description,
+    '@context': STUDIO_CONTEXT['@id'],
     '@type': DEFAULT_STUDIO_TYPE,
   });
 
+  const makeStudioContext = async () => {
+    try {
+      await nexus.Resource.get(
+        orgLabel,
+        projectLabel,
+        encodeURIComponent(STUDIO_CONTEXT['@id'])
+      );
+    } catch (error) {
+      if (error['@type'] === 'NotFound') {
+        // @ts-ignore TODO: update resource type in SDK to allow nested objects
+        // https://github.com/BlueBrain/nexus/issues/937
+        await nexus.Resource.create(orgLabel, projectLabel, {
+          ...STUDIO_CONTEXT,
+        });
+        return;
+      }
+      throw error;
+    }
+  };
+
   const createStudioResource = async (label: string, description?: string) => {
+    await makeStudioContext();
     return await nexus.Resource.create(
       orgLabel,
       projectLabel,
-      generateStudioResource(label, description),
+      generateStudioResource(label, description)
     );
-  }
+  };
 
   const saveStudio = (label: string, description?: string) => {
     setShowModal(false);
 
-    createStudioResource(label, description).then(response => {
-      goToStudio && goToStudio(response['@id']);
-      
-      notification.success({
-        message: 'Studio was created successfully',
-        duration: 2,
+    createStudioResource(label, description)
+      .then(response => {
+        goToStudio && goToStudio(response['@id']);
+
+        notification.success({
+          message: 'Studio was created successfully',
+          duration: 2,
+        });
+      })
+      .catch(error => {
+        notification.error({
+          message: 'An error occurred',
+          description: error.reason || error.message,
+          duration: 3,
+        });
       });
-    }).catch(error => {
-      notification.error({
-        message: 'An error occurred',
-        description: error.reason || error.message,
-        duration: 3,
-      });
-    });
-  }
+  };
 
   return (
     <div className="studio-modal">
-      <Button type="primary" block onClick={() => setShowModal(true)}>Create Studio</Button>
+      <Button type="primary" block onClick={() => setShowModal(true)}>
+        Create Studio
+      </Button>
       <Modal
         title="Create Studio"
         visible={showModal}
@@ -58,9 +85,9 @@ const CreateStudioContainer: React.FC<{
         onCancel={() => setShowModal(false)}
       >
         <StudioEditorForm saveStudio={saveStudio} />
-      </Modal>  
+      </Modal>
     </div>
   );
-}
+};
 
 export default CreateStudioContainer;
