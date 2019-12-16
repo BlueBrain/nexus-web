4650a7da51d45ec9d9eb24adf11ef04440fe0a73
Julien Machon <julienmachon@gmail.com>
2019-12-16 17:13:26 +0100

    first implementation of nexus plugin loader

---
+++ src/shared/containers/NexusPlugin.tsx
@@ -0,0 +1,68 @@
+/**
+ * This component requires SystemJS to be available globally (in window)
+ */
+import * as React from 'react';
+import invariant from 'ts-invariant';
+
+const warningMessage =
+  'SystemJS not found. ' +
+  'To load plugins, Nexus Web requires SystemJS to be available globally.' +
+  ' You can find out more here https://github.com/systemjs/systemjs';
+
+export type NexusPluginProps = {
+  url: string;
+};
+
+export default class RenderRemoteComponent extends React.Component<
+  NexusPluginProps,
+  { hasError: boolean; loading: boolean }
+> {
+  private container: React.RefObject<HTMLDivElement>;
+  private pluginCallback: () => void;
+
+  constructor(props: NexusPluginProps) {
+    super(props);
+    this.state = { hasError: false, loading: true };
+    this.container = React.createRef();
+    this.pluginCallback = () => {};
+    // @ts-ignore
+    invariant(window.System, warningMessage);
+  }
+
+  componentDidCatch(e: Error) {
+    console.error(e);
+    this.setState({ hasError: true, loading: false });
+  }
+
+  componentDidMount() {
+    // @ts-ignore
+    window.System.import(this.props.url)
+      .then(
+        (module: { default: (ref: HTMLDivElement | null) => () => void }) => {
+          this.setState({
+            hasError: false,
+            loading: false,
+          });
+          this.pluginCallback = module.default(this.container.current);
+        }
+      )
+      .catch((error: Error) => {
+        console.error(error);
+        this.setState({ hasError: true, loading: false });
+      });
+  }
+
+  componentWillMount() {
+    this.pluginCallback();
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <p>Error loading plugin {this.props.url}</p>;
+    }
+    if (this.state.loading) {
+      return <p>loading plugin {this.props.url}...</p>;
+    }
+    return <div className="remote-component" ref={this.container}></div>;
+  }
+}
