a9c8c2ca6ce3b279f65a320b019953c479a99b2b
Stafeeva <n.stafeeva@gmail.com>
2020-01-17 09:59:33 +0100

    Update tests

--- cypress/integration/studio/create_studio.spec.js
+++ cypress/integration/studio/create_studio.spec.js
@@ -6,22 +6,24 @@ describe('User is not logged in', () => {
     cy.get('.ListItem').click();
     cy.get('.ListItem').click();
     cy.get('.ant-drawer-close').click();
-    cy.get('.studio-list').find('button').click();
-    cy.get('.studio-label-input').type('My Studio');
+    cy.get('.studio-list')
+      .find('button')
+      .click();
+    cy.get('.studio-label-input').type('Studio Cypress Test');
     cy.get('.studio-description-input').type('Amazing description');
     cy.get('form').submit();
-    cy.contains('My Studio');
+    cy.contains('Studio Cypress Test');
     cy.contains('Amazing description');
     cy.contains('Add Workspace').click();
-    cy.get('.workspace-label-input').type('My Workspace');
+    cy.get('.workspace-label-input').type('Workspace Cypress Test');
     cy.get('.workspace-description-input').type('Simple description');
     cy.get('form').submit();
     cy.wait(3000);
-    cy.contains('My Workspace');
+    cy.contains('Workspace Cypress Test');
     cy.contains('Simple description');
     cy.contains('Add Dashboard').click();
     cy.contains('Create Dashboard');
-    cy.get('.dashboard-label-input').type('My Dashboard');
+    cy.get('.dashboard-label-input').type('Dashboard Cypress Test');
     cy.get('.dashboard-description-input').type('Wow amazing');
   });
 });
--- cypress/integration/studio/studio.spec.js
+++ cypress/integration/studio/studio.spec.js
@@ -1,27 +1,27 @@
 describe('Navigate to Dashboards', () => {
-    it('Navigates to dashboards', () => {
-      cy.visit('http://localhost:8000');
-      cy.contains('Organizations');
-      cy.contains('login');
-      cy.get('.ListItem').click();
-      cy.get('.ListItem').click();
-      cy.get('.ant-drawer-close').click();
-      cy.contains('My Studio').click();
-      cy.contains('My Studio');
-      cy.contains('Amazing description');
-      cy.contains('My Workspace');
-    });
-
-    it('Edits a dashboard', () => {
-      cy.visit('http://localhost:8000');
-      cy.contains('Organizations');
-      cy.contains('login');
-      cy.get('.ListItem').click();
-      cy.get('.ListItem').click();
-      cy.get('.ant-drawer-close').click();
-      cy.contains('My Studio').click();
-      cy.contains('My Studio');
-      cy.contains('Amazing description');
-      cy.contains('My Workspace');
-    });
+  it('Navigates to dashboards', () => {
+    cy.visit('http://localhost:8000');
+    cy.contains('Organizations');
+    cy.contains('login');
+    cy.get('.ListItem').click();
+    cy.get('.ListItem').click();
+    cy.get('.ant-drawer-close').click();
+    cy.contains('Studio Cypress Test').click();
+    cy.contains('Studio Cypress Test');
+    cy.contains('Amazing description');
+    cy.contains('Workspace Cypress Test');
   });
+
+  it('Edits a dashboard', () => {
+    cy.visit('http://localhost:8000');
+    cy.contains('Organizations');
+    cy.contains('login');
+    cy.get('.ListItem').click();
+    cy.get('.ListItem').click();
+    cy.get('.ant-drawer-close').click();
+    cy.contains('Studio Cypress Test').click();
+    cy.contains('Studio Cypress Test');
+    cy.contains('Amazing description');
+    cy.contains('Workspace Cypress Test');
+  });
+});
