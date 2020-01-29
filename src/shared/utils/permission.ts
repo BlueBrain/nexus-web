2c9ba3adafaee0e8a796c7150f2bae86a3f86468
Dhanesh Neela Mana <dhanesh.n.m19@gmail.com>
2020-01-29 14:44:38 +0100

    correct the permissions path

--- src/shared/utils/permission.ts
+++ src/shared/utils/permission.ts
@@ -5,7 +5,7 @@ export const resourcesWritePermissionsWrapper = (
   child: React.ReactNode,
   permissionPath: string
 ) => {
-  const permissions = ['projects/write'];
+  const permissions = ['resources/write'];
   return React.createElement(AccessControl, {
     permissions,
     path: permissionPath,
