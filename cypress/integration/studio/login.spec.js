26f21e86ffe37ff8930c030fe5bc13e30b02ae93
Stafeeva <n.stafeeva@gmail.com>
2020-02-07 11:29:15 +0100

    Add tests for login page

--- cypress/integration/studio/login.spec.js
+++ cypress/integration/studio/login.spec.js
@@ -1,10 +1,34 @@
-const test_path = 'http://localhost:8000';
+const loginPage = 'http://localhost:8000/login';
+const docLink = 'https://bluebrainnexus.io/docs';
+const reportIssueLink =
+  'https://github.com/BlueBrain/nexus/issues/new?labels=frontend,nexus-web';
 
-describe('Navigate to Dashboards', () => {
-  it('User can log in', () => {
-    cy.visit(test_path);
-    cy.contains('Organizations');
-    cy.contains('login').click();
-    cy.contains('Log in').click();
+beforeEach(() => {
+  cy.visit(loginPage);
+});
+
+describe('Login page', () => {
+  it('contains the Nexus logo', () => {
+    cy.get('.logo').should('be.visible');
+  });
+
+  it('has the login button', () => {
+    cy.contains('button', 'Log in');
+  });
+
+  it('contains the link to docs', () => {
+    cy.contains('a', 'Documentation').should('have.attr', 'href', docLink);
+  });
+
+  it('contains the link to open a github issue', () => {
+    cy.contains('a', 'Report Issue').should(
+      'have.attr',
+      'href',
+      reportIssueLink
+    );
+  });
+
+  it('contains the Information button', () => {
+    cy.get('.header-info-button').should('be.visible');
   });
 });
