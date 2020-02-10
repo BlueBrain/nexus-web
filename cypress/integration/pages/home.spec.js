557a17f800b16e43a2b8f73ff12430b4b3d6af40
Stafeeva <n.stafeeva@gmail.com>
2020-02-10 10:04:45 +0100

    WIP

--- cypress/integration/pages/home.spec.js
+++ cypress/integration/pages/home.spec.js
@@ -13,6 +13,11 @@ describe('Homepage', () => {
     cy.contains('h1', 'Organizations');
   });
 
+  it('loads organizations', () => {
+    cy.server();
+    cy.route('GET', '/orgs?deprecated=false&size=20');
+  });
+
   it('allows to search for an organization', () => {
     cy.get('input').type('bbp');
   });
