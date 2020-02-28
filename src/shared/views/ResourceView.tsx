23fee3c43c2b3fb6810eefd3f0a4e3a56c06466a
Dhanesh Neela Mana <dhanesh.n.m19@gmail.com>
2020-02-28 14:05:45 +0100

    remove unused imports

--- src/shared/views/ResourceView.tsx
+++ src/shared/views/ResourceView.tsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
-
-import GraphContainer from '../containers/GraphContainer';
 import useMeasure from '../hooks/useMeasure';
 import ResourceViewContainer from '../containers/ResourceViewContainer';
 
 const ResourceView: React.FunctionComponent = props => {
-  const [{ ref }, { width }] = useMeasure();
-
   return (
     <div className="resource-view view-container -unconstrained-width">
       <ResourceViewContainer />
