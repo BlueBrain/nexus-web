51b4ac31581b3bb49cd9d65e8026f4fb363e073b
Natalia Stafeeva <n.stafeeva@gmail.com>
2020-08-25 14:26:18 +0200

    1456 initialize projects subapp (#663)
    * Add projects tab
    
    * Update messages
    
    * Fix naming

---
+++ src/subapps/projects/views/ProjectsListView.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+const ProjectsListView: React.FC<{}> = () => {
+  return (
+    <div className="view-container">
+      <div>
+        <h1>Projects</h1>
+        <p>Project List will be added soon. Stay tuned!</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsListView;
