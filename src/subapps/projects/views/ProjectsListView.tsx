8191030eb3fb23f08d3ca17927c391a5f28e0c9e
Natalia Stafeeva <n.stafeeva@gmail.com>
2020-09-02 12:16:56 +0200

    Add new project form (#667)
    * Create new project button
    
    * Create new project container
    
    * Project from - WIP
    
    * Create project form
    
    * Rearrange shorthand properties
    
    * Add nexus context
    
    * Add calls to save data
    
    * Tidy up
    
    * Update validation
    
    * Update types
    
    * Fix button styling
    
    * Hide button wher user is not logged in
    
    * Add type

--- src/subapps/projects/views/ProjectsListView.tsx
+++ src/subapps/projects/views/ProjectsListView.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 
 import ProjectCard from '../components/ProjectCard';
+import NewProjectContainer from '../containers/NewProjectContainer';
 
 const ProjectsListView: React.FC<{}> = () => {
   return (
     <div className="view-container projects-subapp-container">
       <div>
         <h1>Projects</h1>
+        <NewProjectContainer />
         {/* This is just an example */}
         {/* <ProjectCard
           name="COVID-19"
