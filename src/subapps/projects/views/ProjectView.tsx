8e4a08529f17947dfdd93f13557e79aa194b814d
Dhanesh Neela Mana <dhanesh.neelamana@epfl.ch>
2020-09-03 14:12:45 +0200

    Update routes/Fetch Project (#672)
    * Update routes/Fetch Project
    
     Fetch project data for ProjectView.

--- src/subapps/projects/views/ProjectView.tsx
+++ src/subapps/projects/views/ProjectView.tsx
@@ -1,16 +1,42 @@
 import * as React from 'react';
-
+import { useProjectsSubappContext } from '..';
 import ProjectHeader from '../components/ProjectHeader';
-
+import { useRouteMatch } from 'react-router';
 import './ProjectView.less';
+import { useNexusContext } from '@bbp/react-nexus';
+import { Project } from '@bbp/nexus-sdk';
 
 const ProjectView: React.FC = () => {
-  // fetch project based on params
-
+  const subapp = useProjectsSubappContext();
+  const match = useRouteMatch<{ orgLabel: string; projectLabel: string }>(
+    `/${subapp.namespace}/:orgLabel/:projectLabel`
+  );
+  const [project, setProject] = React.useState<Project>();
+  const [error, setError] = React.useState<Error>();
+  const projectLabel = match?.params.projectLabel;
+  const orgLabel = match?.params.orgLabel;
+  const nexus = useNexusContext();
+  React.useEffect(() => {
+    if (orgLabel && projectLabel) {
+      nexus.Project.get(orgLabel, projectLabel)
+        .then((value: Project) => {
+          setProject(value);
+        })
+        .catch(error => {
+          setError(error);
+        });
+    }
+  }, []);
   return (
-    <div className="project-container">
-      <ProjectHeader name="Thalamic microcircuit" />
-    </div>
+    <>
+      {project ? (
+        <div className="project-container">
+          <ProjectHeader name={project._label} />
+        </div>
+      ) : (
+        'Project not found'
+      )}
+    </>
   );
 };
 
