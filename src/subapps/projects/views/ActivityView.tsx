7222d56b2477d4c0d062b52a70b61162e19263d0
Stafeeva <n.stafeeva@gmail.com>
2020-09-21 14:50:50 +0200

    Fetch activity and its links

--- src/subapps/projects/views/ActivityView.tsx
+++ src/subapps/projects/views/ActivityView.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import { useRouteMatch } from 'react-router';
+import { useNexusContext } from '@bbp/react-nexus';
+import { Breadcrumb } from 'antd';
 
 import { useProjectsSubappContext } from '..';
 import ProjectPanel from '../components/ProjectPanel';
+import ActivitiesBoard from '../components/Activities/ActivitiesBoard';
 
 const ActivityView: React.FC = () => {
+  const nexus = useNexusContext();
   const subapp = useProjectsSubappContext();
   const match = useRouteMatch<{
     orgLabel: string;
@@ -12,9 +16,42 @@ const ActivityView: React.FC = () => {
     activityId: string;
   }>(`/${subapp.namespace}/:orgLabel/:projectLabel/:activityId`);
 
+  const [activities, setActivities] = React.useState<any[]>([]);
+  const [activity, setActivity] = React.useState<any>();
+
   const projectLabel = match?.params.projectLabel;
   const orgLabel = match?.params.orgLabel;
-  const activityId = match?.params.activityId;
+  // const activityId = match?.params.activityId;
+  const activityId = 'ceede3fc-e033-4c1c-8aca-54a9c0132507';
+
+  React.useEffect(() => {
+    if (orgLabel && projectLabel) {
+      nexus.Resource.get(orgLabel, projectLabel, encodeURIComponent(activityId))
+        .then(response => setActivity(response))
+        .catch(error => console.error(error));
+
+      nexus.Resource.links(
+        orgLabel,
+        projectLabel,
+        encodeURIComponent(activityId),
+        'incoming'
+      )
+        .then(response =>
+          Promise.all(
+            response._results.map(link => {
+              return nexus.Resource.get(
+                orgLabel,
+                projectLabel,
+                encodeURIComponent(link['@id'])
+              );
+            })
+          ).then(response => {
+            setActivities(response);
+          })
+        )
+        .catch(error => console.log('e', error));
+    }
+  }, []);
 
   return (
     <div className="activity-view">
@@ -25,6 +62,14 @@ const ActivityView: React.FC = () => {
           onUpdate={() => {}}
         />
       )}
+      {activity && (
+        <Breadcrumb separator=">">
+          <Breadcrumb.Item>{projectLabel}</Breadcrumb.Item>
+          <Breadcrumb.Item href="">Parent Activity</Breadcrumb.Item>
+          <Breadcrumb.Item href="">{activity.name}</Breadcrumb.Item>
+        </Breadcrumb>
+      )}
+      <ActivitiesBoard activities={activities} />
     </div>
   );
 };
