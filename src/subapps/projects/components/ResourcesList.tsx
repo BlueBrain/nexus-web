dfb96f4fd9d5bffcd78875b934fae23a46b901fd
Natalia Stafeeva <n.stafeeva@gmail.com>
2020-10-12 14:04:00 +0200

    Redirect to resource view (#694)
    * Redirect to admin resource view
    
    * Use link

--- src/subapps/projects/components/ResourcesList.tsx
+++ src/subapps/projects/components/ResourcesList.tsx
@@ -5,7 +5,11 @@ import ActivityResourceItem from './ActivityResourceItem';
 
 import './ResourcesList.less';
 
-const ResourcesList: React.FC<{ resources: Resource[] }> = ({ resources }) => {
+const ResourcesList: React.FC<{
+  resources: Resource[];
+  projectLabel: string;
+  orgLabel: string;
+}> = ({ resources, projectLabel, orgLabel }) => {
   return (
     <div className="resources-list">
       <div className="resources-list__controls">
@@ -14,7 +18,13 @@ const ResourcesList: React.FC<{ resources: Resource[] }> = ({ resources }) => {
       </div>
       <div className="resources-list__resources">
         {resources.map(resource => (
-          <ActivityResourceItem item={resource} key={resource['@id']} />
+          <ActivityResourceItem
+            item={resource}
+            key={resource['@id']}
+            link={`/${orgLabel}/${projectLabel}/resources/${encodeURIComponent(
+              resource['@id']
+            )}`}
+          />
         ))}
       </div>
     </div>
