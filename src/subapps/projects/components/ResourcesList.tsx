74662017b770b56dc1be7cb44eea9ca8341d9611
Stafeeva <n.stafeeva@gmail.com>
2020-10-21 16:10:23 +0200

    Update UI

--- src/subapps/projects/components/ResourcesList.tsx
+++ src/subapps/projects/components/ResourcesList.tsx
@@ -1,21 +1,51 @@
 import * as React from 'react';
 import { Resource } from '@bbp/nexus-sdk';
-import { Empty } from 'antd';
+import { Empty, Input, Checkbox } from 'antd';
 
 import ActivityResourceItem from './ActivityResourceItem';
 
 import './ResourcesList.less';
+
+const { Search } = Input;
 
 const ResourcesList: React.FC<{
   resources: Resource[];
   projectLabel: string;
   orgLabel: string;
 }> = ({ resources, projectLabel, orgLabel }) => {
+  const onSearch = (event: any) => {
+    console.log('searching....', event);
+  };
+
+  const onChange = (event: any) => {
+    console.log('filtering....', event.target.value);
+  };
+
+  const filters = [
+    { label: 'Code', value: 'code' },
+    { label: 'Notes', value: 'notes' },
+    { label: 'Input data', value: 'input-data' },
+    { label: 'Output data', value: 'output-data' },
+  ];
+
   return (
     <div className="resources-list">
       <div className="resources-list__controls">
-        <div>Search: coming soon</div>
-        <div>Filters: coming soon</div>
+        <h3 className="resources-list__group-title">Search</h3>
+        <Search
+          placeholder="Search by label or description"
+          onSearch={onSearch}
+        />
+        <div className="resources-list__filters">
+          <h3 className="resources-list__group-title">Filters</h3>
+          {filters.map(filter => (
+            <div className="resources-list__filter" key={filter.value}>
+              <Checkbox onChange={onChange} value={filter.value}>
+                {filter.label}
+              </Checkbox>
+            </div>
+          ))}
+        </div>
       </div>
       <div className="resources-list__resources">
         {resources.length > 0 ? (
