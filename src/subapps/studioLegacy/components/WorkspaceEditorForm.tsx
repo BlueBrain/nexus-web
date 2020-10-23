a74b2f91a8c0f62103a2928b56abba1b339e37c4
Pirman Kenneth William <github@kenny.wtf>
2020-10-23 14:35:38 +0200

    convert WorkspaceEditorForm

--- src/subapps/studioLegacy/components/WorkspaceEditorForm.tsx
+++ src/subapps/studioLegacy/components/WorkspaceEditorForm.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Resource } from '@bbp/nexus-sdk';
 import { Input, Form, Tooltip, Button } from 'antd';
-import { WrappedFormUtils } from 'antd/lib/form/Form';
 import Icon from '@ant-design/icons/lib/components/Icon';
 import TextArea from 'antd/lib/input/TextArea';
 
@@ -12,26 +11,24 @@ type WorkspaceResource = Resource<{
 }>;
 
 const WorkspaceEditorForm: React.FC<{
-  form: WrappedFormUtils;
   saveWorkspace?(label: string, description?: string): void;
   workspace?: WorkspaceResource | null;
-}> = ({ form, saveWorkspace, workspace }) => {
-  const { getFieldDecorator } = form;
+}> = ({ saveWorkspace, workspace }) => {
+  const [form] = Form.useForm();
 
   const formItemLayout = {
     labelCol: { span: 6 },
     wrapperCol: { span: 14 },
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    form.validateFields((err, values) => {
-      if (!err) {
-        const { label, description } = values;
-        saveWorkspace && saveWorkspace(label, description);
-      }
-    });
+  const handleSubmit = async () => {
+    try {
+      const values = await form.validateFields();
+      const { label, description } = values;
+      saveWorkspace && saveWorkspace(label, description);
+    } catch (error) {
+      // TODO handle errors?
+    }
   };
 
   const { label, description } = workspace || {
@@ -40,7 +37,7 @@ const WorkspaceEditorForm: React.FC<{
   };
 
   return (
-    <Form {...formItemLayout} onSubmit={handleSubmit}>
+    <Form {...formItemLayout} onChange={handleSubmit}>
       <Form.Item
         label={
           <span>
@@ -50,16 +47,16 @@ const WorkspaceEditorForm: React.FC<{
             </Tooltip>
           </span>
         }
+        name="label"
+        initialValue={label}
+        rules={[
+          {
+            required: true,
+            message: 'Please input a label!',
+          },
+        ]}
       >
-        {getFieldDecorator('label', {
-          initialValue: label,
-          rules: [
-            {
-              required: true,
-              message: 'Please input a label!',
-            },
-          ],
-        })(<Input className="ui-workspace-label-input" />)}
+        <Input className="ui-workspace-label-input" />
       </Form.Item>
       <Form.Item
         label={
@@ -70,10 +67,10 @@ const WorkspaceEditorForm: React.FC<{
             </Tooltip>
           </span>
         }
+        name="description"
+        initialValue={description}
       >
-        {getFieldDecorator('description', {
-          initialValue: description,
-        })(<TextArea className="ui-workspace-description-input" />)}
+        <TextArea className="ui-workspace-description-input" />
       </Form.Item>
       <Button type="primary" htmlType="submit">
         Save
@@ -82,8 +79,4 @@ const WorkspaceEditorForm: React.FC<{
   );
 };
 
-export default Form.create<{
-  form: WrappedFormUtils;
-  saveWorkspace?(label: string): void;
-  studio?: WorkspaceResource | null;
-}>()(WorkspaceEditorForm);
+export default WorkspaceEditorForm;
