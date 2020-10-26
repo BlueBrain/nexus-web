a5bacf10a615bbb9916cfa1f6d49b170b32e5e9d
Kenneth Pirman <github@kenny.wtf>
2020-10-26 11:05:03 +0100

    merge from master

---
+++ src/subapps/search/views/SearchView.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const SearchView: React.FC = () => {
+  return (
+    <div className="search-view view-container">
+      <h1>Search View</h1>
+      <h2>TODO</h2>
+    </div>
+  );
+};
+
+export default SearchView;
