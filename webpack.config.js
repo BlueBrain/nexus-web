cd951637a182986e396d5d01ce64fb659bcad5fd
Natalia Stafeeva <n.stafeeva@gmail.com>
2020-11-06 09:10:10 +0100

    Fix css image loading (#713)

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ const config = [
             loader: 'file-loader',
             options: {
               outputPath: 'assets/',
-              publicPath: 'public/assets/',
+              publicPath: '/public/assets/',
             },
           },
         },
