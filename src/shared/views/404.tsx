3ca97e5296a1d85e6597e23ea3fdaec54e24b970
Natalia Stafeeva <n.stafeeva@gmail.com>
2020-11-20 16:04:16 +0100

    Fix login component layout and update 404 screen (#724)
    * Fix login box layout
    
    * Update snapshots
    
    * 404: update not found page
    
    * jest: update snapshots
    
    * loginBox: remove unused prop
    
    * 404: update not found page

--- src/shared/views/404.tsx
+++ src/shared/views/404.tsx
@@ -1,5 +1,20 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { Result, Button } from 'antd';
 
-const NotFound: React.FunctionComponent = () => <p>Page not found</p>;
+const NotFound: React.FunctionComponent = () => {
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle={<h3>Sorry! The page you are looking for was not found.</h3>}
+      extra={
+        <Button type="primary">
+          <Link to="/">Home</Link>
+        </Button>
+      }
+    />
+  );
+};
 
 export default NotFound;
