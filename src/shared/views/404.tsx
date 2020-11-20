8696ddf0a81b4a43b176fff473e13dc1a0c69e25
Stafeeva <n.stafeeva@gmail.com>
2020-11-20 15:15:48 +0100

    404: update not found page

--- src/shared/views/404.tsx
+++ src/shared/views/404.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import { Empty, Button } from 'antd';
+import { Result, Button } from 'antd';
 
 const NotFound: React.FunctionComponent = () => {
   return (
-    <div className="not-found-view">
-      <Empty description={false} />
-      <h2>Sorry! The page you are looking for was not found.</h2>
-      <Button type="primary">
-        <Link to="/">Home</Link>
-      </Button>
-    </div>
+    <Result
+      status="404"
+      title="404"
+      subTitle={<h3>Sorry! The page you are looking for was not found.</h3>}
+      extra={
+        <Button type="primary">
+          <Link to="/">Home</Link>
+        </Button>
+      }
+    />
   );
 };
 
