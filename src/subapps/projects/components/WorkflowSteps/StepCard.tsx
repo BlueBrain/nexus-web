23ded76345d0be76284b96bb973040c6500f923a
Stafeeva <n.stafeeva@gmail.com>
2021-02-03 14:58:32 +0100

    Create status dropdown

--- src/subapps/projects/components/WorkflowSteps/StepCard.tsx
+++ src/subapps/projects/components/WorkflowSteps/StepCard.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import { Tooltip } from 'antd';
+import { Tooltip, Dropdown, Button, Menu } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
 
 import SubStepItem from './SubStepItem';
 import { StepResource } from '../../views/WorkflowStepView';
+import { Status } from '../StatusIcon';
 
 import './StepCard.less';
 
@@ -21,10 +23,39 @@ const StepCard: React.FC<{
   const { name, description, status } = step;
   const stepId = step['@id'];
 
+  console.log('Status', Object.keys(Status));
+
+  const handleMenuClick = (option: any) => {
+    console.log('data', option.key);
+
+    //update status here
+  };
+
+  const menu = (
+    <Menu onClick={handleMenuClick}>
+      {Object.values(Status).map(status => (
+        <Menu.Item key={status}>
+          <span className="step-card__status-item">{status}</span>
+        </Menu.Item>
+      ))}
+    </Menu>
+  );
+
   return (
     <div
       className={`step-card step-card--${status && status.replace(' ', '-')}`}
     >
+      <div
+        className={`step-card__status step-card__status--${status &&
+          status.replace(' ', '-')}`}
+      >
+        <Dropdown overlay={menu}>
+          <Button type="text">
+            <span className="step-card__status-button">{status}</span>
+            <DownOutlined />
+          </Button>
+        </Dropdown>
+      </div>
       <div className="step-card__main">
         <div className="step-card__title">
           <Link to={`/workflow/${orgLabel}/${projectLabel}/${stepId}`}>
@@ -50,26 +81,26 @@ const StepCard: React.FC<{
             <span>{description || '-'}</span>
           )}
         </div>
-      </div>
-      <div className="step-card__subactivities">
-        <div className="step-card__substeps-total">
-          <img src={settingIcon} className="step-card__info-icon" />
-          <span>
-            {(substeps && substeps.length) || 'No'}{' '}
-            {substeps && substeps.length === 1 ? 'sub-step' : 'sub-steps'}
-          </span>
-        </div>
-        <div className="step-card__list-container">
-          {substeps &&
-            substeps.length > 0 &&
-            substeps.map(substep => (
-              <SubStepItem
-                substep={substep}
-                key={substep['@id']}
-                orgLabel={orgLabel}
-                projectLabel={projectLabel}
-              />
-            ))}
+        <div className="step-card__subactivities">
+          <div className="step-card__substeps-total">
+            <img src={settingIcon} className="step-card__info-icon" />
+            <span>
+              {(substeps && substeps.length) || 'No'}{' '}
+              {substeps && substeps.length === 1 ? 'sub-step' : 'sub-steps'}
+            </span>
+          </div>
+          <div className="step-card__list-container">
+            {substeps &&
+              substeps.length > 0 &&
+              substeps.map(substep => (
+                <SubStepItem
+                  substep={substep}
+                  key={substep['@id']}
+                  orgLabel={orgLabel}
+                  projectLabel={projectLabel}
+                />
+              ))}
+          </div>
         </div>
       </div>
     </div>
