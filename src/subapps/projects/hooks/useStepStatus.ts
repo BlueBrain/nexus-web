8559f02cc2039cb819b191cda606976440ab41b3
Dhanesh Neela Mana <dhanesh.neelamana@epfl.ch>
2021-02-18 19:30:58 +0100

    Fix Update status revision
    When updating status of a step, use correct revision number on back to
    back updates.

--- src/subapps/projects/hooks/useStepStatus.ts
+++ src/subapps/projects/hooks/useStepStatus.ts
@@ -2,33 +2,45 @@ import * as React from 'react';
 import { useNexusContext } from '@bbp/react-nexus';
 
 import fusionConfig from '../config';
+import { Resource } from '@bbp/nexus-sdk';
 
 export const useStepStatus = (orgLabel: string, projectLabel: string) => {
   const nexus = useNexusContext();
   const [error, setError] = React.useState<Error>();
   const [success, setSuccess] = React.useState<boolean>();
+  const [updatedResource, setUpdatedResource] = React.useState<Resource>();
 
   const updateStatus = async (
     stepId: string,
     rev: number,
     newStatus: string
   ) => {
-    await nexus.Resource.getSource(
-      orgLabel,
-      projectLabel,
-      encodeURIComponent(stepId)
-    )
-      .then(response => {
-        const originalPayload = response;
+    try {
+      const originalPayload = await nexus.Resource.getSource(
+        orgLabel,
+        projectLabel,
+        encodeURIComponent(stepId)
+      );
 
-        return nexus.Resource.update(orgLabel, projectLabel, stepId, rev, {
+      const revCopy = updatedResource ? updatedResource._rev : rev;
+
+      const updatedCopy = await nexus.Resource.update(
+        orgLabel,
+        projectLabel,
+        stepId,
+        revCopy,
+        {
           ...originalPayload,
           status: newStatus,
           '@type': fusionConfig.workflowStepType,
-        });
-      })
-      .then(() => setSuccess(true))
-      .catch(error => setError(error));
+        }
+      );
+      setUpdatedResource(updatedCopy);
+
+      setSuccess(true);
+    } catch (error) {
+      setError(error);
+    }
   };
 
   return {
