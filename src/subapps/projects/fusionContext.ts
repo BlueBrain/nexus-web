43d2548320c08a730d268e64a3b1a92d52cabfcd
Stafeeva <n.stafeeva@gmail.com>
2021-05-05 15:33:46 +0200

    Update context

--- src/subapps/projects/fusionContext.ts
+++ src/subapps/projects/fusionContext.ts
@@ -9,50 +9,6 @@ export const WORKFLOW_STEP_CONTEXT = {
       description: {
         '@id': 'http://schema.org/description',
       },
-      hasParent: {
-        '@id': 'https://bluebrainnexus.io/workflowStep/vocabulary/hasParent',
-        '@type': '@id',
-      },
-      dueDate: {
-        '@id': 'https://bluebrainnexus.io/workflowStep/vocabulary/dueDate',
-      },
-      positionX: {
-        '@id': 'https://bluebrainnexus.io/workflowStep/vocabulary/positionX',
-      },
-      positionY: {
-        '@id': 'https://bluebrainnexus.io/workflowStep/vocabulary/positionY',
-      },
-      status: {
-        '@id': 'https://bluebrainnexus.io/workflowStep/vocabulary/status',
-      },
-      summary: {
-        '@id': 'https://bluebrainnexus.io/workflowStep/vocabulary/summary',
-      },
-      activityType: {
-        '@id': 'https://bluebrainnexus.io/workflowStep/vocabulary/activityType',
-      },
-      wasInformedBy: {
-        '@id':
-          'https://bluebrainnexus.io/workflowStep/vocabulary/wasInformedBy',
-        '@container': '@set',
-        '@type': '@id',
-      },
-      activities: {
-        '@id': 'https://bluebrainnexus.io/workflowStep/vocabulary/activities',
-        '@container': '@set',
-        '@type': '@id',
-      },
-      inputs: {
-        '@id': 'https://bluebrainnexus.io/workflowStep/vocabulary/inputs',
-        '@container': '@set',
-        '@type': '@id',
-      },
-      wasAssociatedWith: {
-        '@id':
-          'https://bluebrainnexus.io/workflowStep/vocabulary/wasAssociatedWith',
-        '@container': '@set',
-        '@type': '@id',
-      },
     },
   ],
   '@id': 'https://bluebrainnexus.io/workflowStep/workflowStep-context',
@@ -68,37 +24,6 @@ export const FUSION_TABLE_CONTEXT = {
       },
       description: {
         '@id': 'http://schema.org/description',
-      },
-      tableOf: {
-        '@id': 'https://bluebrainnexus.io/fusionTable/vocabulary/tableOf',
-        '@type': '@id',
-      },
-      view: {
-        '@id': 'https://bluebrainnexus.io/fusionTable/vocabulary/view',
-      },
-      enableSearch: {
-        '@id': 'https://bluebrainnexus.io/fusionTable/vocabulary/enableSearch',
-      },
-      enableInteractiveRows: {
-        '@id':
-          'https://bluebrainnexus.io/fusionTable/vocabulary/enableInteractiveRows',
-      },
-      enableDownload: {
-        '@id':
-          'https://bluebrainnexus.io/fusionTable/vocabulary/enableDownload',
-      },
-      enableSave: {
-        '@id': 'https://bluebrainnexus.io/fusionTable/vocabulary/enableSave',
-      },
-      resultsPerPage: {
-        '@id':
-          'https://bluebrainnexus.io/fusionTable/vocabulary/resultsPerPage',
-      },
-      dataQuery: {
-        '@id': 'https://bluebrainnexus.io/fusionTable/vocabulary/dataQuery',
-      },
-      configuration: {
-        '@container': '@set',
       },
     },
   ],
