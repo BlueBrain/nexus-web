dade9b5ef2fd89f9b27832190cdbc34a601f13c1
Stafeeva <n.stafeeva@gmail.com>
2021-06-09 11:11:42 +0200

    Create files structure with a graph

---
+++ src/subapps/admin/components/Projects/ResourceInfoPanel.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const ResourceInfoPanel: React.FC<{}> = () => {
+  return <div style={{ width: '50%' }}>test resource info</div>;
+};
+
+export default ResourceInfoPanel;
