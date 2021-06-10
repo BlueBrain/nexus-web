c796126f380024f8026b1a64e53bade3893d4051
Stafeeva <n.stafeeva@gmail.com>
2021-06-10 16:39:30 +0200

    Graph styling

--- src/subapps/admin/components/Projects/ResourceInfoPanel.tsx
+++ src/subapps/admin/components/Projects/ResourceInfoPanel.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 const ResourceInfoPanel: React.FC<{}> = () => {
-  return <div style={{ width: '50%' }}>test resource info</div>;
+  return <div style={{ width: '10%' }}>test resource info</div>;
 };
 
 export default ResourceInfoPanel;
