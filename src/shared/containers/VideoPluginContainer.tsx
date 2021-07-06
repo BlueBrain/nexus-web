6cff42eb5bcae45b8e2eeb0eaf898a94514f94e6
Nick Smit <nick.smit@gmail.com>
2021-07-06 09:22:24 +0200

    fixed typo

--- src/shared/containers/VideoPluginContainer.tsx
+++ src/shared/containers/VideoPluginContainer.tsx
@@ -43,7 +43,7 @@ const VideoPlugin: React.FunctionComponent<VideoProps> = ({
         <iframe
           width="853"
           height="480"
-          src={resource['video']['emberdUrl']}
+          src={resource['video']['embedUrl']}
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
