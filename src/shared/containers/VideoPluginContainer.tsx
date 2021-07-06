f9e767e38d490e06d7ea3cdbe4b7d3288ee1ea79
Nick Smit <nick.smit@gmail.com>
2021-07-06 09:29:46 +0200

    removed some of the tab change methods to see if it resolves errors

--- src/shared/containers/VideoPluginContainer.tsx
+++ src/shared/containers/VideoPluginContainer.tsx
@@ -1,38 +1,16 @@
 import * as React from 'react';
 import ResourceMetadata from '../components/ResourceMetadata';
-import { Resource, ResourceLink } from '@bbp/nexus-sdk';
+import { Resource } from '@bbp/nexus-sdk';
 import { Collapse } from 'antd';
-
 const { Panel } = Collapse;
 
 type VideoProps = {
   resource: Resource;
-  goToResource: (
-    orgLabel: string,
-    projectLabel: string,
-    resourceId: string,
-    opt: {
-      revision?: number;
-      tab?: string;
-      expanded?: boolean;
-    }
-  ) => void;
-  handleExpanded: (expanded: boolean) => void;
-  handleTabChange: (activeTabKey: string) => void;
 };
 
 const VideoPlugin: React.FunctionComponent<VideoProps> = ({
   resource,
-  goToResource,
-  handleExpanded,
-  handleTabChange,
 }) => {
-  const [tabChange, setTabChange] = React.useState<boolean>(false);
-  const onTabChange = (tab: string) => {
-    // forces a tab to rerender - otherwise RecourceEditor shifts its content left (codemirror issue)
-    setTabChange(!tabChange);
-    handleTabChange(tab);
-  };
   return (
     <Collapse onChange={() => {}}>
       <Panel header="Video" key="1">
@@ -45,7 +23,7 @@ const VideoPlugin: React.FunctionComponent<VideoProps> = ({
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
-          title="Embedded youtube"
+          title="Embedded video"
         />
       </Panel>
     </Collapse>
