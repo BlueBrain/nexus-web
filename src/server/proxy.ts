99dfd63e925f0ee162641cb06bd93161784dea45
Nick Smit <nick.smit@gmail.com>
2021-08-12 03:24:52 +0200

    added default data set, more styling

--- src/server/proxy.ts
+++ src/server/proxy.ts
@@ -1,69 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 import * as express from 'express';
-
+import * as data from './many.json';
 function setUpDeltaProxy(app: express.Express, apiEndpoint: string) {
   const esResult = {
     hits: {
-      hits: [
-        {
-          _type: '_doc',
-          sort: [1627390182151],
-          _id:
-            'https://bbp.epfl.ch/neurosciencegraph/data/d52e34bd-5f62-495a-9e5c-b2d3afa33017',
-          _index: 'delta_3c07805d-0818-4eaf-8af4-91daa452268d_2',
-          _source: {
-            project: {
-              identifier: 'http://example.org/subjectSpecies',
-              label: 'Project',
-            },
-            '@type': ['a', 'b', 'c', 'd'],
-            name: 'A Resource',
-            description: 'Description',
-            subjectSpecies: {
-              identifier: 'http://example.org/subjectSpecies',
-              label: 'Subject Species',
-            },
-            brainRegion: {
-              identifier: 'http://example.org/subjectSpecies',
-              label: 'Brain Region',
-            },
-            contributors: [
-              {
-                identifier: 'http://example.org/subjectSpecies',
-                label: 'James Bond',
-              },
-              {
-                identifier: 'http://example.org/subjectSpecies',
-                label: 'Jason Bourne',
-              },
-            ],
-            organisations: [
-              {
-                identifier: 'http://example.org/subjectSpecies',
-                label: 'MI6',
-              },
-              {
-                identifier: 'http://example.org/subjectSpecies',
-                label: 'CIA',
-              },
-            ],
-            license: {
-              identifier: 'http://example.org/subjectSpecies',
-              label: 'License to Kill',
-            },
-            mType: [
-              {
-                identifier: 'http://example.org/subjectSpecies',
-                label: 'mType1',
-              },
-              {
-                identifier: 'http://example.org/subjectSpecies',
-                label: 'mType2',
-              },
-            ],
-          },
-        },
-      ],
+      hits: data,
       max_score: 0.03460473,
       total: {
         relation: 'eq',
@@ -75,8 +16,8 @@ function setUpDeltaProxy(app: express.Express, apiEndpoint: string) {
     _shards: {
       failed: 0,
       skipped: 0,
-      successful: 1,
-      total: 1,
+      successful: 35,
+      total: 35,
     },
   };
 
