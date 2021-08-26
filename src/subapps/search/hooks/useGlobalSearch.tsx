cee7016463a9127ce8a3a809d92d5961797a8da0
Stafeeva <n.stafeeva@gmail.com>
2021-08-26 15:59:38 +0200

    Remove logging

--- src/subapps/search/hooks/useGlobalSearch.tsx
+++ src/subapps/search/hooks/useGlobalSearch.tsx
@@ -182,8 +182,6 @@ function makeColumnConfig(
   filteredFields: string[],
   sortedFields: ESSortField[]
 ) {
-  console.log('searchConfig', searchConfig);
-
   return searchConfig.fields.map((field: ConfigField) => {
     const sorted = sortedFields.find(s => s.fieldName === field.name);
 
