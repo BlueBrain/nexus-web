f0bdf41842238a1708fda7ed69d4dcc80f94d65e
Stafeeva <n.stafeeva@gmail.com>
2021-09-27 16:01:43 +0200

    Fetch deletion config

--- src/subapps/admin/components/Projects/ProjectWarning.tsx
+++ src/subapps/admin/components/Projects/ProjectWarning.tsx
@@ -1,16 +1,22 @@
 import * as React from 'react';
 import { Alert } from 'antd';
+import moment from 'moment';
 
 import './ProjectWarning.less';
 
-const ProjectWarning: React.FC<{}> = () => {
+const ProjectWarning: React.FC<{
+  projectLastUpdatedAt: string;
+  duration: number;
+}> = ({ projectLastUpdatedAt, duration }) => {
+  const deletionTime = moment(projectLastUpdatedAt)
+    .add(duration, 'seconds')
+    .format('LLL');
+
+  const message = `Your project and data will be automatically deleted on ${deletionTime} if you don't update any resources until then. After deletion, a new empty project will be created for you if you connect to Nexus again.`;
+
   return (
     <div className="project-warning">
-      <Alert
-        message="Your project and data will be automatically deleted on {projectCreatedAt + duration}. After that date, a new empty project will be created for you if you connect to Nexus again."
-        type="info"
-        showIcon
-      />
+      <Alert message={message} type="info" showIcon />
     </div>
   );
 };
--- src/subapps/admin/containers/ProjectToDeleteContainer.tsx
+++ src/subapps/admin/containers/ProjectToDeleteContainer.tsx
@@ -8,25 +8,56 @@ const ProjectToDeleteContainer: React.FC<{
   orgLabel: string;
   projectLabel: string;
 }> = ({ orgLabel, projectLabel }) => {
-  const [duration, setDuration] = React.useState<any>();
+  const [idleInterval, setIdleInterval] = React.useState<any>();
+  const [lastUpdated, setLastUpdated] = React.useState<any>();
   const nexus = useNexusContext();
 
   React.useEffect(() => {
+    loadDeletionConfig();
     loadStatistics();
   });
+
+  const temp = {
+    '@context':
+      'https://bluebrain.github.io/nexus/contexts/project-deletion.json',
+    '@type': 'ProjectDeletionConfig',
+    _idleIntervalInSeconds: 600,
+    _idleCheckPeriodInSeconds: 5,
+    _deleteDeprecatedProjects: true,
+    _includedProjects: ['some.+'],
+    _excludedProjects: ['.+'],
+  };
 
   const loadStatistics = async () => {
     await nexus.Project.statistics(orgLabel, projectLabel)
       .then((response: ProjectStatistics) => {
         // @ts-ignore
-        setDuration(response.duration);
+        setLastUpdated(response.lastProcessedEventDateTime);
       })
       .catch(error => {
         // fail silently
       });
   };
 
-  return <ProjectWarning />;
+  const loadDeletionConfig = async () => {
+    // @ts-ignore
+    await nexus.Project.deletionConfig()
+      .then((response: any) => {
+        // @ts-ignore
+        setIdleInterval(temp._idleIntervalInSeconds);
+      })
+      // @ts-ignore
+      .catch(error => {
+        // fail silently
+      });
+  };
+
+  return (
+    <ProjectWarning
+      projectLastUpdatedAt={lastUpdated}
+      duration={idleInterval}
+    />
+  );
 };
 
 export default ProjectToDeleteContainer;
