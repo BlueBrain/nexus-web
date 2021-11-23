45a77bb4b0948b448c88c6ac78d7b34a0190150b
Nick Wells <wells.n@gmail.com>
2021-11-23 14:11:19 +0100

    2944 custom views to be listed in query tab (#995)
    * Support react.path being an array as supported by react-router
    
    react-router supports specifying multiple paths for a specified
    route by passing an array. Modify fusion code to also support
    this such that we can take advantage. Specifying multiple
    paths for a single route allows us to specify multiple paths
    for a single component without the component remounting when
    the path changes.
    
    * Allow user to select view to target for query

--- src/subapps/admin/views/ElasticSearchQueryView.tsx
+++ src/subapps/admin/views/ElasticSearchQueryView.tsx
@@ -1,18 +1,25 @@
 import * as React from 'react';
-import { useRouteMatch, useLocation } from 'react-router';
+import { useRouteMatch, useLocation, useHistory } from 'react-router';
 import * as queryString from 'query-string';
-import { ViewList, DEFAULT_ELASTIC_SEARCH_VIEW_ID } from '@bbp/nexus-sdk';
+import { ViewList, DEFAULT_ELASTIC_SEARCH_VIEW_ID, View } from '@bbp/nexus-sdk';
 import { useNexusContext } from '@bbp/react-nexus';
 
 import ElasticSearchQueryContainer from '../containers/ElasticSearchQuery';
+import { Col, Row, Select } from 'antd';
+import { getResourceLabel } from '../../../shared/utils';
+import useNotification from '../../../shared/hooks/useNotification';
+import { useAdminSubappContext } from '..';
 
 const ElasticSearchQueryView: React.FunctionComponent = (): JSX.Element => {
+  const subapp = useAdminSubappContext();
   const match = useRouteMatch<{
     orgLabel: string;
     projectLabel: string;
     viewId: string;
   }>();
   const location = useLocation();
+  const history = useHistory();
+  const notification = useNotification();
   const {
     params: { orgLabel, projectLabel, viewId },
   } = match || {
@@ -22,7 +29,7 @@ const ElasticSearchQueryView: React.FunctionComponent = (): JSX.Element => {
       viewId: '',
     },
   };
-  const [, setViews] = React.useState<ViewList>({
+  const [{ _results: views }, setViews] = React.useState<ViewList>({
     '@context': {},
     _total: 0,
     _results: [],
@@ -30,24 +37,66 @@ const ElasticSearchQueryView: React.FunctionComponent = (): JSX.Element => {
   const nexus = useNexusContext();
   const query = queryString.parse(location.search).query;
 
+  const [selectedView, setSelectedView] = React.useState<string>(
+    viewId ? decodeURIComponent(viewId) : DEFAULT_ELASTIC_SEARCH_VIEW_ID
+  );
+
+  React.useEffect(() => {
+    history.replace(
+      `/${
+        subapp.namespace
+      }/${orgLabel}/${projectLabel}/query/${encodeURIComponent(selectedView)}`
+    );
+  }, [selectedView]);
+
+  const { Option } = Select;
+
+  const menu = (
+    <Row>
+      <Col span={24}>
+        <Select
+          value={selectedView as string}
+          onChange={v => setSelectedView(v)}
+          style={{ width: '100%' }}
+          defaultActiveFirstOption={false}
+        >
+          {[views]
+            .flat()
+            .filter(v => v['@type']?.includes('ElasticSearchView'))
+            .map((view: View, index: number) => {
+              return (
+                <Option key={index} value={view['@id']}>
+                  {getResourceLabel(view)}
+                </Option>
+              );
+            })}
+        </Select>
+      </Col>
+    </Row>
+  );
+
   React.useEffect(() => {
     nexus.View.list(orgLabel, projectLabel)
-      .then(setViews)
-      .catch(() => {
-        // 503 ?
+      .then(result => {
+        setViews(result);
+      })
+      .catch(error => {
+        notification.error({
+          message: 'Problem loading Views',
+          description: error.message,
+        });
       });
   }, [orgLabel, projectLabel]);
 
   return (
     <>
+      {menu}
       <div className="view-view view-container -unconstrained-width">
         <ElasticSearchQueryContainer
           orgLabel={orgLabel}
           projectLabel={projectLabel}
           initialQuery={query ? JSON.parse(`${query}`) : null}
-          viewId={
-            viewId ? viewId : encodeURIComponent(DEFAULT_ELASTIC_SEARCH_VIEW_ID)
-          }
+          viewId={encodeURIComponent(selectedView)}
         />
       </div>
     </>
