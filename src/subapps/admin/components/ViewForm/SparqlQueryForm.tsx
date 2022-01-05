1c65a57bfbdbfb7af6b43625f7ae1245111c8a2d
Dhanesh Neela Mana <dhanesh.n.m19@gmail.com>
2022-01-05 20:50:44 +0100

    refactor

--- src/subapps/admin/components/ViewForm/SparqlQueryForm.tsx
+++ src/subapps/admin/components/ViewForm/SparqlQueryForm.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Form, Button } from 'antd';
+import { Button } from 'antd';
 import { SparqlViewQueryResponse } from '@bbp/nexus-sdk';
 
 import SparqlQueryResults, { NexusSparqlError } from './SparqlQueryResults';
