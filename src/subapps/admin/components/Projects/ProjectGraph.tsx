121cdcd7164ef1f76803bd659c996f45dd0ab9f6
Dhanesh Neela Mana <dhanesh.neelamana@epfl.ch>
2022-02-09 11:23:27 +0100

    Fix number formating for GraphAnalytics Panel (#1043)
    * Fix number formatting for Graph Analytics Panel
    
    * reduce the text size for title
    
    * fix width for the graph canvas

--- src/subapps/admin/components/Projects/ProjectGraph.tsx
+++ src/subapps/admin/components/Projects/ProjectGraph.tsx
@@ -94,7 +94,7 @@ const ProjectGraph: React.FC<{
   });
 
   return (
-    <div style={{ width: '80%', height: '100%' }}>
+    <div style={{ width: '100%', height: '100%' }}>
       <div
         style={{
           width: '100%',
