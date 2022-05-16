044d47031a0192749bd99c48c99c4097e2bf195c
Dhanesh Neela Mana <dhanesh.neelamana@epfl.ch>
2022-05-16 14:30:13 +0200

    Handle deleting dashboards from workspaces (#1098)

--- src/subapps/studioLegacy/containers/WorkspaceMenuContainer.tsx
+++ src/subapps/studioLegacy/containers/WorkspaceMenuContainer.tsx
@@ -482,7 +482,7 @@ const WorkspaceMenu: React.FC<WorkspaceMenuProps> = ({
           projectLabel,
           selectedWorkspace['@id'],
           indexToRemove,
-          [...dashboards]
+          [...currentDashboards]
         );
         notification.success({
           message: `Removed ${selectedDashboard.label}`,
@@ -679,14 +679,16 @@ const WorkspaceMenu: React.FC<WorkspaceMenuProps> = ({
         ) : null}
 
         <Modal
-          title="Remove DashBoard"
+          title="Remove Dashboard"
           visible={deleteDashBoardConfirmation}
           onCancel={() => {
             setDeleteDashBoardConfirmation(false);
           }}
           onOk={removeDashBoardCallback}
         >
-          <p>Are you sure you want to remove ?</p>
+          <p>
+            Are you sure you want to remove {`${selectedDashboard?.label}`} ?
+          </p>
         </Modal>
         <Modal
           visible={showEditTableForm}
