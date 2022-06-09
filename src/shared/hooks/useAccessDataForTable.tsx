efe004a296bab3902adadc8de0fb63ea0acb86f1
Dhanesh Neela Mana <dhanesh.n.m19@gmail.com>
2022-06-09 13:04:48 +0200

    Set the right id for column selection

--- src/shared/hooks/useAccessDataForTable.tsx
+++ src/shared/hooks/useAccessDataForTable.tsx
@@ -292,6 +292,8 @@ export const useAccessDataForTable = (
   const [searchValue, setSearchValue] = React.useState<string>('');
   const { addResourceCollectionToCart } = React.useContext(CartContext);
   const onSelect = (selectedRowKeys: React.Key[], selectedRows: Resource[]) => {
+    console.log(selectedRowKeys);
+    console.log(selectedRows);
     setSelectedRows(selectedRowKeys);
     if (
       dataResult?.data?.view &&
