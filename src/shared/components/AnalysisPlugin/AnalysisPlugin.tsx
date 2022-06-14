d7af927466fbadafcda7330201b1be0404c06739
Nick Smit <nick.smit@gmail.com>
2022-06-14 16:14:41 +0200

    dropdown added

--- src/shared/components/AnalysisPlugin/AnalysisPlugin.tsx
+++ src/shared/components/AnalysisPlugin/AnalysisPlugin.tsx
@@ -34,6 +34,7 @@ enum ActionType {
   EDIT = 'edit',
   INITIALIZE = 'initialize',
   SELECT_ASSET = 'select_asset',
+  ACTIVATE_ANALYSES = 'activate_analyses',
 }
 
 const DEFAULT_SCALE = 50;
@@ -42,17 +43,20 @@ type AnalysesAction =
   | { type: ActionType.RESCALE; payload: number }
   | { type: ActionType.EDIT; payload: { analysisId: string } }
   | { type: ActionType.INITIALIZE }
-  | { type: ActionType.SELECT_ASSET; payload: { assetId: string } };
+  | { type: ActionType.SELECT_ASSET; payload: { assetId: string } }
+  | { type: ActionType.ACTIVATE_ANALYSES; payload: { analyses: string[] } };
 
 type AnalysesState = {
   scale: number;
   mode: 'view' | 'edit';
   editing?: string;
   selected?: string[];
+  activeAnalyses?: string[];
 };
-const initState = (): AnalysesState => ({
+const initState = (activeAnalysisId?: string): AnalysesState => ({
   scale: DEFAULT_SCALE,
   mode: 'view',
+  activeAnalyses: activeAnalysisId ? [activeAnalysisId] : [],
 });
 
 export default ({ analyses }: AnalysisPluginProps) => {
@@ -90,44 +94,63 @@ export default ({ analyses }: AnalysisPluginProps) => {
             action.payload.assetId,
           ],
         };
+      case ActionType.ACTIVATE_ANALYSES:
+        return {
+          ...state,
+          activeAnalyses: action.payload.analyses,
+        };
       case ActionType.INITIALIZE:
-        return initState();
-
+        console.log('ANALYSES LENGTH', analyses.length > 0);
+        console.log('ANALYSES FIRST', analyses[0].id);
+        return analyses.length > 0 ? initState(analyses[0].id) : initState();
       default:
         throw new Error();
     }
   };
 
-  const [{ scale, mode, editing, selected }, dispatch] = React.useReducer(
-    reducer,
-    {
-      scale: DEFAULT_SCALE,
-      mode: 'view',
-    }
-  );
+  const [
+    { scale, mode, editing, selected, activeAnalyses },
+    dispatch,
+  ] = React.useReducer(reducer, {
+    scale: DEFAULT_SCALE,
+    mode: 'view',
+  });
+
   const { Option } = Select;
 
-  const onChange = (value: string) => {
-    console.log(`selected ${value}`);
+  const onAnalysesChange = (value: string[]) => {
+    dispatch({
+      type: ActionType.ACTIVATE_ANALYSES,
+      payload: { analyses: value },
+    });
   };
 
   const onSearch = (value: string) => {
-    console.log('search:', value);
+    let res = analyses.filter(a =>
+      a.name.toLowerCase().includes(value.toLowerCase())
+    );
+    console.log('SEARCH', res);
+    return res;
   };
+
   // alert(`editing`);
   return (
     <div className="analysis">
       <label>Selected</label>
       {selected}
+      <br />
+      {activeAnalyses}
       <>
         {mode === 'view' && (
           <Row className="analysisTools">
-            <Col span={2}>
+            <Col span={8}>
               <Select
                 showSearch
-                placeholder="Select Analysis"
+                mode="multiple"
+                placeholder="Please select"
+                style={{ width: '100%' }}
                 optionFilterProp="children"
-                onChange={onChange}
+                onChange={onAnalysesChange}
                 onSearch={onSearch}
                 filterOption={(input, option) =>
                   ((option!.children as unknown) as string)
@@ -140,7 +163,7 @@ export default ({ analyses }: AnalysisPluginProps) => {
                 ))}
               </Select>
             </Col>
-            <Col span={2}>
+            <Col span={3}>
               <Slider
                 tooltipVisible={false}
                 value={scale}
@@ -153,82 +176,88 @@ export default ({ analyses }: AnalysisPluginProps) => {
             </Col>
           </Row>
         )}
-        {analyses.map((a, i) => (
-          <section key={i}>
-            <h1 aria-label="Analysis Name">
-              {(mode === 'view' || editing !== a.id) && a.name}
-              {mode === 'edit' && editing === a.id && (
-                <>
-                  <Input type="text" value={a.name} style={{ width: '60%' }} />
-                  <Button
-                    type="default"
-                    onClick={() => dispatch({ type: ActionType.INITIALIZE })}
-                  >
-                    Cancel
-                  </Button>
-                  <Button
-                    type="primary"
-                    onClick={() => dispatch({ type: ActionType.INITIALIZE })}
-                  >
-                    Save
-                  </Button>
-                </>
-              )}
-              {mode === 'view' && (
-                <Dropdown.Button
-                  overlay={
-                    <Menu onClick={() => {}}>
-                      <Menu.Item
-                        onClick={() =>
-                          dispatch({
-                            type: ActionType.EDIT,
-                            payload: { analysisId: a.id },
-                          })
-                        }
-                      >
-                        Edit
-                      </Menu.Item>
-                      <Menu.Item onClick={() => console.log('download')}>
-                        Download
-                      </Menu.Item>
-                    </Menu>
-                  }
-                  icon={<MoreOutlined />}
-                />
-              )}
-            </h1>
-            <p aria-label="Analysis Description">
-              {(mode === 'view' || editing !== a.id) && a.description}
-              {mode === 'edit' && editing === a.id && (
-                <Input.TextArea value={a.description} />
-              )}
-            </p>
-            <section aria-label="Analysis Assets" className="assets">
-              {a.analyses.map((asset, i) => (
-                <div
-                  key={i}
-                  aria-label="Analysis Asset"
-                  className={`asset ${
-                    selected && selected.findIndex(v => v === asset.id) > -1
-                      ? 'selected'
-                      : ''
-                  }`}
-                  onClick={() => {
-                    if (!(mode === 'edit' && editing === a.id)) {
-                      return;
+        {analyses
+          .filter(a => activeAnalyses?.includes(a.id))
+          .map((a, i) => (
+            <section key={i}>
+              <h1 aria-label="Analysis Name">
+                {(mode === 'view' || editing !== a.id) && a.name}
+                {mode === 'edit' && editing === a.id && (
+                  <>
+                    <Input
+                      type="text"
+                      value={a.name}
+                      style={{ width: '60%' }}
+                    />
+                    <Button
+                      type="default"
+                      onClick={() => dispatch({ type: ActionType.INITIALIZE })}
+                    >
+                      Cancel
+                    </Button>
+                    <Button
+                      type="primary"
+                      onClick={() => dispatch({ type: ActionType.INITIALIZE })}
+                    >
+                      Save
+                    </Button>
+                  </>
+                )}
+                {mode === 'view' && (
+                  <Dropdown.Button
+                    overlay={
+                      <Menu onClick={() => {}}>
+                        <Menu.Item
+                          onClick={() =>
+                            dispatch({
+                              type: ActionType.EDIT,
+                              payload: { analysisId: a.id },
+                            })
+                          }
+                        >
+                          Edit
+                        </Menu.Item>
+                        <Menu.Item onClick={() => console.log('download')}>
+                          Download
+                        </Menu.Item>
+                      </Menu>
                     }
-                    dispatch({
-                      type: ActionType.SELECT_ASSET,
-                      payload: { assetId: asset.id },
-                    });
-                  }}
-                >
-                  {asset.preview({ scale: scale, mode: mode })}
-                </div>
-              ))}
+                    icon={<MoreOutlined />}
+                  />
+                )}
+              </h1>
+              <p aria-label="Analysis Description">
+                {(mode === 'view' || editing !== a.id) && a.description}
+                {mode === 'edit' && editing === a.id && (
+                  <Input.TextArea value={a.description} />
+                )}
+              </p>
+              <section aria-label="Analysis Assets" className="assets">
+                {a.analyses.map((asset, i) => (
+                  <div
+                    key={i}
+                    aria-label="Analysis Asset"
+                    className={`asset ${
+                      selected && selected.findIndex(v => v === asset.id) > -1
+                        ? 'selected'
+                        : ''
+                    }`}
+                    onClick={() => {
+                      if (!(mode === 'edit' && editing === a.id)) {
+                        return;
+                      }
+                      dispatch({
+                        type: ActionType.SELECT_ASSET,
+                        payload: { assetId: asset.id },
+                      });
+                    }}
+                  >
+                    {asset.preview({ scale: scale, mode: mode })}
+                  </div>
+                ))}
+              </section>
             </section>
-          </section>
-        ))}
+          ))}
       </>
     </div>
   );
