09ee47ab14747aece208ee28cbca7647cecab9e9
Smit Nick <smit@bb-c02d62n2ml85.epfl.ch>
2022-06-24 16:12:39 +0200

    initial test for analysis plugin

--- src/shared/containers/AnalysisPlugin/__tests__/AnalysisPluginContainer.spec.tsx
+++ src/shared/containers/AnalysisPlugin/__tests__/AnalysisPluginContainer.spec.tsx
@@ -1,172 +1,69 @@
-import { NexusProvider } from '@bbp/react-nexus';
-import { createNexusClient, Resource } from '@bbp/nexus-sdk';
-import AnalysisPluginContainer from '../AnalysisPlugin/AnalysisPluginContainer';
-import fetch from 'node-fetch';
-import { render, server, screen, fireEvent } from '../../../../utils/testUtil';
-import { rest } from 'msw';
-import '@testing-library/jest-dom';
-import { act } from 'react-dom/test-utils';
-describe('VideoPluginContainer', () => {
+import { NexusProvider } from '@bbp/react-nexus'
+import { createNexusClient, Resource } from '@bbp/nexus-sdk'
+import * as React from 'react'
+import fetch from 'node-fetch'
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import AnalysisPluginContainer from '../AnalysisPluginContainer'
+import {
+  render,
+  server,
+  fireEvent,
+  waitFor,
+  screen,
+} from '../../../../utils/testUtil'
+import '@testing-library/jest-dom'
+import { act } from 'react-dom/test-utils'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+
+describe('Analysis Plugin', () => {
   // establish API mocking before all tests
-  beforeAll(() => server.listen());
+  beforeAll(() => server.listen())
   // reset any request handlers that are declared as a part of our tests
   // (i.e. for testing one-time error scenarios)
-  afterEach(() => server.resetHandlers());
+  afterEach(() => server.resetHandlers())
   // clean up once the tests are done
-  afterAll(() => server.close());
+  afterAll(() => server.close())
 
   const nexus = createNexusClient({
     fetch,
     uri: 'https://localhost:3000',
-  });
+  })
+  const mockState = {
+    config: {
+      apiEndpoint: 'https://localhost:3000',
+      analysisPluginSparqlDataQuery: 'detailedCircuit',
+    },
+  }
+  const queryClient = new QueryClient()
+  const mockStore = configureStore()
+  jest.mock('react-redux', () => {
+    const ActualReactRedux = jest.requireActual('react-redux')
+    return {
+      ...ActualReactRedux,
+      useSelector: jest.fn().mockImplementation(() => {
+        return mockState
+      }),
+    }
+  })
 
-  // react-player component used by VideoPluginContainer calls window.fetch so specify the node one
-  // @ts-ignore
-  window.fetch = fetch;
-
-  const resource = ({
-    '@context': [
-      'https://bbp.neuroshapes.org',
-      'https://bluebrain.github.io/nexus/contexts/resource.json',
-    ],
-    '@id': 'videoresourceId',
-    '@type': ['Entity'],
-    video: [
-      {
-        name: 'video',
-        embedUrl: 'https://youtu.be/d4gSor3KyIw',
-      },
-      {
-        name: 'cool brain video',
-        uploadDate: '2020-07-10 15:00:00.000',
-        embedUrl: 'https://www.example.com/embed/123',
-      },
-    ],
-  } as unknown) as Resource;
-  server.use(
-    rest.get(
-      'https://localhost:3000/resources/org/project/_/videoresourceId',
-      (req, res, ctx) => {
-        const mockResponse = resource;
-
-        return res(
-          // Respond with a 200 status code
-          ctx.status(200),
-          ctx.json(mockResponse)
-        );
-      }
-    )
-  );
-  it('renders with well formatted data', async () => {
+  it('renders with  prop values', async () => {
+    const store = mockStore(mockState)
     await act(async () => {
       const { container } = await render(
-        <NexusProvider nexusClient={nexus}>
-          <VideoPluginContainer
-            resource={resource}
-            orgLabel="org"
-            projectLabel="project"
-            collapsed={false}
-            handleCollapseChanged={jest.fn}
-          ></VideoPluginContainer>
-        </NexusProvider>
-      );
-      expect(container).toMatchSnapshot();
-    });
-  });
-
-  it('fails to render when json does not match schema', async () => {
-    const resource = ({
-      '@context': [
-        'https://bbp.neuroshapes.org',
-        'https://bluebrain.github.io/nexus/contexts/resource.json',
-      ],
-      '@id': 'videoresourceId',
-      '@type': [
-        'Entity',
-        'InVitroSliceReconstructedPatchedNeuron',
-        'https://bbp-nexus.epfl.ch/vocabs/bbp/neurosciencegraph/core/v0.1.0/InVitroSliceReconstructedPatchedNeuron',
-        'ReconstructedPatchedCell',
-        'ReconstructedCell',
-        'https://bbp-nexus.epfl.ch/vocabs/bbp/neurosciencegraph/core/v0.1.0/ReconstructedPatchedCell',
-        'https://bbp-nexus.epfl.ch/vocabs/bbp/neurosciencegraph/core/v0.1.0/ReconstructedCell',
-        'Dataset',
-      ],
-      video: [
-        {
-          name: 'video',
-          embedurl: ['yyy'],
-        },
-        {
-          name: 'cool brain video',
-          embedurl: ['yyy'],
-        },
-      ],
-    } as unknown) as Resource;
-    server.use(
-      rest.get(
-        'https://localhost:3000/resources/org/project/_/videoresourceId',
-        (req, res, ctx) => {
-          const mockResponse = {
-            '@context': [
-              'https://bbp.neuroshapes.org',
-              'https://bluebrain.github.io/nexus/contexts/resource.json',
-            ],
-            '@id': 'video_resource_id',
-            '@type': ['Entity'],
-            video: [
-              {
-                name: 'video',
-                embedurl: ['yyy'],
-              },
-              {
-                name: 'cool brain video',
-                embedurl: ['yyy'],
-              },
-            ],
-          };
-
-          return res(
-            // Respond with a 200 status code
-            ctx.status(200),
-            ctx.json(mockResponse)
-          );
-        }
+        <Provider store={store}>
+          <QueryClientProvider client={queryClient}>
+            <NexusProvider nexusClient={nexus}>
+              <AnalysisPluginContainer
+                projectLabel='projectLabel'
+                orgLabel='orgLabel'
+                resourceId='resourceId'
+              ></AnalysisPluginContainer>
+            </NexusProvider>
+          </QueryClientProvider>
+        </Provider>
       )
-    );
-    await act(async () => {
-      const { container } = await render(
-        <NexusProvider nexusClient={nexus}>
-          <VideoPluginContainer
-            resource={resource}
-            orgLabel="org"
-            projectLabel="project"
-            collapsed={false}
-            handleCollapseChanged={jest.fn}
-          ></VideoPluginContainer>
-        </NexusProvider>
-      );
-      expect(container).toMatchSnapshot();
-    });
-  });
-
-  it('When clicked fires collapse event handler', async () => {
-    const collapseHandler = jest.fn();
-    await act(async () => {
-      const { container } = await render(
-        <NexusProvider nexusClient={nexus}>
-          <VideoPluginContainer
-            resource={resource}
-            orgLabel="org"
-            projectLabel="project"
-            collapsed={false}
-            handleCollapseChanged={collapseHandler}
-          ></VideoPluginContainer>
-        </NexusProvider>
-      );
-      const panel = await screen.getByRole('button');
-      expect(panel).toBeVisible();
-      fireEvent.click(panel);
-      expect(collapseHandler).toHaveBeenCalled();
-    });
-  });
-});
+      expect(container).toMatchSnapshot()
+    })
+  })
+})
