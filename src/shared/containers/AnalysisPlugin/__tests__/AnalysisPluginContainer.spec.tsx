6d0d0e10e036d89e4964ca20e87ee137c0bf53c0
Nick Smit <nick.smit@gmail.com>
2022-07-25 08:08:07 +0200

    aded test for edit, delete button showing up

--- src/shared/containers/AnalysisPlugin/__tests__/AnalysisPluginContainer.spec.tsx
+++ src/shared/containers/AnalysisPlugin/__tests__/AnalysisPluginContainer.spec.tsx
@@ -831,4 +831,134 @@ describe('Analysis Plugin', () => {
       )
     ).toBe('Our Very First Analysis Report!');
   });
+
+	  it('On image delete, selected images are no longer visible', async () => {
+      server.use(
+        rest.post(
+          'https://localhost:3000/views/orgLabel/projectLabel/graph/sparql',
+          (req, res, ctx) => {
+            const mockResponse = {
+              head: {
+                vars: [
+                  'container_resource_id',
+                  'container_resource_name',
+                  'analysis_report_id',
+                  'analysis_report_name',
+                  'analysis_report_description',
+                  'created_by',
+                  'created_at',
+                  'asset_content_url',
+                  'asset_encoding_format',
+                  'asset_name',
+                  'self',
+                ],
+              },
+              results: {
+                bindings: [
+                  {
+                    analysis_report_description: {
+                      type: 'literal',
+                      value:
+                        "This is our analysis report. Isn't it great! Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. 555",
+                    },
+                    analysis_report_id: {
+                      type: 'uri',
+                      value:
+                        'https://dev.nise.bbp.epfl.ch/nexus/v1/resources/bbp-users/nicholas/_/MyTestAnalysisReport1',
+                    },
+                    analysis_report_name: {
+                      type: 'literal',
+                      value: 'Our Very First Analysis Report!',
+                    },
+                    asset_content_url: {
+                      type: 'literal',
+                      value:
+                        'https://dev.nise.bbp.epfl.ch/nexus/v1/resources/bbp-users/nicholas/_/d3d1cc48-9547-4c9c-a08f-f281ffb458cc',
+                    },
+                    asset_encoding_format: {
+                      type: 'literal',
+                      value: 'image/png',
+                    },
+                    asset_name: {
+                      type: 'literal',
+                      value: 'insta_logo_large.png',
+                    },
+                    container_resource_id: {
+                      type: 'uri',
+                      value:
+                        'https://dev.nise.bbp.epfl.ch/nexus/v1/resources/bbp-users/nicholas/_/MyTestAnalysis1',
+                    },
+                    container_resource_name: {
+                      type: 'literal',
+                      value: 'Analysis container',
+                    },
+                    created_at: {
+                      datatype: 'http://www.w3.org/2001/XMLSchema#dateTime',
+                      type: 'literal',
+                      value: '2022-06-17T04:14:06.357Z',
+                    },
+                    created_by: {
+                      type: 'uri',
+                      value:
+                        'https://dev.nise.bbp.epfl.ch/nexus/v1/realms/local/users/localuser',
+                    },
+                    self: {
+                      type: 'uri',
+                      value:
+                        'https://dev.nise.bbp.epfl.ch/nexus/v1/resources/bbp-users/nicholas/_/MyTestAnalysisReport1',
+                    },
+                  },
+                ],
+              },
+            };
+
+            return res(
+              // Respond with a 200 status code
+              ctx.status(200),
+              ctx.json(mockResponse)
+            );
+          }
+        )
+      );
+      const history = createMemoryHistory({});
+
+      const store = mockStore(mockState);
+      await act(async () => {
+        await render(
+          <Router history={history}>
+            <Provider store={store}>
+              <QueryClientProvider client={queryClient}>
+                <NexusProvider nexusClient={nexus}>
+                  <AnalysisPluginContainer
+                    projectLabel="projectLabel"
+                    orgLabel="orgLabel"
+                    resourceId="https://dev.nise.bbp.epfl.ch/nexus/v1/resources/bbp-users/nicholas/_/MyTestAnalysis1"
+                  ></AnalysisPluginContainer>
+                </NexusProvider>
+              </QueryClientProvider>
+            </Provider>
+          </Router>
+        );
+      });
+
+      await act(async () => {
+        const optionsButton = await screen.findByRole('button', {
+          name: 'Options',
+        });
+				fireEvent.mouseEnter(optionsButton);
+      });
+			
+			await act(async () => {
+				const editButton = await screen.findByRole('menuitem', {
+          	name: 'Edit',
+        	});
+        	fireEvent.mouseEnter(editButton);
+			});
+
+      expect(
+				await waitFor(() =>
+          screen.getByRole('button', { name: 'Delete' })
+        )
+      ).toBeInTheDocument();
+    });
 });
