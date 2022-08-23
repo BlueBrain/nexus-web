c689f0a3f073238d4c83cd889dafef15b64e54ec
Nick Smit <nick.smit@gmail.com>
2022-08-23 15:06:54 +0200

    continueing to divide components so we can manage a different view for the new report

--- src/shared/components/AnalysisPlugin/CategoryWidget.tsx
+++ src/shared/components/AnalysisPlugin/CategoryWidget.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import * as React from 'react'
 import { Button } from 'antd'
 import { flatten, map, uniq, intersection } from 'lodash'
@@ -6,6 +7,38 @@ import './Categories.less'
 import { CategoryWidgetProps } from '../../types/plugins/report'
 
 import { REPORT_CATEGORIES as CATEGORIES } from '../../../constants'
+=======
+import * as React from 'react';
+import { Button } from 'antd';
+import { without, flatten, map, uniq, intersection } from 'lodash';
+import { FolderAddOutlined, InfoCircleOutlined } from '@ant-design/icons';
+import './Categories.less';
+import {
+  ActionType,
+  AnalysesAction,
+} from '../../../shared/containers/AnalysisPlugin/AnalysisPluginContainer';
+
+import { AnalysisReport } from '../../../shared/components/AnalysisPlugin/AnalysisPlugin';
+
+const CATEGORIES = {
+  circuit: [
+    'Anatomical',
+    'Connectivity',
+    'Volumetric',
+    'Morphometric',
+    'Synapse',
+  ],
+  simulation: ['Spiking', 'Soma voltage', 'LFP', 'VSD', 'Plasticity'],
+};
+
+type CategoryWidgetProps = {
+  analysisReports?: AnalysisReport[];
+  mode: 'view' | 'edit' | 'create';
+  selectedCategories: string[];
+  selectCategory: (value: string) => void;
+  dispatch: (action: AnalysesAction) => void;
+};
+>>>>>>> 2ae36754 (continueing to divide components so we can manage a different view for the new report)
 
 const CategoryWidget = ({
   dispatch,
@@ -20,6 +53,7 @@ const CategoryWidget = ({
     mode === 'create'
       ? CATEGORIES.circuit
       : intersection(
+<<<<<<< HEAD
           uniq(flatten(map(analysisReports, 'categories'))),
           CATEGORIES.circuit
         )
@@ -52,5 +86,51 @@ const CategoryWidget = ({
     </>
   )
 }
+=======
+          uniq(flatten(map(analysisReports, 'containerCategory'))),
+          CATEGORIES.circuit
+        );
+
+  return (
+    <div className="categories">
+      {mode !== 'create' && (
+        <h3>
+          Categories
+          <Button
+            type="primary"
+            title="Add Analysis Report"
+            aria-label="Add Analysis Report"
+            onClick={() => {
+              dispatch({
+                type: ActionType.ADD_ANALYSIS_REPORT,
+              });
+            }}
+          >
+            Add Report
+            <FolderAddOutlined />
+          </Button>
+        </h3>
+      )}
+      <p>you may select one or multiple from the list</p>
+      {CATEGORIES.circuit
+        .filter(o => availableCategories.includes(o))
+        .map((object, i) => (
+          <Button
+            type="default"
+            onClick={() => selectCategory(object)}
+            className={`group-buttons ${
+              selectedCategories.includes(object) ? 'active' : ''
+            }`}
+          >
+            <h5>
+              {object}
+              <InfoCircleOutlined />
+            </h5>
+          </Button>
+        ))}
+    </div>
+  );
+};
+>>>>>>> 2ae36754 (continueing to divide components so we can manage a different view for the new report)
 
 export default CategoryWidget
--- src/shared/components/AnalysisPlugin/NewReportForm.tsx
+++ src/shared/components/AnalysisPlugin/NewReportForm.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import * as React from 'react'
 import { Form, Button, Input, Select, Typography } from 'antd'
 import './NewReportForm.less'
@@ -54,6 +55,50 @@ const NewReportForm = ({
         <TextArea rows={10} aria-label='Analysis Description' />
       </Form.Item>
       <Form.Item label='3. Categories' aria-label='Analysis Categories'>
+=======
+import * as React from 'react';
+import { Form, Button, Input} from 'antd';
+import {
+  ActionType,
+  AnalysesAction,
+} from '../../../shared/containers/AnalysisPlugin/AnalysisPluginContainer';
+import './NewReportForm.less';
+import CategoryWidget from './CategoryWidget';
+import { without } from 'lodash';
+
+const { TextArea } = Input;
+
+type NewReportFormProps = {
+    analysisReportId?: string | undefined;
+    imagePreviewScale: number;
+  dispatch: (action: AnalysesAction) => void;
+};
+
+const NewReportForm = ({
+  analysisReportId,
+  dispatch,
+  imagePreviewScale,
+}: NewReportFormProps) => {
+  const [form] = Form.useForm();
+const [selectedCategories, setSelectedCategories] = React.useState<string[]>(
+  []
+);
+
+const selectCategory = (value: string) => {
+  !selectedCategories.includes(value)
+    ? setSelectedCategories([...selectedCategories, value])
+    : setSelectedCategories(without(selectedCategories, value));
+};
+  return (
+    <Form layout={'vertical'} className="new-report-form">
+      <Form.Item label="Report Name">
+        <Input placeholder="type name here" />
+      </Form.Item>
+      <Form.Item label="Report Description">
+        <TextArea rows={10} />
+      </Form.Item>
+      <Form.Item label="Categories">
+>>>>>>> 2ae36754 (continueing to divide components so we can manage a different view for the new report)
         <CategoryWidget
           dispatch={dispatch}
           mode={'create'}
@@ -61,6 +106,7 @@ const NewReportForm = ({
           selectCategory={selectCategory}
         />
       </Form.Item>
+<<<<<<< HEAD
       <Form.Item label='4. Types' aria-label='Analysis Types'>
         <TypeWidget
           dispatch={dispatch}
@@ -121,3 +167,33 @@ const NewReportForm = ({
 }
 
 export default NewReportForm
+=======
+      <Form.Item>
+        <Button type="primary" size="large">
+          Save
+        </Button>
+      </Form.Item>
+      <Form.Item>
+        <Button
+          style={{ marginRight: '10px' }}
+          type="default"
+          aria-label="Cancel"
+          onClick={() =>
+            dispatch({
+              type: ActionType.INITIALIZE,
+              payload: {
+                scale: imagePreviewScale,
+                analysisReportId: analysisReportId ? [analysisReportId] : [],
+              },
+            })
+          }
+        >
+          Cancel
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default NewReportForm;
+>>>>>>> 2ae36754 (continueing to divide components so we can manage a different view for the new report)
