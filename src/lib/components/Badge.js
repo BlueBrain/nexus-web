a3a34bf9e97cefb45d1e507ca1b962abe368c7e0
Smit Nick <smit@bb-c02d62n2ml85.epfl.ch>
2023-02-06 15:46:55 +0100

    fixed prettier issue with component

--- src/lib/components/Badge.js
+++ src/lib/components/Badge.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Badge = (props) => {
-   return (
-      <div className={`badge ${!props.value ? 'badge--none' :''} `}>
-        <h4 className="heavy">{props.value || 0}</h4>
-      </div>
-     )
-}
+const Badge = props => {
+  return (
+    <div className={`badge ${!props.value ? 'badge--none' : ''} `}>
+      <h4 className="heavy">{props.value || 0}</h4>
+    </div>
+  );
+};
 
 export default Badge;
