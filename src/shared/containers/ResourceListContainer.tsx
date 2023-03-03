d6c2cc4ab0a7dc07200b8770321aaad4aecaa903
Meddah Bilal <meddah@bb-pggwpwnhqj.epfl.ch>
2023-03-03 15:15:51 +0100

    update: set deprecated for elasticSearch query to undefined

--- src/shared/containers/ResourceListContainer.tsx
+++ src/shared/containers/ResourceListContainer.tsx
@@ -178,6 +178,7 @@ const ResourceListContainer: React.FunctionComponent<{
       query: {
         ...list.query,
         type: !!value ? value : undefined,
+        deprecated: undefined,
         from: 0,
       },
     });
