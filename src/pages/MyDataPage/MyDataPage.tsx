60e9ca3b2905176dbc436a863dd149c37a339b5c
Bilal MEDDAH <b_meddah@esi.dz>
2023-06-02 15:56:27 +0200

    [f-3923] update styles

--- src/pages/MyDataPage/MyDataPage.tsx
+++ src/pages/MyDataPage/MyDataPage.tsx
@@ -2,7 +2,7 @@ import { MyData } from '../../shared/canvas';
 
 const MyDataPage = () => {
   return (
-    <div className="my-data-view view-container" style={{ padding: '0 1em'}}>
+    <div className="my-data-view view-container" style={{ padding: '0 1em' }}>
       <MyData />
     </div>
   );
