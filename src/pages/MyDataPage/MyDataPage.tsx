eb54565612c9458a2d9eaaba4693cd9598ce541d
Bilal MEDDAH <b_meddah@esi.dz>
2023-06-02 15:56:08 +0200

    [f-3923] change mydataView to top level page

---
+++ src/pages/MyDataPage/MyDataPage.tsx
@@ -0,0 +1,11 @@
+import { MyData } from '../../shared/canvas';
+
+const MyDataPage = () => {
+  return (
+    <div className="my-data-view view-container" style={{ padding: '0 1em'}}>
+      <MyData />
+    </div>
+  );
+};
+
+export default MyDataPage;
