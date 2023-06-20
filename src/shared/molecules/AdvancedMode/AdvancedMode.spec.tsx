8816ffd4e195ae9080b2f7211f0f60cea6217816
Bilal MEDDAH <b_meddah@esi.dz>
2023-06-20 11:57:11 +0200

    [f-3983] add advanced mode toggle

---
+++ src/shared/molecules/AdvancedMode/AdvancedMode.spec.tsx
@@ -0,0 +1,84 @@
+import '@testing-library/jest-dom'
+import { Provider } from 'react-redux';
+import { act } from 'react-dom/test-utils';
+import { Router,  } from 'react-router-dom';
+import { History } from 'history';
+import { Store } from 'redux';
+
+import { createBrowserHistory } from 'history';
+import { ConnectedRouter } from 'connected-react-router';
+import { createNexusClient } from '@bbp/nexus-sdk';
+import AdvancedModeToggle from './AdvancedMode';
+import configureStore from '../../store';
+import {
+  render,
+  fireEvent,
+  waitFor,
+  screen,
+} from '../../../utils/testUtil';
+
+describe('AdvancedModeToggle', () => {
+  let history: History<unknown>, nexus, store: Store;
+  beforeEach(() => {
+    history = createBrowserHistory({ basename: '/' });
+    nexus = createNexusClient({
+      fetch,
+      uri: 'https://localhost:3000',
+    });
+    store = configureStore(history, { nexus }, {});
+  })
+  it('should toggle advanced mode be in the', async () => {
+    history = createBrowserHistory({ basename: '/' });
+    nexus = createNexusClient({
+      fetch,
+      uri: 'https://localhost:3000',
+    });
+    const store = configureStore(history, { nexus }, {});
+    await act(async () => {
+      await render(
+        <Provider store={store}>
+          <ConnectedRouter history={history}>
+            <AdvancedModeToggle />
+          </ConnectedRouter>
+        </Provider>
+      );
+    });
+    await waitFor(async () => {
+      const toggleSwitch = await screen.getByTestId('advanced-mode-toggle');
+      expect(toggleSwitch).toBeInTheDocument();
+    });
+
+  });
+  it('should the history be in /data-explorer', async () => {
+    await act(async () => {
+      await render(
+        <Provider store={store}>
+          <ConnectedRouter history={history}>
+            <AdvancedModeToggle />
+          </ConnectedRouter>
+        </Provider>
+      );
+    });
+    await waitFor(async () => {
+      const toggleSwitch = await screen.getByTestId('advanced-mode-toggle');
+      fireEvent.click(toggleSwitch);
+    });
+    const state = store.getState();
+    const currentPath = history.location.pathname;
+    expect(state.uiSettings.isAdvancedModeEnabled).toBeTruthy();
+    expect(currentPath).toBe('/data-explorer');
+  })
+  it('should not render the toggle on blacklisted pages', () => {
+    history.push('/studios');
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <AdvancedModeToggle />
+        </Router>
+      </Provider>
+    );
+
+    const toggleSwitch = screen.queryByTestId('advanced-mode-toggle');
+    expect(toggleSwitch).toBeNull();
+  });
+});
---
+++ src/shared/molecules/AdvancedMode/AdvancedMode.tsx
@@ -0,0 +1,63 @@
+
+import * as React from 'react';
+import { useLocation, useHistory } from 'react-router';
+import { match as pmatch } from 'ts-pattern'
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { Switch } from 'antd';
+import { RootState } from '../../store/reducers';
+import { UISettingsActionTypes } from '../../store/actions/ui-settings';
+import './styles.less';
+
+export const advancedModeBlackList = [
+    '/studios',
+    '/studio',
+    '/data-explorer'
+]
+export default function AdvancedModeToggle() {
+    const location = useLocation();
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const { isAdvancedModeEnabled } = useSelector((state: RootState) => state.uiSettings);
+    const showToggle = !advancedModeBlackList.includes(location.pathname);
+
+    const onToggle = () => {
+        dispatch({
+            type: UISettingsActionTypes.ENABLE_ADVANCED_MODE
+        })
+        history.push('/data-explorer');
+    }
+
+    React.useEffect(() => {
+        history.listen((location) => {
+            if (
+                location.pathname !== '/data-explorer' &&
+                isAdvancedModeEnabled
+            ) {
+                dispatch({
+                    type: UISettingsActionTypes.ENABLE_ADVANCED_MODE,
+                    payload: false,
+                })
+            }
+        })
+    }, [history, isAdvancedModeEnabled])
+
+    return pmatch(showToggle)
+        .with(true, () => {
+            return (
+                <div className='advanced-mode-toggle'>
+                    <Switch
+                        data-testid='advanced-mode-toggle'
+                        defaultChecked={false}
+                        checked={isAdvancedModeEnabled}
+                        onClick={onToggle}
+                    />
+                    <span className='advanced'>Advanced Mode</span>
+                    <span className='beta'>Beta</span>
+                </div>
+            )
+        })
+        .otherwise(
+            () => <></>
+        );
+}
