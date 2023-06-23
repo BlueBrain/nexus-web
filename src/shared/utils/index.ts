a2d870ddd9d0db028cf31a394252e77e4ccb0f1c
Bilal MEDDAH <b_meddah@esi.dz>
2023-06-24 00:06:05 +0200

    de-8: add another utility function to extract org/project tuple from resource object

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -347,6 +347,21 @@ export function getOrgAndProjectFromProjectId(
   }
   return null;
 }
+export function getOrgAndProjectFromResourceObject(resource: Resource) {
+  if (resource._project)
+    return getOrgAndProjectFromProjectId(resource._project);
+  else if (resource['@type'] === 'Project') {
+    return {
+      orgLabel: resource._organizationLabel,
+      projectLabel: resource._label,
+    };
+  } else {
+    return {
+      orgLabel: '',
+      projectLabel: '',
+    };
+  }
+}
 
 /**
  * Returns a project and org labels from url
