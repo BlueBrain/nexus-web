cf3182e0fa43fc98331788f968c505b74a6cf2ca
Bilal MEDDAH <b_meddah@esi.dz>
2023-06-24 01:32:30 +0200

    de-10: add navigation humberger when nav-stack is expanded

--- src/shared/molecules/DataExplorerGraphFlowMolecules/index.ts
+++ src/shared/molecules/DataExplorerGraphFlowMolecules/index.ts
@@ -2,3 +2,4 @@ export { default as DataExplorerGraphFlowContentLimitedHeader } from './ContentL
 export { default as NavigationBack } from './NavigationBack';
 export { default as NavigationStackItem } from './NavigationStackItem';
 export { default as NavigationStackShrinkedItem } from './NavigationStackShrinkedItem';
+export { default as NavigationHamburguer } from './NavigationHamburguer';
