32d142f31c5edbdf472bf778fa26de390b91c875
Bilal MEDDAH <b_meddah@esi.dz>
2023-06-24 00:54:09 +0200

    de-8: add data explorer graph flow page

---
+++ src/pages/DataExplorerGraphFlowPage/DataExplorerGraphFlowPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DataExplorerResolverPage from '../../shared/canvas/DataExplorerGraphFlow/DateExplorerGraphFlow';
+
+const DataExplorerGraphFlowPage = () => {
+  return <DataExplorerResolverPage />;
+};
+
+export default DataExplorerGraphFlowPage;
