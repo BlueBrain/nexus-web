ada34b7a19f54618c6657f9510a4b34a503c5f29
Bilal MEDDAH <b_meddah@esi.dz>
2023-06-24 00:11:11 +0200

    de-8: add data explorer graph flow state slice to redux

---
+++ src/shared/store/reducers/data-explorer.ts
@@ -0,0 +1,169 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { slice, omit, clone, dropRight, nth } from 'lodash';
+
+type TProject = string;
+type TOrganization = string;
+type TResourceID = string;
+type TVersionTag = number;
+export type TDEResource = [TOrganization, TProject, TResourceID, TVersionTag];
+export type TDELink = {
+  _self: string;
+  title: string;
+  types?: string | string[];
+  resource?: TDEResource;
+};
+export type TDataExplorerState = {
+  links: TDELink[];
+  current: TDELink | null;
+  shrinked: boolean;
+  limited: boolean;
+  highlightIndex: number;
+};
+
+const initialState: TDataExplorerState = {
+  links: [],
+  current: null,
+  shrinked: false,
+  limited: false,
+  highlightIndex: -1,
+};
+
+export const DATA_EXPLORER_GRAPH_FLOW_DIGEST = 'data-explorer-last-navigation';
+export const MAX_NAVIGATION_ITEMS_IN_STACK = 5;
+const calculateNewDigest = (state: TDataExplorerState) => {
+  const clonedState = clone(state);
+  const digest = btoa(
+    JSON.stringify({
+      ...clonedState,
+      links: clonedState.links.map(i => omit(i, ['highlight'])),
+      current: omit(clonedState.current, ['highlight']),
+    })
+  );
+  localStorage.setItem(DATA_EXPLORER_GRAPH_FLOW_DIGEST, digest);
+};
+
+const isShrinkable = (links: TDELink[]) => {
+  return links.length > MAX_NAVIGATION_ITEMS_IN_STACK;
+};
+
+export const dataExplorerSlice = createSlice({
+  initialState,
+  name: 'data-explorer-graph-flow',
+  reducers: {
+    PopulateDataExplorerGraphFlow: (state, action) => {
+      const digest = action.payload;
+      const _state: TDataExplorerState = JSON.parse(atob(digest));
+      try {
+        return {
+          ..._state,
+          shrinked: isShrinkable(_state.links),
+        };
+      } catch (error) {
+        return state;
+      }
+    },
+    InitNewVisitDataExplorerGraphView: (
+      state,
+      { payload: { source, current, limited } }
+    ) => {
+      const newState = {
+        ...state,
+        links:
+          source && current
+            ? source._self === current._self
+              ? []
+              : [source]
+            : [],
+        current: current,
+        limited,
+      };
+      calculateNewDigest(newState);
+      return newState;
+    },
+    AddNewNodeDataExplorerGraphFlow: (state, action) => {
+      const linkIndex = state.links.findIndex(
+        item => item._self === action.payload._self
+      );
+      const isCurrentLink = state.current?._self === action.payload._self;
+      const newLinks = isCurrentLink
+        ? state.links
+        : linkIndex !== -1
+        ? state.links
+        : [...state.links, state.current!];
+      const newCurrent =
+        isCurrentLink || linkIndex !== -1 ? state.current : action.payload;
+      const newState: TDataExplorerState = {
+        ...state,
+        links: newLinks,
+        current: newCurrent,
+        highlightIndex: linkIndex,
+        shrinked: linkIndex !== -1 ? false : isShrinkable(newLinks),
+      };
+      calculateNewDigest(newState);
+      return newState;
+    },
+    JumpToNodeDataExplorerGrpahFlow: (state, action) => {
+      const newLinks = slice(state.links, 0, action.payload);
+      const newState: TDataExplorerState = {
+        ...state,
+        links: newLinks,
+        current: state.links[action.payload],
+        shrinked: isShrinkable(newLinks),
+      };
+      calculateNewDigest(newState);
+      return newState;
+    },
+    ReturnBackDataExplorerGraphFlow: state => {
+      const lastItem = state.links.length ? nth(state.links, -1) : null;
+      const newLinks = dropRight(state.links);
+      const newState = lastItem
+        ? {
+            ...state,
+            links: newLinks,
+            current: lastItem,
+            shrinked: isShrinkable(newLinks),
+          }
+        : state;
+      calculateNewDigest(newState);
+      return newState;
+    },
+    ExpandNavigationStackDataExplorerGraphFlow: state => {
+      const newState = {
+        ...state,
+        shrinked: false,
+      };
+      calculateNewDigest(newState);
+      return newState;
+    },
+    ShrinkNavigationStackDataExplorerGraphFlow: state => {
+      const newState = {
+        ...state,
+        shrinked: isShrinkable(state.links) ? true : false,
+      };
+      calculateNewDigest(newState);
+      return newState;
+    },
+    ResetDataExplorerGraphFlow: () => {
+      return initialState;
+    },
+    ResetHighlightedNodeDataExplorerGraphFlow: state => {
+      return {
+        ...state,
+        highlightIndex: -1,
+      };
+    },
+  },
+});
+export const {
+  PopulateDataExplorerGraphFlow,
+  InitNewVisitDataExplorerGraphView,
+  AddNewNodeDataExplorerGraphFlow,
+  JumpToNodeDataExplorerGrpahFlow,
+  ExpandNavigationStackDataExplorerGraphFlow,
+  ShrinkNavigationStackDataExplorerGraphFlow,
+  ReturnBackDataExplorerGraphFlow,
+  ResetDataExplorerGraphFlow,
+  ResetHighlightedNodeDataExplorerGraphFlow,
+} = dataExplorerSlice.actions;
+
+export default dataExplorerSlice.reducer;
