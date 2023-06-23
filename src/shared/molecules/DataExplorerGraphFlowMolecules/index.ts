57569c64708d42eae30db7b218a8b216be140ca4
Bilal MEDDAH <b_meddah@esi.dz>
2023-06-24 01:31:36 +0200

    de-10: add navigation stack shrinked item

--- src/shared/molecules/DataExplorerGraphFlowMolecules/index.ts
+++ src/shared/molecules/DataExplorerGraphFlowMolecules/index.ts
@@ -1,3 +1,4 @@
 export { default as DataExplorerGraphFlowContentLimitedHeader } from './ContentLimitedHeader';
 export { default as NavigationBack } from './NavigationBack';
 export { default as NavigationStackItem } from './NavigationStackItem';
+export { default as NavigationStackShrinkedItem } from './NavigationStackShrinkedItem';
