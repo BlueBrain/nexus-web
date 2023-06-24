cb2e255777a71046acde7facb88b0fa3dca2b20b
Bilal MEDDAH <b_meddah@esi.dz>
2023-06-24 11:46:22 +0200

    de-dn: add file extension to file downloaded name

--- src/shared/utils/download.ts
+++ src/shared/utils/download.ts
@@ -1,11 +1,14 @@
+import { fileExtensionFromResourceEncoding } from '../../utils/contentTypes';
+
 export const download = (filename: string, mediaType: string, data: any) => {
   const blob = new Blob([data], { type: mediaType });
+  const extention = fileExtensionFromResourceEncoding(mediaType);
   if (window.navigator.msSaveBlob) {
     window.navigator.msSaveBlob(blob, filename);
   } else {
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
-    link.download = filename;
+    link.download = extention ? `${filename}.${extention}` : filename;
     link.click();
   }
 };
