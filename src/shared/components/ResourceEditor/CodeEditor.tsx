095627ba5dbe2348c0ff61a955a3621e1046067b
Bilal MEDDAH <b_meddah@esi.dz>
2023-06-27 10:56:42 +0200

    de-8: refactor code editor

---
+++ src/shared/components/ResourceEditor/CodeEditor.tsx
@@ -0,0 +1,76 @@
+import React, { forwardRef } from 'react';
+import codemiror, { EditorConfiguration } from 'codemirror';
+import { UnControlled as CodeMirror } from 'react-codemirror2';
+import { INDENT_UNIT } from '.';
+import { clsx } from 'clsx';
+import { Spin } from 'antd';
+
+type TCodeEditor = {
+  busy: boolean;
+  value: string;
+  editable: boolean;
+  keyFoldCode(cm: any): void;
+  loadingResolution: boolean;
+  handleChange(editor: any, data: any, value: any): void;
+  onLinkClick(_: any, ev: MouseEvent): void;
+  onLinksFound(): void;
+};
+type TEditorConfiguration = EditorConfiguration & {
+  foldCode: boolean;
+};
+const CodeEditor = forwardRef<codemiror.Editor | undefined, TCodeEditor>(
+  (
+    {
+      busy,
+      value,
+      editable,
+      keyFoldCode,
+      loadingResolution,
+      handleChange,
+      onLinkClick,
+      onLinksFound,
+    },
+    ref
+  ) => {
+    return (
+      <Spin spinning={busy}>
+        <CodeMirror
+          data-testId="code-mirror-editor"
+          value={value}
+          autoCursor={false}
+          detach={false}
+          options={
+            {
+              readOnly: !editable,
+              mode: { name: 'javascript', json: true },
+              theme: 'base16-light',
+              lineNumbers: true,
+              lineWrapping: true,
+              viewportMargin: Infinity,
+              foldGutter: true,
+              foldCode: true,
+              indentUnit: INDENT_UNIT,
+              gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
+              extraKeys: {
+                'Ctrl-Q': keyFoldCode,
+              },
+            } as TEditorConfiguration
+          }
+          className={clsx(
+            'code-mirror-editor',
+            loadingResolution && 'resolution-on-progress'
+          )}
+          onChange={handleChange}
+          editorDidMount={editor => {
+            // @ts-ignore
+            ref!.current = editor;
+          }}
+          onMouseDown={onLinkClick}
+          onUpdate={onLinksFound}
+        />
+      </Spin>
+    );
+  }
+);
+
+export default CodeEditor;
