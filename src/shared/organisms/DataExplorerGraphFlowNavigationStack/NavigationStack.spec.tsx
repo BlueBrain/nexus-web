2fcab82fa5e573e601e1f9d12a440dfc437a3b0e
Bilal MEDDAH <b_meddah@esi.dz>
2023-06-28 12:57:39 +0200

    de-10: add test for populating the dataexpolrer to store

--- src/shared/organisms/DataExplorerGraphFlowNavigationStack/NavigationStack.spec.tsx
+++ src/shared/organisms/DataExplorerGraphFlowNavigationStack/NavigationStack.spec.tsx
@@ -14,11 +14,61 @@ import {
   JumpToNodeDataExplorerGrpahFlow,
   ReturnBackDataExplorerGraphFlow,
   AddNewNodeDataExplorerGraphFlow,
+  ResetDataExplorerGraphFlow,
+  PopulateDataExplorerGraphFlow,
 } from '../../../shared/store/reducers/data-explorer';
 import { AnyAction, Store } from 'redux';
 import userEvent from '@testing-library/user-event';
 import { UserEvent } from '@testing-library/user-event/dist/types/setup/setup';
 
+const sampleDigest =
+  'eyJjdXJyZW50Ijp7Il9zZWxmIjoiaHR0cHM6Ly9iYnAuZXBmbC5jaC9uZXh1cy92MS9yZXNvdXJjZXMvYmJwL2F0bGFzL2RhdGFzaGFwZXM6dm9sdW1ldHJpY2RhdGFsYXllci9jY2UwNmU5Ni04NWE0LTQwM2QtOGI4Ny02MzRhMTU1NGNkYjkiLCJ0aXRsZSI6IkJCUCBNb3VzZSBCcmFpbiBUZW1wbGF0ZSBWb2x1bWUsIDI1bSIsInR5cGVzIjpbIlZvbHVtZXRyaWNEYXRhTGF5ZXIiLCJCcmFpblRlbXBsYXRlRGF0YUxheWVyIiwiRGF0YXNldCJdLCJyZXNvdXJjZSI6WyJiYnAiLCJhdGxhcyIsImh0dHBzOi8vYmJwLmVwZmwuY2gvbmV1cm9zY2llbmNlZ3JhcGgvZGF0YS9jY2UwNmU5Ni04NWE0LTQwM2QtOGI4Ny02MzRhMTU1NGNkYjkiLDJdfSwibGlua3MiOlt7Il9zZWxmIjoiaHR0cHM6Ly9iYnAuZXBmbC5jaC9uZXh1cy92MS9yZXNvdXJjZXMvYmJwL2xubWNlL2RhdGFzaGFwZXM6ZGF0YXNldC90cmFjZXMlMkY0NjBiZmEyZS1jYjdkLTQ0MjAtYTQ0OC0yMDMwYTZiZjRhZTQiLCJ0aXRsZSI6IjAwMV8xNDEyMTZfQTFfQ0ExcHlfUl9NUEciLCJ0eXBlcyI6WyJFbnRpdHkiLCJUcmFjZSIsIkRhdGFzZXQiXSwicmVzb3VyY2UiOlsiYmJwIiwibG5tY2UiLCJodHRwczovL2JicC5lcGZsLmNoL25ldXJvc2NpZW5jZWdyYXBoL2RhdGEvdHJhY2VzLzQ2MGJmYTJlLWNiN2QtNDQyMC1hNDQ4LTIwMzBhNmJmNGFlNCIsMTVdfSx7Il9zZWxmIjoiaHR0cHM6Ly9iYnAuZXBmbC5jaC9uZXh1cy92MS9yZXNvdXJjZXMvYmJwL2F0bGFzL2RhdGFzaGFwZXM6YXRsYXNzcGF0aWFscmVmZXJlbmNlc3lzdGVtL2FsbGVuX2NjZnYzX3NwYXRpYWxfcmVmZXJlbmNlX3N5c3RlbSIsInRpdGxlIjoiQWxsZW4gTW91c2UgQ0NGIiwidHlwZXMiOlsiQXRsYXNTcGF0aWFsUmVmZXJlbmNlU3lzdGVtIiwiQnJhaW5BdGxhc1NwYXRpYWxSZWZlcmVuY2VTeXN0ZW0iXSwicmVzb3VyY2UiOlsiYmJwIiwiYXRsYXMiLCJodHRwczovL2JicC5lcGZsLmNoL25ldXJvc2NpZW5jZWdyYXBoL2RhdGEvYWxsZW5fY2NmdjNfc3BhdGlhbF9yZWZlcmVuY2Vfc3lzdGVtIiw5XX1dLCJzaHJpbmtlZCI6ZmFsc2UsImhpZ2hsaWdodEluZGV4IjotMX0=';
+const digestCurrentSelf =
+  'https://bbp.epfl.ch/nexus/v1/resources/bbp/atlas/datashapes:volumetricdatalayer/cce06e96-85a4-403d-8b87-634a1554cdb9';
+const initialDataExplorerState = {
+  current: {
+    isDownloadable: false,
+    _self:
+      'https://bbp.epfl.ch/nexus/v1/resources/bbp/atlas/datashapes:organization/https:%2F%2Fwww.grid.ac%2Finstitutes%2Fgrid.417881.3',
+    title: 'Allen Institute for Brain Science',
+    types: ['Agent', 'Organization'],
+    resource: [
+      'bbp',
+      'atlas',
+      'https://www.grid.ac/institutes/grid.417881.3',
+      1,
+    ],
+  },
+  links: [
+    {
+      _self:
+        'https://bbp.epfl.ch/nexus/v1/resources/bbp/lnmce/datashapes:dataset/traces%2F460bfa2e-cb7d-4420-a448-2030a6bf4ae4',
+      title: '001_141216_A1_CA1py_R_MPG',
+      types: ['Entity', 'Trace', 'Dataset'],
+      resource: [
+        'bbp',
+        'lnmce',
+        'https://bbp.epfl.ch/neurosciencegraph/data/traces/460bfa2e-cb7d-4420-a448-2030a6bf4ae4',
+      ],
+    },
+    {
+      isDownloadable: false,
+      _self:
+        'https://bbp.epfl.ch/nexus/v1/resources/bbp/atlas/datashapes:organization/https:%2F%2Fwww.grid.ac%2Finstitutes%2Fgrid.5333.6',
+      title: 'Ecole Polytechnique Federale de Lausanne',
+      types: ['Organization', 'prov#Agent'],
+      resource: [
+        'bbp',
+        'atlas',
+        'https://www.grid.ac/institutes/grid.5333.6',
+        1,
+      ],
+    },
+  ],
+  shrinked: false,
+  highlightIndex: -1,
+  limited: false,
+};
 const fourthItemInStack = {
   isDownloadable: false,
   _self:
@@ -49,52 +99,7 @@ describe('NavigationStack', () => {
     store = configureStore(
       history,
       { nexus },
-      {
-        dataExplorer: {
-          current: {
-            isDownloadable: false,
-            _self:
-              'https://bbp.epfl.ch/nexus/v1/resources/bbp/atlas/datashapes:organization/https:%2F%2Fwww.grid.ac%2Finstitutes%2Fgrid.417881.3',
-            title: 'Allen Institute for Brain Science',
-            types: ['Agent', 'Organization'],
-            resource: [
-              'bbp',
-              'atlas',
-              'https://www.grid.ac/institutes/grid.417881.3',
-              1,
-            ],
-          },
-          links: [
-            {
-              _self:
-                'https://bbp.epfl.ch/nexus/v1/resources/bbp/lnmce/datashapes:dataset/traces%2F460bfa2e-cb7d-4420-a448-2030a6bf4ae4',
-              title: '001_141216_A1_CA1py_R_MPG',
-              types: ['Entity', 'Trace', 'Dataset'],
-              resource: [
-                'bbp',
-                'lnmce',
-                'https://bbp.epfl.ch/neurosciencegraph/data/traces/460bfa2e-cb7d-4420-a448-2030a6bf4ae4',
-              ],
-            },
-            {
-              isDownloadable: false,
-              _self:
-                'https://bbp.epfl.ch/nexus/v1/resources/bbp/atlas/datashapes:organization/https:%2F%2Fwww.grid.ac%2Finstitutes%2Fgrid.5333.6',
-              title: 'Ecole Polytechnique Federale de Lausanne',
-              types: ['Organization', 'prov#Agent'],
-              resource: [
-                'bbp',
-                'atlas',
-                'https://www.grid.ac/institutes/grid.5333.6',
-                1,
-              ],
-            },
-          ],
-          shrinked: false,
-          highlightIndex: -1,
-          limited: false,
-        },
-      }
+      { dataExplorer: initialDataExplorerState }
     );
     app = (
       <Provider store={store}>
@@ -110,7 +115,6 @@ describe('NavigationStack', () => {
     rerender = component.rerender;
     user = userEvent.setup();
   });
-
   it('should render the correct number of NavigationStackItem components in the state', () => {
     const navigationItems = container.querySelectorAll(
       '.navigation-stack-item:not(.no-more)'
@@ -243,5 +247,14 @@ describe('NavigationStack', () => {
   it('should the fourth item in the stack will be the current one when the user jump to the 4th item', () => {
     const state = store.getState();
     expect(state.dataExplorer.current._self).toEqual(fourthItemInStack._self);
+  });
+  it('should decode the navigation digest at first render', () => {
+    store.dispatch(ResetDataExplorerGraphFlow());
+    store.dispatch(PopulateDataExplorerGraphFlow(sampleDigest));
+    render(app);
+    const dataExplorerState = store.getState().dataExplorer;
+    expect(dataExplorerState.links.length).toBe(2);
+    expect(dataExplorerState.current._self).toBe(digestCurrentSelf);
+    expect(dataExplorerState.shrinked).toBe(false);
   });
 });
