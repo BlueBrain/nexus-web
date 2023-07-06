23c1222e4bb52fb533b1c6423510ae2d1b89ef2a
Dinika Saxena <dinikasaxenas@gmail.com>
2023-07-06 18:03:40 +0200

    3911 // Can be removed later - Simplify test assertions

--- src/shared/components/EditTableForm.tsx
+++ src/shared/components/EditTableForm.tsx
@@ -560,7 +560,6 @@ const EditTableForm: React.FC<{
               <Select
                 value={viewName}
                 aria-label="View"
-                data-testid="view-selector"
                 style={{ width: 650 }}
                 onChange={value => {
                   onChangeViewDropDown(value);
@@ -568,7 +567,11 @@ const EditTableForm: React.FC<{
               >
                 {availableViews &&
                   availableViews.map(view => (
-                    <Option key={view['@id']} value={view['@id']}>
+                    <Option
+                      key={view['@id']}
+                      value={view['@id']}
+                      data-testid={view['@id']}
+                    >
                       {view['@id']}
                     </Option>
                   ))}
