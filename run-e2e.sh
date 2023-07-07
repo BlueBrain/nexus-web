ee1fbc89d85baa7102296950b474037dbd41f9d4
Dinika Saxena <dinikasaxenas@gmail.com>
2023-07-07 15:21:51 +0200

    3911 // Enable debug logs

--- run-e2e.sh
+++ run-e2e.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
-# Xvfb -screen 0 1024x768x24 :99 &
+Xvfb -screen 0 1024x768x24 :99 &
 cypress run --config-file cypress.config.ts --browser chrome
 # cypress run --headed --config-file cypress.config.ts --browser chrome --spec "cypress/e2e/AnalysisPlugin.cy.ts"
