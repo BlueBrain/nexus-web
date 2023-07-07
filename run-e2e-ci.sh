17a547719d4f3e1eba372eea1554575889f3d039
Dinika Saxena <dinikasaxenas@gmail.com>
2023-07-07 11:48:58 +0200

    3911 // Run in headless mode in ci

--- run-e2e-ci.sh
+++ run-e2e-ci.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
-Xvfb -screen 0 1024x768x24 :99 &
-cypress run --headed --config-file cypress.config.ts --browser chrome --record --key ${{ secrets.CYPRESS_RECORD_KEY }}  
+#!/bin/bash -x
+#Xvfb -screen 0 1024x768x24 :99 &
+#cypress run --headed --config-file cypress.config.ts --browser chrome --record
+cypress run --config-file cypress.config.ts --browser chrome --record
--- run-e2e.sh
+++ run-e2e.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-Xvfb -screen 0 1024x768x24 :99 &
-cypress run --headed --config-file cypress.config.ts --browser chrome --spec "cypress/e2e/Studios.cy.ts"
-cypress run --headed --config-file cypress.config.ts --browser chrome --spec "cypress/e2e/AnalysisPlugin.cy.ts"
+#!/bin/bash -x
+# Xvfb -screen 0 1024x768x24 :99 &
+cypress run --config-file cypress.config.ts --browser chrome
+# cypress run --headed --config-file cypress.config.ts --browser chrome --spec "cypress/e2e/AnalysisPlugin.cy.ts"
