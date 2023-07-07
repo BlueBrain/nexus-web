25d9d24438da547d4269ca185a7e9d069f7e00f7
Dinika Saxena <dinikasaxenas@gmail.com>
2023-07-07 07:24:16 +0200

    3911 // hard wait

--- cypress/support/Studios/StudioList.ts
+++ cypress/support/Studios/StudioList.ts
@@ -4,14 +4,8 @@ export class StudioListPage {
     cy.findByRole('textbox', { name: /Label/ }).type(name);
     cy.findByRole('button', { name: /Save/ }).click();
 
-    cy.findByText(/Studio has been created Successfully/i);
     cy.wait(5000);
-    cy.location('pathname').should(
-      'include',
-      encodeURIComponent('resources/Cypress-Testing')
-    );
-    const studioElement = cy.findByRole('heading', { name, timeout: 10_001 });
-    studioElement.contains(name);
-    return studioElement;
+
+    return cy;
   }
 }
