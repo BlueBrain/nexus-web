24bd91ec2e4f0973f6b9fe75deb5968ccb7d99b9
Dinika Saxena <dinikasaxenas@gmail.com>
2023-07-07 16:46:36 +0200

    3911 // Uninstall and reinstall cypress

--- run-e2e.sh
+++ run-e2e.sh
@@ -1,4 +1,3 @@
 #!/bin/bash -x
-cypress cache clear
 cypress run --config-file cypress.config.ts --browser chrome
 # cypress run --headed --config-file cypress.config.ts --browser chrome --spec "cypress/e2e/AnalysisPlugin.cy.ts"
