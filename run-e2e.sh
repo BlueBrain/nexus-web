443188c507151c3e797d780e6b3f484aa65591ca
Dinika Saxena <dinikasaxenas@gmail.com>
2023-07-09 21:55:04 +0200

    3911 // Remove old files

--- run-e2e.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -x
-cypress run --config-file cypress.config.ts --browser chrome
-# cypress run --headed --config-file cypress.config.ts --browser chrome --spec "cypress/e2e/AnalysisPlugin.cy.ts"
