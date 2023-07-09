95ff930809031b92b6629d95db73eb02a21b8421
Dinika Saxena <dinikasaxenas@gmail.com>
2023-07-09 22:05:58 +0200

    3911 // Remove changes that are not needed in source code

--- cypress/support/Studios/StudioList.ts
+++ cypress/support/Studios/StudioList.ts
@@ -3,16 +3,6 @@ export class StudioListPage {
     cy.findByRole('button', { name: /Create Studio/i }).click();
     cy.findByRole('textbox', { name: /Label/ }).type(name);
     cy.findByRole('button', { name: /Save/ }).click();
-
-    // cy.get('h1[data-testid="studio-title"]').contains(/name/i);
-    // cy.findByText(/Studio has been created Successfully/i);
-    // cy.location('pathname').should(
-    //   'include',
-    //   encodeURIComponent('resources/Cypress-Testing')
-    // );
-    // const studioElement = cy.findByRole('heading', { name, timeout: 10_001 });
-    // studioElement.contains(name);
-    // return studioElement;
     const studioElement = cy.findByRole('heading', { name });
     studioElement.contains(name);
     return studioElement;
