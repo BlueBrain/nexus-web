1da2d3db4281d9f05672c8cab46335b4ecec386d
Bilal MEDDAH <b_meddah@esi.dz>
2023-07-12 14:10:23 +0200

    d-4044/update: update navigation btns (simple/shrinked) and styles

--- src/shared/molecules/DataExplorerGraphFlowMolecules/NavigationStackShrinkedItem.tsx
+++ src/shared/molecules/DataExplorerGraphFlowMolecules/NavigationStackShrinkedItem.tsx
@@ -4,39 +4,42 @@ import { clsx } from 'clsx';
 import { MoreOutlined, PlusOutlined } from '@ant-design/icons';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../store/reducers';
-import { ExpandNavigationStackDataExplorerGraphFlow } from '../../store/reducers/data-explorer';
 import './styles.less';
+import { TDELink } from 'shared/store/reducers/data-explorer';
 
 const BORDER_ITEMS = 2;
-const NavigationStackShrinkedItem = () => {
-  const { links, shrinked, highlightIndex } = useSelector(
-    (state: RootState) => state.dataExplorer
-  );
-  const dispatch = useDispatch();
+const NavigationStackShrinkedItem = ({
+  side,
+  links,
+  shrinked,
+  onExpand,
+}: {
+  side: 'left' | 'right';
+  shrinked: boolean;
+  links: TDELink[];
+  onExpand: () => void;
+}) => {
   const count = links.length - BORDER_ITEMS;
-  const onClick = () => dispatch(ExpandNavigationStackDataExplorerGraphFlow());
   return (
     <div
-      className={clsx(
-        'navigation-stack-item',
-        shrinked ? 'more' : 'no-more',
-        shrinked && highlightIndex !== -1 && 'highlight'
-      )}
+      className={clsx('navigation-stack-item', shrinked && 'more', side)}
       hidden={!shrinked}
     >
-      <Tooltip
-        placement="bottomRight"
-        overlayClassName="navigation-item-tooltip"
-        title={<div>Open {count} other resources</div>}
-      >
-        <PlusOutlined
-          role="open-shrinked-nav"
-          className="icon"
-          onClick={onClick}
-        />
-      </Tooltip>
-      <div className="count">{count}</div>
-      <MoreOutlined style={{ fontSize: 18, color: '#BFBFBF' }} />
+      <div className={clsx('navigation-stack-item__wrapper')}>
+        <Tooltip
+          placement="bottomRight"
+          overlayClassName="navigation-item-tooltip"
+          title={<div>Open {count} other resources</div>}
+        >
+          <PlusOutlined
+            role="open-shrinked-nav"
+            className="icon"
+            onClick={onExpand}
+          />
+        </Tooltip>
+        <div className="count">{count}</div>
+        <MoreOutlined style={{ fontSize: 18, color: '#BFBFBF' }} />
+      </div>
     </div>
   );
 };
