24dd947e9cd6ddb955670ce7cec088cb0f5efa31
Bilal MEDDAH <b_meddah@esi.dz>
2023-07-12 14:13:43 +0200

    d-4044/update: back btn

--- src/shared/molecules/DataExplorerGraphFlowMolecules/NavigationBackButton.tsx
+++ src/shared/molecules/DataExplorerGraphFlowMolecules/NavigationBackButton.tsx
@@ -15,35 +15,34 @@ const NavigationBack = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const location = useLocation();
-  const { links, referer } = useSelector(
-    (state: RootState) => state.dataExplorer
-  );
+  const { referer } = useSelector((state: RootState) => state.dataExplorer);
 
-  const onBack = () => {
-    if (referer?.pathname && !links.length) {
-      dispatch(ResetDataExplorerGraphFlow({ initialState: null }));
-      localStorage.removeItem(DATA_EXPLORER_GRAPH_FLOW_DIGEST);
-      history.push(`${referer.pathname}${referer.search}`, {
-        ...referer.state,
-      });
-      return;
-    }
-    history.replace(location.pathname);
-    dispatch(ReturnBackDataExplorerGraphFlow());
-  };
+  // const onBack = () => {
+  //   if (referer?.pathname && !links.length) {
+  //     dispatch(ResetDataExplorerGraphFlow({ initialState: null }));
+  //     localStorage.removeItem(DATA_EXPLORER_GRAPH_FLOW_DIGEST);
+  //     history.push(`${referer.pathname}${referer.search}`, {
+  //       ...referer.state,
+  //     });
+  //     return;
+  //   }
+  //   history.replace(location.pathname);
+  //   dispatch(ReturnBackDataExplorerGraphFlow());
+  // };
 
-  return links.length || !!referer?.pathname ? (
-    <button
-      className={clsx(
-        'navigation-back-btn',
-        referer?.pathname && !links.length && 'go-back-to-referer'
-      )}
-      onClick={onBack}
-    >
-      <ArrowLeftOutlined />
-      <span>Back</span>
-    </button>
-  ) : null;
+  // return links.length || !!referer?.pathname ? (
+  //   <button
+  //     className={clsx(
+  //       'navigation-back-btn',
+  //       referer?.pathname && !links.length && 'go-back-to-referer'
+  //     )}
+  //     onClick={onBack}
+  //   >
+  //     <ArrowLeftOutlined />
+  //     <span>Back</span>
+  //   </button>
+  // ) : null;
+  return null;
 };
 
 export default NavigationBack;
