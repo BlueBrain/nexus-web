5113687e1f750e4593665a054539602859f6876b
Bilal MEDDAH <b_meddah@esi.dz>
2023-07-13 10:20:31 +0200

    f-4046/update: add navigation arrows

---
+++ src/shared/molecules/DataExplorerGraphFlowMolecules/NavigationArrow.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@ant-design/icons';
+import './styles.less';
+
+type NavigationArrowDirection = 'back' | 'forward';
+
+const NavigationArrow = ({
+  direction,
+  visible,
+  title,
+  onClick,
+}: {
+  direction: NavigationArrowDirection;
+  visible: boolean;
+  title: string;
+  onClick: () => void;
+}) => {
+  return visible ? (
+    <button
+      className={`navigation-arrow-btn ${direction}`}
+      aria-label={`${direction}-arrow`}
+      onClick={onClick}
+    >
+      {direction === 'back' && <ArrowLeftOutlined />}
+      <span>{title}</span>
+      {direction === 'forward' && <ArrowRightOutlined />}
+    </button>
+  ) : null;
+};
+
+export default NavigationArrow;
