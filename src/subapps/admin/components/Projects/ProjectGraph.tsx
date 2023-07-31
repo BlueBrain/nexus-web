448de38a448b3de93f404bbc208b200eb58f326c
Bilal MEDDAH <b_meddah@esi.dz>
2023-07-31 13:51:14 +0200

    f-4117/update: update styles

--- src/subapps/admin/components/Projects/ProjectGraph.tsx
+++ src/subapps/admin/components/Projects/ProjectGraph.tsx
@@ -64,7 +64,7 @@ const ProjectGraph: React.FC<{
       layout: {
         // @ts-ignore
         name: 'avsdf',
-         // @ts-ignore
+        // @ts-ignore
         fit: true,
         nodeSeparation: 100,
       },
