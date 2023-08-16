81c430be4da91f0227d77a6fffbfe4f4dde9f57d
Bilal MEDDAH <b_meddah@esi.dz>
2023-08-16 17:01:10 +0200

    update: add vite config and add needed packages

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+/** @type {import('vite').UserConfig} */
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgrPlugin from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import 'vite-compatible-readable-stream';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths(), svgrPlugin()],
+  resolve: {
+    alias: {
+      process: 'process/browser',
+      buffer: 'buffer',
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      assert: 'assert',
+      http: 'stream-http',
+      https: 'https-browserify',
+      os: 'os-browserify',
+      url: 'url',
+      util: 'util',
+    },
+  },
+  define: {
+    'process.env': process.env,
+  },
+  assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.mp4'],
+});
