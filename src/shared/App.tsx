471ca08ee6c2422d3ed13d1f46fdc099e9c9200e
Bilal MEDDAH <b_meddah@esi.dz>
2023-08-22 08:45:23 +0200

    update parts of app

--- src/shared/App.tsx
+++ src/shared/App.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import { useSelector } from 'react-redux';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { useQuery } from 'react-query';
@@ -8,7 +7,6 @@ import routes from '../shared/routes';
 import FusionMainLayout from './layouts/FusionMainLayout';
 import SubAppsView from './views/SubAppsView';
 import useSubApps from './hooks/useSubApps';
-import useDataCart, { CartContext, CartType } from './hooks/useDataCart';
 import {
   getNotificationContextValue,
   NotificationContext,
@@ -16,25 +14,17 @@ import {
 } from './hooks/useNotification';
 import { RootState } from './store/reducers';
 import DataPanel from './organisms/DataPanel/DataPanel';
-import CreateProject from './modals/CreateProject/CreateProject';
-import CreateOrganization from './modals/CreateOrganization/CreateOrganization';
-import CreateStudio from './modals/CreateStudio/CreateStudio';
 import AppInfo from './modals/AppInfo/AppInfo';
+import EntityCreation from './modals';
 
 import './App.scss';
 
 const App: React.FC = () => {
   const nexus = useNexusContext();
   const { subAppRoutes } = useSubApps();
-  const cartData: CartType = useDataCart();
-  const { oidc, config } = useSelector((state: RootState) => ({
-    oidc: state.oidc,
-    config: state.config,
-  }));
-  const authenticated = !!oidc.user;
-  const token = oidc.user && oidc.user.access_token;
+  const config = useSelector((state: RootState) => state.config);
+
   const notificationData: NotificationContextType = getNotificationContextValue();
-  const userAuthenticated = Boolean(authenticated) && Boolean(token);
   const routesWithSubApps = [...routes, ...subAppRoutes];
 
   const { data: nexusEcosystem } = useQuery({
@@ -44,26 +34,20 @@ const App: React.FC = () => {
         path: `${config.apiEndpoint}/version`,
         context: { as: 'json' },
       }),
+    refetchOnWindowFocus: false,
   });
+
   return (
-    <CartContext.Provider value={cartData}>
-      <NotificationContext.Provider value={notificationData}>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <FusionMainLayout environment={nexusEcosystem?.environment}>
-          <SubAppsView routesWithSubApps={routesWithSubApps} />
-          <AppInfo {...{ ...nexusEcosystem }} />
-          <DataPanel />
-          <GalleryView />
-          {userAuthenticated && (
-            <React.Fragment>
-              <CreateProject />
-              <CreateOrganization />
-              <CreateStudio />
-            </React.Fragment>
-          )}
-        </FusionMainLayout>
-      </NotificationContext.Provider>
-    </CartContext.Provider>
+    <NotificationContext.Provider value={notificationData}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <FusionMainLayout environment={nexusEcosystem?.environment}>
+        <SubAppsView routesWithSubApps={routesWithSubApps} />
+        <AppInfo {...{ ...nexusEcosystem }} />
+        <DataPanel />
+        <GalleryView />
+        <EntityCreation />
+      </FusionMainLayout>
+    </NotificationContext.Provider>
   );
 };
 
