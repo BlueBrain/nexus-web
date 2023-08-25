4608ed8a3e8a79f26c7419b64bc97ca1ae7b57f0
Bilal MEDDAH <b_meddah@esi.dz>
2023-08-25 13:38:18 +0200

    update: refactor main parts
    update: add auth manager
    
    update: refactor fonts

---
+++ src/styles.ts
@@ -0,0 +1,8 @@
+import '@fontsource/titillium-web/200.css';
+import '@fontsource/titillium-web/300.css';
+import '@fontsource/titillium-web/400.css';
+import '@fontsource/titillium-web/600.css';
+import '@fontsource/titillium-web/700.css';
+import '@fontsource/titillium-web/900.css';
+
+import './global.scss';
