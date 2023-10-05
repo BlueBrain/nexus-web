b18b12595e65f9661c10bc6b3edb0de4c1e4a420
Bilal MEDDAH <b_meddah@esi.dz>
2023-10-05 11:43:47 +0200

    update: move state constructor to constants

--- server/constants.ts
+++ server/constants.ts
@@ -1,3 +1,4 @@
+import { Request } from 'express';
 // Report Constants
 export const DEFAULT_SCALE = 50;
 
@@ -117,3 +118,139 @@ WHERE {
   }
 }
 LIMIT 1000`;
+
+const DEFAULT_SEARCH_CONFIG_PROJECT = 'webapps/nexus-web';
+const DEFAULT_SERVICE_ACCOUNTS_REALM = 'serviceaccounts';
+const rawBase = process.env.BASE_PATH || '';
+// to develop plugins locally, change PLUGINS_PATH to '/public/plugins'
+const pluginsManifestPath = process.env.PLUGINS_MANIFEST_PATH || '/plugins';
+
+// configure instance logo
+const layoutSettings = {
+  docsLink: process.env.DOCS_LINK || '',
+  logoImg: process.env.LOGO_IMG || '',
+  forgeLink: process.env.FORGE_LINK || '',
+  organizationImg: process.env.ORGANIZATION_IMG || '',
+  projectsImg: process.env.PROJECTS_IMG || '',
+  studiosImg: process.env.STUDIOS_IMG || '',
+  projectImg: process.env.PROJECT_IMG || '',
+  landingVideo: process.env.LANDING_VIDEO || '',
+  landingPosterImg: process.env.LANDING_POSTER_IMG || '',
+  mainColor: process.env.MAIN_COLOR || '#062d68',
+};
+
+// configure search settings
+const searchSettings = {
+  searchConfigProject:
+    process.env.SEARCH_CONFIG_PROJECT || DEFAULT_SEARCH_CONFIG_PROJECT,
+};
+
+// configure datamodels projects
+const dataModelsLocation = process.env.DATA_MODELS || '';
+
+const subAppsManifestPath =
+  process.env.SUB_APPS_MANIFEST_PATH || '/public/sub-apps';
+
+const base = rawBase.replace(/\/$/, '');
+
+type TMode = 'production' | 'development';
+type TRealms = { _results: any[]; _total: number };
+
+const gePreloadedState = ({
+  req,
+  realms,
+  mode,
+}: {
+  req: Request;
+  realms: TRealms;
+  mode: TMode;
+}) => ({
+  auth: {
+    realms: { data: realms },
+    identities: { data: null },
+  },
+  config: {
+    searchSettings,
+    layoutSettings,
+    pluginsManifestPath,
+    subAppsManifestPath,
+    dataModelsLocation,
+    mode: mode,
+    apiEndpoint: process.env.PROXY ? '/proxy' : process.env.API_ENDPOINT || '',
+    basePath: base,
+    clientId: process.env.CLIENT_ID || 'bbp-nise-dev-nexus-fusion',
+    redirectHostName: `${process.env.HOST_NAME ||
+      `${req.protocol}://${req.headers.host}`}${base}`,
+    serviceAccountsRealm:
+      process.env.SERVICE_ACCOUNTS_REALM || DEFAULT_SERVICE_ACCOUNTS_REALM,
+    sentryDsn: process.env.SENTRY_DSN,
+    gtmCode: process.env.GTM_CODE,
+    studioView: process.env.STUDIO_VIEW || '',
+    jiraUrl: process.env.JIRA_URL || '',
+    jiraResourceCustomFieldName: process.env.JIRA_RESOURCE_FIELD_NAME || '',
+    jiraResourceCustomFieldLabel:
+      process.env.JIRA_RESOURCE_FIELD_LABEL || 'Nexus Resource',
+    jiraProjectCustomFieldName: process.env.JIRA_PROJECT_FIELD_NAME || '',
+    jiraProjectCustomFieldLabel:
+      process.env.JIRA_PROJECT_FIELD_LABEL || 'Nexus Project',
+    ...(process.env.JIRA_SUPPORTED_REALMS && {
+      jiraSupportedRealms: process.env.JIRA_SUPPORTED_REALMS.split(','),
+    }),
+    analysisPluginShowOnTypes: process.env.ANALYSIS_PLUGIN_SHOW_ON_TYPES
+      ? process.env.ANALYSIS_PLUGIN_SHOW_ON_TYPES.split(',')
+      : [],
+    analysisPluginExcludeTypes: process.env.ANALYSIS_PLUGIN_EXCLUDE_TYPES
+      ? process.env.ANALYSIS_PLUGIN_EXCLUDE_TYPES.split(',')
+      : [],
+    analysisPluginSparqlDataQuery:
+      process.env.ANALYSIS_PLUGIN_SPARQL_DATA_QUERY ||
+      DEFAULT_ANALYSIS_DATA_SPARQL_QUERY,
+    analysisPluginCategories: process.env.ANALYSIS_PLUGIN_CATEGORIES
+      ? JSON.parse(process.env.ANALYSIS_PLUGIN_CATEGORIES)
+      : DEFAULT_REPORT_CATEGORIES,
+    analysisPluginTypes: process.env.ANALYSIS_PLUGIN_TYPES
+      ? JSON.parse(process.env.ANALYSIS_PLUGIN_TYPES)
+      : DEFAULT_REPORT_TYPES,
+    httpHeaderForInaccessibleDueToVPN:
+      process.env.HTTP_HEADER_WHERE_INACCESSIBLE_OUTSIDE_OF_VPN ||
+      'x-requires-vpn',
+  },
+  uiSettings: {
+    openCreationPanel: false,
+    pageSizes: {
+      orgsListPageSize: 5,
+      projectsListPageSize: 5,
+      resourcesListPageSize: 20,
+      linksListPageSize: 10,
+    },
+    currentResourceView: null,
+    isAdvancedModeEnabled: false,
+  },
+  oidc: {
+    user: undefined,
+    isLoadingUser: false,
+  },
+  search: {
+    searchConfigs: {
+      isFetching: false,
+      data: null,
+      error: null,
+    },
+    searchPreference: null,
+  },
+  modals: {
+    isCreateOrganizationModelVisible: false,
+    isCreateProjectModelVisible: false,
+    isCreateStudioModelVisible: false,
+    isAboutModelVisible: false,
+  },
+  dataExplorer: {
+    current: null,
+    leftNodes: { links: [], shrinked: false },
+    rightNodes: { links: [], shrinked: false },
+    fullscreen: false,
+    origin: '',
+  },
+});
+
+export { base, gePreloadedState };
