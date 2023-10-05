9d066d57c941739ef12532659ea116f6ee6aae3a
Dinika Saxena <dinikasaxenas@gmail.com>
2023-10-05 17:22:58 +0200

    Add theme for modals

--- src/theme/antd.ts
+++ src/theme/antd.ts
@@ -3,9 +3,18 @@ import { ThemeConfig } from 'antd';
 export const antdTheme: ThemeConfig = {
   token: {
     fontFamily: 'var(--font-family)',
-    colorTextHeading: '#8c8c8c', // $fusion-neutral-7
     colorLink: '#0070c9', // $fusion-active-link-color
     borderRadius: 2,
+  },
+  components: {
+    Modal: {
+      titleColor: '#333333', // $fusion-primary-text-color,
+      contentBg: '#f2f2f2',
+      headerBg: '#f2f2f2',
+    },
+    Table: {
+      headerColor: '#8c8c8c', // $fusion-neutral-7
+    },
   },
   hashed: false,
 };
