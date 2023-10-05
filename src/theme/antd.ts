b3a84bc0e3b9843ccbd31cffb6624344718f13b2
Dinika Saxena <dinikasaxenas@gmail.com>
2023-10-05 16:31:33 +0200

    Fix search styles

--- src/theme/antd.ts
+++ src/theme/antd.ts
@@ -3,7 +3,8 @@ import { ThemeConfig } from 'antd';
 export const antdTheme: ThemeConfig = {
   token: {
     fontFamily: 'var(--font-family)',
-    colorTextHeading: '#8c8c8c',
+    colorTextHeading: '#8c8c8c', // $fusion-neutral-7
+    colorLink: '#0070c9', // $fusion-active-link-color
     borderRadius: 2,
   },
   hashed: false,
