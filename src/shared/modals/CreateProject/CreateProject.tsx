c2b9a1cfbaee3429269fbbd82adfd8a35172ac3c
Bilal Meddah <bilal.meddah@epfl.ch>
2023-11-01 09:45:05 +0100

    f4011/fix: test error text for regex of org/project/tag

--- src/shared/modals/CreateProject/CreateProject.tsx
+++ src/shared/modals/CreateProject/CreateProject.tsx
@@ -298,7 +298,8 @@ const CreateProject: React.FC<{}> = ({}) => {
             },
             {
               pattern: /^[a-zA-Z0-9_-]+$/,
-              message: 'Label must contains only letters and numbers',
+              message:
+                'Label should include only letters, numbers, underscores, and dashes.',
             },
           ]}
         >
