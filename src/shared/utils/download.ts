90d86c4ddd90eebd2d6bb28ed683532556649a80
Daniel Burger <public@danielburger.online>
2023-12-04 11:18:17 +0100

    Fix bug

--- src/shared/utils/download.ts
+++ src/shared/utils/download.ts
@@ -3,12 +3,18 @@ import { fileExtensionFromResourceEncoding } from '../../utils/contentTypes';
 export const download = (filename: string, mediaType: string, data: any) => {
   const blob = new Blob([data], { type: mediaType });
   const extension = fileExtensionFromResourceEncoding(mediaType);
+
+  // Check if filename already ends with the correct extension
+  const hasCorrectExtension = filename.endsWith(`.${extension}`);
+
   if (window.navigator.msSaveBlob) {
     window.navigator.msSaveBlob(blob, filename);
   } else {
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
-    link.download = extension ? `${filename}.${extension}` : filename;
+
+    // Only append extension if it's not already present
+    link.download = hasCorrectExtension ? filename : `${filename}.${extension}`;
     link.click();
   }
 };
