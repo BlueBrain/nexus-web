03a3d81b68bbcfbd8a8ead175433e0851351bc36
Daniel Burger <public@danielburger.online>
2023-12-11 15:32:05 +0100

    Improve Regex to ignore underscore in the middle

--- src/shared/components/ResourceEditor/customLinter.ts
+++ src/shared/components/ResourceEditor/customLinter.ts
@@ -7,10 +7,11 @@ export const customLinter = (text: string): LinterIssue[] => {
   const linterErrors: LinterIssue[] = [];
   const lines = text.split('\n');
 
+  // Check for fields starting with an underscore
+  const regex = /"\s*_(\w+)"/g;
+
   // TODO Improve the performance of this linter by only checking the lines that have changed
   lines.forEach(line => {
-    // TODO Check to only check if it's an underscore at the beginning of the line
-    const regex = /"([^"]*_[^"]*)"\s*:/g;
     if (line.match(regex)) {
       linterErrors.push({
         message: 'Cannot have fields starting with an underscore',
