d46f71625151ca3b88672671c05b4284224bf123
Bilal Meddah <bilal.meddah@epfl.ch>
2023-12-22 14:05:18 +0100

    update: antd Select component props

--- src/subapps/dataExplorer/PredicateSelector.tsx
+++ src/subapps/dataExplorer/PredicateSelector.tsx
@@ -220,7 +220,7 @@ export const PredicateSelector: React.FC<Props> = ({
             aria-label="path-selector"
             data-testid="path-selector"
             style={{ width: 200, minWidth: 'max-content' }}
-            dropdownMatchSelectWidth={false} // This ensures that the items in the dropdown list are always fully legible (ie they are not truncated) just because the input of select is too short.
+            popupMatchSelectWidth={false} // This ensures that the items in the dropdown list are always fully legible (ie they are not truncated) just because the input of select is too short.
           />
         </Tooltip>
       </Form.Item>
