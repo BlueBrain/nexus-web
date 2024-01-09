f21a74ebf061f8c91ac8ebb62d7abc777a8b02bb
Bilal Meddah <bilal.meddah@epfl.ch>
2024-01-09 12:17:34 +0100

    add right path to facvicons in production

--- server/index.ts
+++ server/index.ts
@@ -102,6 +102,10 @@ async function transformer(html: string, req: Request) {
     .replace(
       '<!--app-head-->',
       `
+        <link rel="icon" href="favicon.ico" sizes="48x48" >
+        <link rel="icon" href="favicon.svg" sizes="any" type="image/svg+xml" >
+        <link rel="apple-touch-icon" href="apple-touch-icon-180x180.png" >
+        <link rel="manifest" href="manifest.json" >
         <script>
           window.__BASE__ = '${base}'
         </script>
